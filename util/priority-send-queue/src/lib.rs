034a2137306fe359d3675d69c628cc0118adf436
Ming Wu <ming@conflux-chain.org>
2019-04-16 11:36:02 +0800

    nit

--- util/priority-send-queue/src/lib.rs
+++ util/priority-send-queue/src/lib.rs
@@ -22,41 +22,48 @@ impl<T> PrioritySendQueue<T> {
         PrioritySendQueue { queues }
     }
 
+    fn queue(&self, priority: SendQueuePriority) -> &VecDeque<T> {
+        &self.queues[priority as usize]
+    }
+
+    fn queue_mut(&mut self, priority: SendQueuePriority) -> &mut VecDeque<T> {
+        &mut self.queues[priority as usize]
+    }
+
     pub fn pop_front(&mut self) -> Option<T> {
-        let res = self.queues[SendQueuePriority::High as usize].pop_front();
+        let res = self.queue_mut(SendQueuePriority::High).pop_front();
         if res.is_some() {
             return res;
         }
 
-        self.queues[SendQueuePriority::Normal as usize].pop_front()
+        self.queue_mut(SendQueuePriority::Normal).pop_front()
     }
 
     pub fn front_mut(&mut self) -> Option<&mut T> {
-        if self.queues[SendQueuePriority::High as usize].is_empty() {
-            let res =
-                self.queues[SendQueuePriority::Normal as usize].pop_front();
+        if self.queue(SendQueuePriority::High).is_empty() {
+            let res = self.queue_mut(SendQueuePriority::Normal).pop_front();
             if res.is_none() {
                 return None;
             }
 
             let res = res.unwrap();
-            self.queues[SendQueuePriority::High as usize].push_back(res);
+            self.queue_mut(SendQueuePriority::High).push_back(res);
         }
 
-        self.queues[SendQueuePriority::High as usize].front_mut()
+        self.queue_mut(SendQueuePriority::High).front_mut()
     }
 
     pub fn is_empty(&self) -> bool {
-        self.queues[SendQueuePriority::High as usize].is_empty()
-            && self.queues[SendQueuePriority::Normal as usize].is_empty()
+        self.queue(SendQueuePriority::High).is_empty()
+            && self.queue(SendQueuePriority::Normal).is_empty()
     }
 
     pub fn push_back(&mut self, value: T, priority: SendQueuePriority) {
-        self.queues[priority as usize].push_back(value);
+        self.queue_mut(priority).push_back(value);
     }
 
     pub fn len(&self) -> usize {
-        self.queues[SendQueuePriority::High as usize].len()
-            + self.queues[SendQueuePriority::Normal as usize].len()
+        self.queue(SendQueuePriority::High).len()
+            + self.queue(SendQueuePriority::Normal).len()
     }
 }
