752c0fe704848678c56f2036ffa50a579d351b62
peilun.li <peilun.li@conflux-chain.org>
2019-04-20 15:28:02 +0800

    Change parameters.

--- test/balance_attack_basic.py
+++ test/balance_attack_basic.py
@@ -14,9 +14,10 @@ class P2PTest(ConfluxTestFramework):
         self.setup_clean_chain = True
         self.num_nodes = 2
         self.start_attack = True
+        self.difficulty = 1000
         self.conf_parameters = {
             "start_mining": "true",
-            "initial_difficulty": "1000",
+            "initial_difficulty": str(self.difficulty),
             "test_mining_sleep_us": "100",
             "mining_author": '"' + "0"*40 + '"',
         }
@@ -24,24 +25,32 @@ class P2PTest(ConfluxTestFramework):
     def setup_network(self):
         self.setup_nodes()
         connect_nodes(self.nodes, 0, 1)
+
+        # Set latency between two groups
         self.nodes[0].addlatency(self.nodes[1].key, 1000)
         self.nodes[1].addlatency(self.nodes[0].key, 1000)
 
     def run_test(self):
         start_p2p_connection(self.nodes)
+
+        # Some blocks may have been mined before we setup the latency,
+        # so wait for the latency and find the fork point
         time.sleep(1)
         chain0 = self.nodes[0].getPivotChainAndWeight()
         chain1 = self.nodes[1].getPivotChainAndWeight()
         fork_height = 0
         while True:
-            print(fork_height)
             if chain0[fork_height][0] != chain1[fork_height][0]:
+                self.log.info("Forked at height %d", fork_height)
                 break
             fork_height += 1
             if fork_height >= min(len(chain0), len(chain1)):
-                assert False, ("No fork")
+                assert False, "No fork"
+
         while True:
+            # This roughly determines adversary's mining power
             time.sleep(0.1)
+
             chain0 = self.process_chain(self.nodes[0].getPivotChainAndWeight())
             self.check_chain_heavy(chain0, 0, fork_height)
             chain1 = self.process_chain(self.nodes[1].getPivotChainAndWeight())
@@ -55,18 +64,24 @@ class P2PTest(ConfluxTestFramework):
                 self.log.info("chain0 %s", chain0)
                 self.log.info("chain1 %s", chain1)
                 break
+
+            ''' Send blocks to keep balance.
+                The adversary's mining power and strategy is not strictly designed in the naive version.
+                If two forks are already balanced, we need to send blocks to both sides in case no blocks are mined.
+            '''
             if self.start_attack:
                 if fork0[1] <= fork1[1]:
                     parent = fork0[0]
-                    block = NewBlock(create_block(decode_hex(parent), height=fork_height+1, difficulty=1000, timestamp=random.randint(1, 2 ** 31)))
+                    block = NewBlock(create_block(decode_hex(parent), height=fork_height+1, difficulty=self.difficulty, timestamp=random.randint(1, 2 ** 31)))
                     self.nodes[0].p2p.send_protocol_msg(block)
                     self.log.info("send to 0 block %s, weight %d %d", block.block.hash_hex(), fork0[1], fork1[1])
                 if fork0[1] >= fork1[1]:
                     parent = fork1[0]
-                    block = NewBlock(create_block(decode_hex(parent), height=fork_height+1, difficulty=1000, timestamp=random.randint(1, 2 ** 31)))
+                    block = NewBlock(create_block(decode_hex(parent), height=fork_height+1, difficulty=self.difficulty, timestamp=random.randint(1, 2 ** 31)))
                     self.nodes[1].p2p.send_protocol_msg(block)
                     self.log.info("send to 1 block %s, weight %d %d", block.block.hash_hex(), fork0[1], fork1[1])
 
+    # Convert weight to integer
     def process_chain(self, chain):
         for i in range(len(chain)):
             chain[i][1] = parse_as_int(chain[i][1])
@@ -74,10 +89,10 @@ class P2PTest(ConfluxTestFramework):
 
     def check_chain_heavy(self, chain, chain_id, fork_height):
         for i in range(fork_height+1, len(chain)-1):
-            if chain[i][1] - chain[i+1][1] >= 1000 * 240:
+            if chain[i][1] - chain[i+1][1] >= self.difficulty * 240:
                 self.log.info("chain %d is heavy at height %d %d %d", chain_id, i,  chain[i][1], chain[i+1][1])
                 return
-        if chain[-1][1] >= 1000 * 240:
+        if chain[-1][1] >= self.difficulty * 240:
             self.log.info("chain %d is heavy at height %d %d %d", chain_id, i,  chain[i][1], chain[i+1][1])
 
 
