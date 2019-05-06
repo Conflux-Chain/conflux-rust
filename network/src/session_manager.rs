783d6c83e06aa2389a86e8b397726822728e71a9
boqiu <82121246@qq.com>
2019-05-06 23:10:39 +0800

    fix comment

--- network/src/session_manager.rs
+++ network/src/session_manager.rs
@@ -12,34 +12,20 @@ use parking_lot::RwLock;
 use slab::{Slab, SlabIter};
 use std::{collections::HashMap, net::SocketAddr, sync::Arc};
 
-struct SessionStatus {
-    node_id_index: HashMap<NodeId, usize>,
-    ip_limit: NodeIpLimit,
-}
-
-impl SessionStatus {
-    fn new(nodes_per_ip: usize) -> Self {
-        SessionStatus {
-            node_id_index: HashMap::new(),
-            ip_limit: NodeIpLimit::new(nodes_per_ip),
-        }
-    }
-}
-
 /// Session manager maintains all ingress and egress TCP connections.
 /// It supports to limit the connections according to node IP policy.
 pub struct SessionManager {
     sessions: Slab<Arc<RwLock<Session>>, usize>,
-    ingress_status: SessionStatus,
-    egress_status: SessionStatus,
+    node_id_index: HashMap<NodeId, usize>,
+    ip_limit: NodeIpLimit,
 }
 
 impl SessionManager {
     pub fn new(offset: usize, capacity: usize, nodes_per_ip: usize) -> Self {
         SessionManager {
             sessions: Slab::new_starting_at(offset, capacity),
-            ingress_status: SessionStatus::new(nodes_per_ip),
-            egress_status: SessionStatus::new(nodes_per_ip),
+            node_id_index: HashMap::new(),
+            ip_limit: NodeIpLimit::new(nodes_per_ip),
         }
     }
 
@@ -75,8 +61,7 @@ impl SessionManager {
     }
 
     pub fn contains_node(&self, id: &NodeId) -> bool {
-        self.ingress_status.node_id_index.contains_key(id)
-            || self.egress_status.node_id_index.contains_key(id)
+        self.node_id_index.contains_key(id)
     }
 
     /// Creates a new session with specified TCP socket. It is egress connection
@@ -86,33 +71,23 @@ impl SessionManager {
         io: &IoContext<NetworkIoMessage>, host: &NetworkServiceInner,
     ) -> Result<usize, String>
     {
+        // ensure the node id is unique if specified.
+        if let Some(node_id) = id {
+            if self.node_id_index.contains_key(node_id) {
+                return Err(format!(
+                    "session already exists, nodeId = {:?}",
+                    node_id
+                ));
+            }
+        }
+
+        // validate against node IP policy.
         let ip = address.ip();
-
-        // ensure unique node id and validate against node IP policy
-        match id {
-            Some(node_id) => {
-                if self.egress_status.node_id_index.contains_key(node_id) {
-                    return Err(format!(
-                        "egress session already exists, nodeId = {:?}",
-                        node_id
-                    ));
-                }
-
-                if !self.egress_status.ip_limit.is_ip_allowed(&ip) {
-                    return Err(format!(
-                        "egress IP policy limited, nodeId = {:?}, addr = {:?}",
-                        node_id, address
-                    ));
-                }
-            }
-            None => {
-                if !self.ingress_status.ip_limit.is_ip_allowed(&ip) {
-                    return Err(format!(
-                        "ingress IP policy limited, addr = {:?}",
-                        address
-                    ));
-                }
-            }
+        if !self.ip_limit.is_ip_allowed(&ip) {
+            return Err(format!(
+                "IP policy limited, nodeId = {:?}, addr = {:?}",
+                id, address
+            ));
         }
 
         let index = match self.sessions.vacant_entry() {
@@ -122,55 +97,38 @@ impl SessionManager {
                 {
                     Err(e) => Err(format!("{:?}", e)),
                     Ok(session) => {
-                        let session = Arc::new(RwLock::new(session));
-                        Ok(entry.insert(session).index())
+                        Ok(entry.insert(Arc::new(RwLock::new(session))).index())
                     }
                 }
             }
         }?;
 
-        // update status on creation succeeded
-        match id {
-            Some(node_id) => {
-                self.egress_status
-                    .node_id_index
-                    .insert(node_id.clone(), index);
-                self.egress_status.ip_limit.on_add(ip);
-            }
-            None => {
-                self.ingress_status.ip_limit.on_add(ip);
-            }
+        // update on creation succeeded
+        if let Some(node_id) = id {
+            self.node_id_index.insert(node_id.clone(), index);
         }
+
+        self.ip_limit.on_add(ip);
 
         Ok(index)
     }
 
     pub fn remove(&mut self, session: &Session) {
         if self.sessions.remove(session.token()).is_some() {
-            if session.metadata.originated {
-                self.egress_status
-                    .ip_limit
-                    .on_delete(session.address().ip());
-                self.egress_status.node_id_index.remove(
-                    session.id().expect("egress session should have node id"),
-                );
-            } else {
-                self.ingress_status
-                    .ip_limit
-                    .on_delete(session.address().ip());
-                if let Some(id) = session.id() {
-                    self.ingress_status.node_id_index.remove(id);
-                }
+            if let Some(node_id) = session.id() {
+                self.node_id_index.remove(node_id);
             }
+
+            self.ip_limit.on_delete(session.address().ip());
         }
     }
 
     pub fn update_ingress_node_id(
         &mut self, idx: usize, node_id: &NodeId,
     ) -> Result<(), String> {
-        // ensure the ingress node id is unique
-        if let Some(cur_idx) = self.ingress_status.node_id_index.get(node_id) {
-            return Err(format!("ingress session already exists, node_id = {:?}, cur_idx = {}, new_idx = {}", node_id.clone(), *cur_idx, idx));
+        // ensure the node id is unique
+        if let Some(cur_idx) = self.node_id_index.get(node_id) {
+            return Err(format!("session already exists, node_id = {:?}, cur_idx = {}, new_idx = {}", node_id.clone(), *cur_idx, idx));
         }
 
         if !self.sessions.contains(idx) {
@@ -181,9 +139,7 @@ impl SessionManager {
             ));
         }
 
-        self.ingress_status
-            .node_id_index
-            .insert(node_id.clone(), idx);
+        self.node_id_index.insert(node_id.clone(), idx);
 
         Ok(())
     }
