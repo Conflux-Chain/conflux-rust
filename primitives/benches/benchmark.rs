f34383c95b77e98e10d9e4928554a29d502531ea
peilun-conflux <48905552+peilun-conflux@users.noreply.github.com>
2019-05-14 17:48:40 +0800

    Add benchmark for recover public and verify. (#103)
    * Add benchmark for recover public and verify.

---
+++ primitives/benches/benchmark.rs
@@ -0,0 +1,43 @@
+// Copyright 2019 Conflux Foundation. All rights reserved.
+// Conflux is free software and distributed under GNU General Public License.
+// See http://www.gnu.org/licenses/
+
+use criterion::Criterion;
+use keylib::{KeyPair, sign, recover, verify_public};
+use criterion::{criterion_group, criterion_main};
+use keccak_hash::keccak;
+
+fn recover_benchmark(c: &mut Criterion) {
+    let secret =
+        "46b9e861b63d3509c88b7817275a30d22d62c8cd8fa6486ddee35ef0d8e0495f"
+            .parse()
+            .unwrap();
+    let msg = keccak("0".as_bytes());
+    let kp = KeyPair::from_secret(secret).unwrap();
+    let sig = sign(kp.secret(), &msg).unwrap();
+    c.bench_function("Recover public", move |b| {
+        b.iter(|| {
+            recover(&sig, &msg).unwrap();
+        });
+    });
+}
+
+
+fn verify_benchmark(c: &mut Criterion) {
+    let secret =
+        "46b9e861b63d3509c88b7817275a30d22d62c8cd8fa6486ddee35ef0d8e0495f"
+            .parse()
+            .unwrap();
+    let msg = keccak("0".as_bytes());
+    let kp = KeyPair::from_secret(secret).unwrap();
+    let pub_key = kp.public().clone();
+    let sig = sign(kp.secret(), &msg).unwrap();
+    c.bench_function("Verify public", move |b| {
+        b.iter(|| {
+            verify_public(&pub_key, &sig, &msg).unwrap();
+        });
+    });
+}
+
+criterion_group!(benches, recover_benchmark, verify_benchmark);
+criterion_main!(benches);
