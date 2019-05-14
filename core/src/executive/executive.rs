01bc062df6f357f12367aceb005873af226e6429
Zhe Yang <yz@conflux-chain.org>
2019-05-14 16:59:31 +0200

    Ethereum compatible patch in experiment: chain id.

--- core/src/executive/executive.rs
+++ core/src/executive/executive.rs
@@ -974,7 +974,7 @@ impl<'a, 'b> Executive<'a, 'b> {
         let nonce = self.state.nonce(&sender)?;
 
         let spec = self.spec;
-        let base_gas_required = U256::from(Self::gas_required_for(
+        let mut base_gas_required = U256::from(Self::gas_required_for(
             match tx.action {
                 Action::Create => true,
                 Action::Call(_) => false,
@@ -984,10 +984,14 @@ impl<'a, 'b> Executive<'a, 'b> {
         ));
 
         if tx.gas < base_gas_required {
+            // FIXME: changed to be compatible to eth transactions.
+            /*
             return Err(ExecutionError::NotEnoughBaseGas {
                 required: base_gas_required,
                 got: tx.gas,
             });
+            */
+            base_gas_required = tx.gas;
         }
 
         if !tx.is_unsigned()
@@ -1010,6 +1014,8 @@ impl<'a, 'b> Executive<'a, 'b> {
         // This should never happen because we have checked block gas limit
         // before SyncGraph Validate if transaction fits into give block
         if self.env.gas_used + tx.gas > self.env.gas_limit {
+            // FIXME: make gas limit configurable.
+            debug!("block gas limit reached!");
             return Err(ExecutionError::BlockGasLimitReached {
                 gas_limit: self.env.gas_limit,
                 gas_used: self.env.gas_used,
@@ -1138,9 +1144,15 @@ impl<'a, 'b> Executive<'a, 'b> {
             cmp::min(refunds_bound, (tx.gas - gas_left_prerefund) >> 1);
         let gas_left = gas_left_prerefund + refunded;
 
+        // FIXME: preserve the refunded value.
         let gas_used = tx.gas - gas_left;
-        let refund_value = U256::zero();
-        let fees_value = tx.gas * tx.gas_price;
+
+        // FIXME: do not charge tx fee in eth replay test.
+        let refund_value = tx.gas * tx.gas_price;
+        let fees_value = gas_used * tx.gas_price;
+        // self.env.gas_used += tx.gas;
+        // let refund_value = U256::zero();
+        // let fees_value = tx.gas * tx.gas_price;
 
         trace!("exec::finalize: tx.gas={}, sstore_refunds={}, suicide_refunds={}, refunds_bound={}, gas_left_prerefund={}, refunded={}, gas_left={}, gas_used={}, refund_value={}, fees_value={}\n",
                tx.gas, sstore_refunds, suicide_refunds, refunds_bound, gas_left_prerefund, refunded, gas_left, gas_used, refund_value, fees_value);
--- primitives/src/transaction.rs
+++ primitives/src/transaction.rs
@@ -173,9 +173,27 @@ pub struct Transaction {
     pub data: Bytes,
 }
 
+mod eth_signature {
+    pub fn add_chain_replay_protection(v: u64, chain_id: Option<u64>) -> u64 {
+        v + if let Some(n) = chain_id {
+            35 + n * 2
+        } else {
+            27
+        }
+    }
+}
+
 impl Transaction {
     /// Append object with a without signature into RLP stream
-    pub fn rlp_append_unsigned_transaction(&self, s: &mut RlpStream, chain_id: Option<u64>) {
+    pub fn rlp_append_unsigned_transaction(
+        &self, s: &mut RlpStream, chain_id: Option<u64>,
+    ) {
+        // FIXME: we should think more about the transaction spec as a base to
+        // sign. FIXME: Eth added 3 fields because it tries to be
+        // compatible to v, r, s, FIXME: but it doesn't make much sense.
+        // FIXME: Another aspect to consider is multi-sig account. Maybe we will
+        // need to add FIXME: sender address. And there must be a way to
+        // verify sender address with FIXME: the number of signatures.
         s.begin_list(if chain_id.is_none() { 6 } else { 9 });
         s.append(&self.nonce);
         s.append(&self.gas_price);
@@ -199,7 +217,7 @@ impl Transaction {
     pub fn sign(self, secret: &Secret) -> SignedTransaction {
         let sig = ::keylib::sign(secret, &self.hash(None))
             .expect("data is valid and context has signing capabilities; qed");
-        let tx_with_sig = self.with_signature(sig);
+        let tx_with_sig = self.with_eth_signature(sig, None);
         let public = tx_with_sig
             .recover_public()
             .expect("secret is valid so it's recoverable");
@@ -207,12 +225,17 @@ impl Transaction {
     }
 
     /// Signs the transaction with signature.
-    pub fn with_signature(self, sig: Signature) -> TransactionWithSignature {
+    pub fn with_eth_signature(
+        self, sig: Signature, chain_id: Option<u64>,
+    ) -> TransactionWithSignature {
         TransactionWithSignature {
             unsigned: self,
             r: sig.r().into(),
             s: sig.s().into(),
-            v: sig.v(),
+            v: eth_signature::add_chain_replay_protection(
+                sig.v() as u64,
+                chain_id,
+            ) as u8,
             hash: 0.into(),
             rlp_size: None,
         }
@@ -345,7 +368,9 @@ impl TransactionWithSignature {
 
     /// Construct a signature object from the sig.
     pub fn signature(&self) -> Signature {
-        Signature::from_rsv(&self.r.into(), &self.s.into(), self.v)
+        Signature::from_electrum(
+            &Signature::from_rsv(&self.r.into(), &self.s.into(), self.v)[..],
+        )
     }
 
     /// Checks whether the signature has a low 's' value.
@@ -370,7 +395,10 @@ impl TransactionWithSignature {
 
     /// Recovers the public key of the sender.
     pub fn recover_public(&self) -> Result<Public, keylib::Error> {
-        Ok(recover(&self.signature(), &self.unsigned.hash(self.chain_id()))?)
+        Ok(recover(
+            &self.signature(),
+            &self.unsigned.hash(self.chain_id()),
+        )?)
     }
 
     /// Verify basic signature params. Does not attempt sender recovery.
