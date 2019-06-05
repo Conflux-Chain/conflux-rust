356b64cbb93a4eec012f09c8d9dd07c6059d72a1
boqiu <82121246@qq.com>
2019-06-05 16:18:25 +0800

    add metrics lib

---
+++ util/metrics/src/gauge.rs
@@ -0,0 +1,39 @@
+use crate::is_enabled;
+use prometheus;
+
+pub struct Gauge {
+    inner: Option<prometheus::IntGauge>,
+}
+
+impl Gauge {
+    pub fn register(key: &'static str) -> Self {
+        if !is_enabled() {
+            return Gauge { inner: None };
+        }
+
+        let gauge = prometheus::IntGauge::new(key, " ").unwrap();
+        prometheus::default_registry()
+            .register(Box::new(gauge.clone()))
+            .unwrap();
+
+        Gauge { inner: Some(gauge) }
+    }
+
+    pub fn inc(&self) {
+        if let Some(ref gauge) = self.inner {
+            gauge.inc();
+        }
+    }
+
+    pub fn dec(&self) {
+        if let Some(ref gauge) = self.inner {
+            gauge.dec();
+        }
+    }
+
+    pub fn update(&self, value: i64) {
+        if let Some(ref gauge) = self.inner {
+            gauge.set(value);
+        }
+    }
+}
