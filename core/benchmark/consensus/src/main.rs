c7bc5fa66c7fdfdbdf970d9e2f2d81bf3d4359ad
fanlong <fanl@cs.toronto.edu>
2019-06-05 12:22:08 +0800

    Add some basic comments to consensus/mod.rs (#168)

--- core/benchmark/consensus/src/main.rs
+++ core/benchmark/consensus/src/main.rs
@@ -1,3 +1,7 @@
+// Copyright 2019 Conflux Foundation. All rights reserved.
+// Conflux is free software and distributed under GNU General Public License.
+// See http://www.gnu.org/licenses/
+
 #![allow(unused)]
 use cfx_types::{Address, H256, U256};
 use cfxcore::{
