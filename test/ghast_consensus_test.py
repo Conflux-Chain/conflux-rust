ef0e11adb3d49cf0ed64b88eede21b2856e8fbf4
Fan Long <fanl@csail.mit.edu>
2019-06-08 17:34:54 +0800

    Add testcase

--- test/ghast_consensus_test.py
+++ test/ghast_consensus_test.py
@@ -30,7 +30,8 @@ TEST_INPUT = [
     "partial-invalid-case1.in",
     "partial-invalid-case2.in",
     "adaptive-case1.in",
-    "adaptive-case2.in"]
+    "adaptive-case2.in",
+    "adaptive-case3.in"]
 
 test_dir = os.path.dirname(os.path.realpath(__file__))
 consensus_bench_dir = test_dir + "/../core/benchmark/consensus"
