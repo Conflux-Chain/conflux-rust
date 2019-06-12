09acbed5fed057ec3c80f9f4d54d4a8e6a88eede
boqiu <82121246@qq.com>
2019-06-12 12:39:11 +0800

    fix build error

--- jenkins_ci.sh
+++ jenkins_ci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export PATH=$HOME/.cargo/bin:$PATH
-killall -q -9 conflux || echo "All conflux process killed before build and test ."
+killall -q -9 conflux || echo "All conflux process killed before build and test .aa"
 ./dev-support/test.sh
