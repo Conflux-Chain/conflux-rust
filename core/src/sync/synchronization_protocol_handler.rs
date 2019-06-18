6e185f0512e5ff412c68c2be54389576d36ef9fd
Bo QIU <35757521+boqiu@users.noreply.github.com>
2019-06-18 11:45:08 +0800

    disconnect peer on synchronization failure (#230)
    * disconnect peer for sync failures
    
    * fmt code
    
    * do not disconnect peer when timeout

--- core/src/sync/synchronization_protocol_handler.rs
+++ core/src/sync/synchronization_protocol_handler.rs
@@ -240,7 +240,7 @@ impl SynchronizationProtocolHandler {
             MsgId::GET_TERMINAL_BLOCK_HASHES => {
                 self.on_get_terminal_block_hashes(io, peer, &rlp)
             }
-            MsgId::TRANSACTIONS => self.on_transactions(io, peer, &rlp),
+            MsgId::TRANSACTIONS => self.on_transactions(peer, &rlp),
             MsgId::GET_CMPCT_BLOCKS => {
                 self.on_get_compact_blocks(io, peer, &rlp)
             }
@@ -267,15 +267,70 @@ impl SynchronizationProtocolHandler {
             }
             _ => {
                 warn!("Unknown message: peer={:?} msgid={:?}", peer, msg_id);
+                io.disconnect_peer(peer, Some(UpdateNodeOperation::Remove));
                 Ok(())
             }
         }
-        .unwrap_or_else(|e| {
-            warn!(
-                "Error while handling message msgid={:?}, error={:?}",
-                msg_id, e
-            );
-        });
+        .unwrap_or_else(|e| self.handle_error(io, peer, msg_id, e));
+    }
+
+    /// Error handling for dispatched messages.
+    fn handle_error(
+        &self, io: &NetworkContext, peer: PeerId, msg_id: MsgId, e: Error,
+    ) {
+        warn!(
+            "Error while handling message, peer={}, msgid={:?}, error={:?}",
+            peer, msg_id, e
+        );
+
+        let mut disconnect = true;
+        let mut op = None;
+
+        // NOTE, DO NOT USE WILDCARD IN THE FOLLOWING MATCH STATEMENT!
+        // COMPILER WILL HELP TO FIND UNHANDLED ERROR CASES.
+        match e.0 {
+            ErrorKind::Invalid => op = Some(UpdateNodeOperation::Demotion),
+            ErrorKind::UnknownPeer => op = Some(UpdateNodeOperation::Failure),
+            // TODO handle the unexpected response case (timeout or real invalid message type)
+            ErrorKind::UnexpectedResponse => disconnect = false,
+            ErrorKind::TooManyTrans => {}
+            ErrorKind::Decoder(_) => op = Some(UpdateNodeOperation::Remove),
+            ErrorKind::Network(kind) => match kind {
+                network::ErrorKind::AddressParse => disconnect = false,
+                network::ErrorKind::AddressResolve(_) => disconnect = false,
+                network::ErrorKind::Auth => disconnect = false,
+                network::ErrorKind::BadProtocol => {
+                    op = Some(UpdateNodeOperation::Remove)
+                }
+                network::ErrorKind::BadAddr => disconnect = false,
+                network::ErrorKind::Decoder => {
+                    op = Some(UpdateNodeOperation::Remove)
+                }
+                network::ErrorKind::Expired => disconnect = false,
+                network::ErrorKind::Disconnect(_) => disconnect = false,
+                network::ErrorKind::InvalidNodeId => disconnect = false,
+                network::ErrorKind::OversizedPacket => disconnect = false,
+                network::ErrorKind::Io(_) => disconnect = false,
+                network::ErrorKind::Throttling(_) => disconnect = false,
+                network::ErrorKind::SocketIo(_) => {
+                    op = Some(UpdateNodeOperation::Failure)
+                }
+                network::ErrorKind::Msg(_) => {
+                    op = Some(UpdateNodeOperation::Failure)
+                }
+                network::ErrorKind::__Nonexhaustive {} => {
+                    op = Some(UpdateNodeOperation::Failure)
+                }
+            },
+            ErrorKind::Msg(_) => op = Some(UpdateNodeOperation::Failure),
+            ErrorKind::__Nonexhaustive {} => {
+                op = Some(UpdateNodeOperation::Failure)
+            }
+        }
+
+        if disconnect {
+            io.disconnect_peer(peer, op);
+        }
     }
 
     fn on_get_compact_blocks(
@@ -533,8 +588,7 @@ impl SynchronizationProtocolHandler {
             }
         };
         if should_disconnect {
-            io.disconnect_peer(peer, None);
-            return Err(ErrorKind::TooManyTrans.into());
+            bail!(ErrorKind::TooManyTrans);
         }
         self.request_manager.request_transactions(
             io,
@@ -713,9 +767,7 @@ impl SynchronizationProtocolHandler {
         Ok(())
     }
 
-    fn on_transactions(
-        &self, io: &NetworkContext, peer: PeerId, rlp: &Rlp,
-    ) -> Result<(), Error> {
+    fn on_transactions(&self, peer: PeerId, rlp: &Rlp) -> Result<(), Error> {
         let transactions = rlp.as_val::<Transactions>()?;
         let transactions = transactions.transactions;
         debug!(
@@ -745,8 +797,7 @@ impl SynchronizationProtocolHandler {
         };
 
         if should_disconnect {
-            io.disconnect_peer(peer, None);
-            return Err(ErrorKind::TooManyTrans.into());
+            bail!(ErrorKind::TooManyTrans);
         }
 
         let (signed_trans, _) =
