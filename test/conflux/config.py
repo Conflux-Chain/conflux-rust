a925a88edd4b67dd6f8a65b240b3e3984b576d20
Zhe Yang <yz@conflux-chain.org>
2019-06-20 00:45:19 +0200

    python changes.

--- test/conflux/config.py
+++ test/conflux/config.py
@@ -1,3 +1,4 @@
+from . import trie
 from eth_utils import decode_hex
 
 default_config = dict(
@@ -6,7 +7,7 @@ default_config = dict(
     GENESIS_COINBASE=b'\x00' * 20,
     GENESIS_PRI_KEY=decode_hex("46b9e861b63d3509c88b7817275a30d22d62c8cd8fa6486ddee35ef0d8e0495f"),
     TOTAL_COIN= 5 * 10**9 * 10**18 * 10**6,
-    GENESIS_STATE_ROOT=decode_hex("0xb11554381a6ee59a94e33efcd7288e09f332cf4bff51e9b6ce8a5b194ba55b92"),
+    GENESIS_STATE_ROOT=trie.state_root(delta_root=decode_hex("0xb11554381a6ee59a94e33efcd7288e09f332cf4bff51e9b6ce8a5b194ba55b92")),
     GENESIS_RECEIPTS_ROOT=decode_hex("0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347"),
     GENESIS_AUTHOR=decode_hex("0x0000000000000000000000000000000000000006"),
 )
