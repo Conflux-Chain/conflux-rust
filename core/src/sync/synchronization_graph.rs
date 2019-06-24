df7b1ec1a3b2bbd525a057d77c2c17012101572a
sparkmiw <spark_ren@hotmail.com>
2019-06-24 16:04:09 +0800

    WIP: handle cross-checkpoint nodes in sync graph (#254)
    * WIP: handle cross-checkpoint nodes in sync graph
    
    * Merge branch 'master' into spark
    
    * Merge branch 'master' into spark
    
    * fix

--- core/src/sync/synchronization_graph.rs
+++ core/src/sync/synchronization_graph.rs
@@ -72,6 +72,8 @@ pub struct SynchronizationGraphNode {
     pub graph_status: u8,
     /// Whether the block body is ready.
     pub block_ready: bool,
+    /// Wether the parent or uncles of the block are older than checkpoint.
+    pub parent_referees_too_old: bool,
     /// The index of the parent of the block.
     pub parent: usize,
     /// The indices of the children of the block.
@@ -143,6 +145,7 @@ impl SynchronizationGraphInner {
         let me = self.arena.insert(SynchronizationGraphNode {
             graph_status: BLOCK_INVALID,
             block_ready: false,
+            parent_referees_too_old: false,
             parent: NULL,
             children: Vec::new(),
             referees: Vec::new(),
@@ -197,6 +200,7 @@ impl SynchronizationGraphInner {
                 BLOCK_HEADER_ONLY
             },
             block_ready: *header.parent_hash() == H256::default(),
+            parent_referees_too_old: false,
             parent: NULL,
             children: Vec::new(),
             referees: Vec::new(),
@@ -258,10 +262,42 @@ impl SynchronizationGraphInner {
         me
     }
 
+    pub fn block_older_than_checkpoint(&self, _hash: &H256) -> bool { false }
+
+    pub fn check_parent_referees_too_old(
+        &mut self, me: usize, header: Arc<BlockHeader>,
+    ) -> bool {
+        let mut too_old = false;
+
+        if self.block_older_than_checkpoint(header.parent_hash()) {
+            too_old = true;
+        }
+
+        // FIXME: not sure the correct way to handle here.
+        /*
+        for referee in header.referee_hashes() {
+            if self.block_older_than_checkpoint(referee) {
+                assert!(self.arena[me].pending_referee_count > 0);
+                self.arena[me].pending_referee_count -= 1;
+            }
+        }
+        */
+
+        if too_old {
+            self.arena[me].parent_referees_too_old = true;
+        }
+
+        too_old
+    }
+
     pub fn new_to_be_header_parental_tree_ready(&self, index: usize) -> bool {
         let ref node_me = self.arena[index];
         if node_me.graph_status >= BLOCK_HEADER_PARENTAL_TREE_READY {
             return false;
+        }
+
+        if node_me.parent_referees_too_old {
+            return true;
         }
 
         let parent = node_me.parent;
@@ -278,6 +314,17 @@ impl SynchronizationGraphInner {
 
         if node_me.pending_referee_count > 0 {
             return false;
+        }
+
+        //FIXME: revisit this logic later.
+        if node_me.parent_referees_too_old {
+            if node_me.referrers.is_empty() && node_me.children.is_empty() {
+                return false;
+            } else {
+                return !node_me.referees.iter().any(|&referee| {
+                    self.arena[referee].graph_status < BLOCK_HEADER_GRAPH_READY
+                });
+            }
         }
 
         let parent = node_me.parent;
@@ -619,18 +666,18 @@ impl SynchronizationGraph {
 
             if let Some(mut block) = self.block_by_hash_from_db(&hash) {
                 // This is for constructing synchronization graph.
-                let res = self.insert_block_header(
+                let (success, _, is_old) = self.insert_block_header(
                     &mut block.block_header,
                     true,
                     false,
                 );
-                assert!(res.0);
+                assert!(success);
 
                 let parent = block.block_header.parent_hash().clone();
                 let referees = block.block_header.referee_hashes().clone();
 
                 // This is necessary to construct consensus graph.
-                self.insert_block(block, true, false, false);
+                self.insert_block(block, !is_old, false, false);
 
                 if !self.contains_block(&parent)
                     && !visited_blocks.contains(&parent)
@@ -689,12 +736,12 @@ impl SynchronizationGraph {
 
             if let Some(mut block) = self.block_by_hash_from_db(&hash) {
                 // This is for constructing synchronization graph.
-                let res = self.insert_block_header(
+                let (success, _, is_old) = self.insert_block_header(
                     &mut block.block_header,
                     true,
                     false,
                 );
-                assert!(res.0);
+                assert!(success);
 
                 let parent = block.block_header.parent_hash().clone();
                 let referees = block.block_header.referee_hashes().clone();
@@ -703,7 +750,7 @@ impl SynchronizationGraph {
                 // TODO possible by inserting blocks in topological order
                 // TODO Read only headers from db
                 // This is necessary to construct consensus graph.
-                self.insert_block(block, true, false, true);
+                self.insert_block(block, !is_old, false, true);
 
                 if !self.contains_block(&parent)
                     && !visited_blocks.contains(&parent)
@@ -886,21 +933,22 @@ impl SynchronizationGraph {
 
     pub fn insert_block_header(
         &self, header: &mut BlockHeader, need_to_verify: bool, bench_mode: bool,
-    ) -> (bool, Vec<H256>) {
+    ) -> (bool, Vec<H256>, bool) {
+        let mut me_is_old = false;
         let mut inner = self.inner.write();
         let hash = header.hash();
 
         if self.verified_invalid(&hash) {
-            return (false, Vec::new());
+            return (false, Vec::new(), me_is_old);
         }
 
-        if inner.indices.contains_key(&hash) {
+        if let Some(me) = inner.indices.get(&hash) {
             if need_to_verify {
                 // Compute pow_quality, because the input header may be used as
                 // a part of block later
                 VerificationConfig::compute_header_pow_quality(header);
             }
-            return (true, Vec::new());
+            return (true, Vec::new(), inner.arena[*me].parent_referees_too_old);
         }
 
         let verification_passed = if need_to_verify {
@@ -926,6 +974,8 @@ impl SynchronizationGraph {
         };
         debug!("insert_block_header() Block = {}, index = {}, need_to_verify = {}, bench_mode = {}",
                header.hash(), me, need_to_verify, bench_mode);
+
+        me_is_old = inner.check_parent_referees_too_old(me, header_arc.clone());
 
         // Start to pass influence to descendants
         let mut need_to_relay: Vec<H256> = Vec::new();
@@ -973,30 +1023,31 @@ impl SynchronizationGraph {
                         );
                         continue;
                     }
-                    // Passed verification on header_arc.
-                    if inner.arena[index].block_ready {
-                        need_to_relay
-                            .push(inner.arena[index].block_header.hash());
-                    }
 
+                    // Passed verification on header_arc.
                     let parent = inner.arena[index].parent;
                     inner.ancestor_tree.make_tree(index);
                     inner.ancestor_tree.link(parent, index);
                     inner.collect_blockset_in_own_view_of_epoch(index);
 
+                    if inner.arena[index].block_ready {
+                        need_to_relay
+                            .push(inner.arena[index].block_header.hash());
+                    }
+
                     for child in &inner.arena[index].children {
-                        debug_assert!(
-                            inner.arena[*child].graph_status
-                                < BLOCK_HEADER_GRAPH_READY
-                        );
-                        queue.push_back(*child);
+                        if inner.arena[*child].graph_status
+                            < BLOCK_HEADER_GRAPH_READY
+                        {
+                            queue.push_back(*child);
+                        }
                     }
                     for referrer in &inner.arena[index].referrers {
-                        debug_assert!(
-                            inner.arena[*referrer].graph_status
-                                < BLOCK_HEADER_GRAPH_READY
-                        );
-                        queue.push_back(*referrer);
+                        if inner.arena[*referrer].graph_status
+                            < BLOCK_HEADER_GRAPH_READY
+                        {
+                            queue.push_back(*referrer);
+                        }
                     }
                 } else if inner.new_to_be_header_parental_tree_ready(index) {
                     inner.arena[index].graph_status =
@@ -1009,6 +1060,26 @@ impl SynchronizationGraph {
                         queue.push_back(*child);
                     }
                 }
+
+                let index_parent = inner.arena[index].parent;
+                if index_parent != NULL {
+                    if inner.arena[index_parent].parent_referees_too_old
+                        && inner.arena[index_parent].graph_status
+                            < BLOCK_HEADER_GRAPH_READY
+                    {
+                        queue.push_back(index_parent);
+                    }
+                }
+
+                for referee in inner.arena[index].referees.iter() {
+                    if inner.arena[*referee].parent_referees_too_old
+                        && inner.arena[*referee].graph_status
+                            < BLOCK_HEADER_GRAPH_READY
+                    {
+                        queue.push_back(*referee);
+                    }
+                }
+
                 // Note that we have to insert it here immediately instead of
                 // after the loop because its children may
                 // become ready and being processed in the loop later. It
@@ -1027,10 +1098,10 @@ impl SynchronizationGraph {
         self.process_invalid_blocks(inner.deref_mut(), &invalid_set);
 
         if me_invalid {
-            return (false, need_to_relay);
+            return (false, need_to_relay, me_is_old);
         }
 
-        (true, need_to_relay)
+        (true, need_to_relay, me_is_old)
     }
 
     pub fn contains_block(&self, hash: &H256) -> bool {
