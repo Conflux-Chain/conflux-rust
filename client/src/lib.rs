91371f138890832ce36c9381ec524b04aa3e1651
peilun-conflux <48905552+peilun-conflux@users.noreply.github.com>
2019-06-29 18:20:10 +0800

    Update Version. (#277)

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -60,7 +60,7 @@ use txgen::{
 /// Used in Genesis author to indicate testnet version
 /// Increase by one for every test net reset
 const TESTNET_VERSION: &'static str =
-    "0000000000000000000000000000000000000006";
+    "0000000000000000000000000000000000000007";
 
 pub struct ClientHandle {
     pub debug_rpc_http_server: Option<HttpServer>,
