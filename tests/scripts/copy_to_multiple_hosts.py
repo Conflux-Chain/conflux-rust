d86dff52134e974038f68f07b7d705b056fa9a76
ypliu <resodo.liu@gmail.com>
2019-07-02 22:20:58 +0800

    WIP: eth replay test

---
+++ tests/scripts/copy_to_multiple_hosts.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os
+
+file_name = "ip_list.txt"
+local_path = ""
+remote_path = ""
+
+ips = []
+with open(file_name) as fp:
+    line = fp.readline()
+    while line:
+        ips.append(line.strip().strip(','))
+        line = fp.readline()
+
+for ip in ips:
+    cmd = "ssh-copy-id -f -o StrictHostKeyChecking=no -i pubkey.pub ubuntu@{}".format(ip)
+    print(cmd)
+    os.system(cmd)
