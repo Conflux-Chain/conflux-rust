a86f1dc989b20b20e3789bda8c26eaa9d2a7f6d2
fanlong <fanl@cs.toronto.edu>
2019-07-04 21:20:17 +0800

    Remove index increasing assumption from AnticoneCache (#306)
    * Remove index increasing assumption from AnticoneCache
    
    * Remove incorrect Slab usage

--- core/src/consensus/anticone_cache.rs
+++ core/src/consensus/anticone_cache.rs
@@ -7,20 +7,26 @@ use std::{
 const CACHE_INDEX_STRIDE: usize = 1000;
 const MAX_ANTICONE_SIZE: usize = 300;
 
+/// AnticoneCache keeps only the anticone set of the recent CACHE_INDEX_STRIDE
+/// blocks. It also removes a block anticone set from it if the set is larger
+/// than MAX_ANTICONE_SIZE
 pub struct AnticoneCache {
     max_seen_index: usize,
-    data: HashMap<usize, HashSet<usize>>,
+    seq_number: u64,
+    data: HashMap<usize, (HashSet<usize>, u64)>,
 }
 
 impl AnticoneCache {
     pub fn new() -> Self {
         Self {
             max_seen_index: 0,
+            seq_number: 0,
             data: HashMap::new(),
         }
     }
 
     pub fn update(&mut self, me: usize, anticone: &BitSet) {
+        self.seq_number += 1;
         self.max_seen_index = max(self.max_seen_index, me);
         // BitSet does not have len() method
         if anticone.len() < MAX_ANTICONE_SIZE {
@@ -28,14 +34,14 @@ impl AnticoneCache {
             for index in anticone.iter() {
                 tmp.insert(index as usize);
             }
-            self.data.insert(me, tmp);
+            self.data.insert(me, (tmp, self.seq_number));
         }
 
         if anticone.len() < self.data.len() {
             for index in anticone.iter() {
                 let index_usize = index as usize;
                 if self.data.contains_key(&index_usize) {
-                    let s = self.data.get_mut(&index_usize).unwrap();
+                    let s = &mut self.data.get_mut(&index_usize).unwrap().0;
                     s.insert(me);
                     if s.len() > MAX_ANTICONE_SIZE {
                         self.data.remove(&index_usize);
@@ -43,23 +49,28 @@ impl AnticoneCache {
                 }
             }
             if self.data.len() > 2 * CACHE_INDEX_STRIDE {
-                let max_seen_index = self.max_seen_index;
-                self.data
-                    .retain(|k, _| (max_seen_index - *k <= CACHE_INDEX_STRIDE));
+                let seq_number = self.seq_number;
+                self.data.retain(|_, (_, k)| {
+                    (seq_number - *k <= CACHE_INDEX_STRIDE as u64)
+                });
             }
         } else {
-            let max_seen_index = self.max_seen_index;
+            let seq_number = self.seq_number;
             self.data.retain(|k, v| {
                 if anticone.contains(*k as u32) {
-                    v.insert(me);
+                    v.0.insert(me);
                 }
-                (v.len() <= MAX_ANTICONE_SIZE)
-                    && (max_seen_index - *k <= CACHE_INDEX_STRIDE)
+                (v.0.len() <= MAX_ANTICONE_SIZE)
+                    && (seq_number - v.1 <= CACHE_INDEX_STRIDE as u64)
             });
         }
     }
 
     pub fn get(&self, me: usize) -> Option<&HashSet<usize>> {
-        self.data.get(&me)
+        if let Some(v) = self.data.get(&me) {
+            Some(&v.0)
+        } else {
+            None
+        }
     }
 }
