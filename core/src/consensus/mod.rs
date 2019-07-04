41e7d16e884a63eb549aaf7da15fb8fcbe17f586
Yanpei LIU <resodo.liu@gmail.com>
2019-07-04 21:11:04 +0800

    Log (#304)
    * test: make load_chain_test more automatic
    
    * fix
    
    * Merge remote-tracking branch 'upstream/master'
    
    * Merge remote-tracking branch 'upstream/master'
    
    * Merge remote-tracking branch 'upstream/master'
    
    * Merge remote-tracking branch 'upstream/master'
    
    * Merge remote-tracking branch 'upstream/master'
    
    * Merge remote-tracking branch 'upstream/master'
    
    * Merge remote-tracking branch 'upstream/master'
    
    * Merge remote-tracking branch 'upstream/master'
    
    * Merge remote-tracking branch 'upstream/master'
    
    * Merge remote-tracking branch 'upstream/master'
    
    * log

--- core/src/consensus/mod.rs
+++ core/src/consensus/mod.rs
@@ -3629,6 +3629,8 @@ impl ConsensusGraph {
                                     .map(move |(i, log)| LocalizedLogEntry {
                                         entry: log,
                                         block_hash: *hash,
+                                        // TODO
+                                        block_number: 0,
                                         transaction_hash: tx_hash,
                                         // iterating in reverse order
                                         transaction_index: receipts_len - index - 1,
