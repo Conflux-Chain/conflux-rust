5be18cb230ba96756c7845b12cbabbf2bbf347cf
Xi Lin <zimpha@gmail.com>
2019-07-05 16:58:48 +0800

    revert synchronization_graph.rs

--- core/src/sync/synchronization_graph.rs
+++ core/src/sync/synchronization_graph.rs
@@ -659,8 +659,7 @@ impl SynchronizationGraphInner {
 
     fn set_and_propagate_invalid(
         &mut self, queue: &mut VecDeque<usize>,
-        invalid_set: &mut HashSet<usize>, visited: &mut HashSet<usize>,
-        index: usize,
+        invalid_set: &mut HashSet<usize>, index: usize,
     )
     {
         let children =
@@ -668,10 +667,7 @@ impl SynchronizationGraphInner {
         for child in &children {
             if !invalid_set.contains(&child) {
                 self.arena[*child].graph_status = BLOCK_INVALID;
-                if !visited.contains(&child) {
-                    visited.insert(*child);
-                    queue.push_back(*child);
-                }
+                queue.push_back(*child);
                 invalid_set.insert(*child);
             }
         }
@@ -681,10 +677,7 @@ impl SynchronizationGraphInner {
         for referrer in &referrers {
             if !invalid_set.contains(&referrer) {
                 self.arena[*referrer].graph_status = BLOCK_INVALID;
-                if !visited.contains(&referrer) {
-                    visited.insert(*referrer);
-                    queue.push_back(*referrer);
-                }
+                queue.push_back(*referrer);
                 invalid_set.insert(*referrer);
             }
         }
@@ -1016,9 +1009,7 @@ impl SynchronizationGraph {
         let mut need_to_relay: Vec<H256> = Vec::new();
         let mut me_invalid = false;
         let mut invalid_set: HashSet<usize> = HashSet::new();
-        let mut visited = HashSet::new();
         let mut queue = VecDeque::new();
-        visited.insert(me);
         queue.push_back(me);
         while let Some(index) = queue.pop_front() {
             if inner.arena[index].graph_status == BLOCK_INVALID {
@@ -1029,7 +1020,6 @@ impl SynchronizationGraph {
                 inner.set_and_propagate_invalid(
                     &mut queue,
                     &mut invalid_set,
-                    &mut visited,
                     index,
                 );
             } else {
@@ -1058,7 +1048,6 @@ impl SynchronizationGraph {
                         inner.set_and_propagate_invalid(
                             &mut queue,
                             &mut invalid_set,
-                            &mut visited,
                             index,
                         );
                         continue;
@@ -1073,18 +1062,14 @@ impl SynchronizationGraph {
                     for child in &inner.arena[index].children {
                         if inner.arena[*child].graph_status
                             < BLOCK_HEADER_GRAPH_READY
-                            && !visited.contains(child)
                         {
-                            visited.insert(*child);
                             queue.push_back(*child);
                         }
                     }
                     for referrer in &inner.arena[index].referrers {
                         if inner.arena[*referrer].graph_status
                             < BLOCK_HEADER_GRAPH_READY
-                            && !visited.contains(referrer)
                         {
-                            visited.insert(*referrer);
                             queue.push_back(*referrer);
                         }
                     }
@@ -1096,14 +1081,11 @@ impl SynchronizationGraph {
                         .unwrap()
                         .as_secs();
                     for child in &inner.arena[index].children {
-                        if !visited.contains(child) {
-                            debug_assert!(
-                                inner.arena[*child].graph_status
-                                    < BLOCK_HEADER_PARENTAL_TREE_READY
-                            );
-                            visited.insert(*child);
-                            queue.push_back(*child);
-                        }
+                        debug_assert!(
+                            inner.arena[*child].graph_status
+                                < BLOCK_HEADER_PARENTAL_TREE_READY
+                        );
+                        queue.push_back(*child);
                     }
                 }
 
@@ -1201,9 +1183,7 @@ impl SynchronizationGraph {
         }
 
         let mut invalid_set: HashSet<usize> = HashSet::new();
-        let mut visited = HashSet::new();
         let mut queue = VecDeque::new();
-        visited.insert(me);
         queue.push_back(me);
 
         let block = Arc::new(block);
@@ -1228,7 +1208,6 @@ impl SynchronizationGraph {
                 inner.set_and_propagate_invalid(
                     &mut queue,
                     &mut invalid_set,
-                    &mut visited,
                     index,
                 );
             } else if inner.new_to_be_block_graph_ready(index) {
@@ -1250,24 +1229,16 @@ impl SynchronizationGraph {
                 }
 
                 for child in &inner.arena[index].children {
-                    if !visited.contains(child) {
-                        debug_assert!(
-                            inner.arena[*child].graph_status
-                                < BLOCK_GRAPH_READY
-                        );
-                        visited.insert(*child);
-                        queue.push_back(*child);
-                    }
+                    debug_assert!(
+                        inner.arena[*child].graph_status < BLOCK_GRAPH_READY
+                    );
+                    queue.push_back(*child);
                 }
                 for referrer in &inner.arena[index].referrers {
-                    if !visited.contains(referrer) {
-                        debug_assert!(
-                            inner.arena[*referrer].graph_status
-                                < BLOCK_GRAPH_READY
-                        );
-                        visited.insert(*referrer);
-                        queue.push_back(*referrer);
-                    }
+                    debug_assert!(
+                        inner.arena[*referrer].graph_status < BLOCK_GRAPH_READY
+                    );
+                    queue.push_back(*referrer);
                 }
             }
         }
@@ -1365,11 +1336,10 @@ impl SynchronizationGraph {
 
         let mut queue = VecDeque::new();
         let mut expire_set = HashSet::new();
-        let mut visited = HashSet::new();
         for index in &inner.not_ready_block_indices {
             if !indices_with_referees.contains(index) {
                 queue.push_back(*index);
-                visited.insert(*index);
+                expire_set.insert(*index);
             }
         }
 
@@ -1382,13 +1352,19 @@ impl SynchronizationGraph {
                 || now - inner.arena[index].timestamp > expire_time
             {
                 inner.arena[index].graph_status = BLOCK_INVALID;
-                expire_set.insert(index);
                 inner.set_and_propagate_invalid(
                     &mut queue,
                     &mut expire_set,
-                    &mut visited,
                     index,
                 );
+            } else {
+                // `expired_set` is used as `visited` in the iteration, and
+                // should only contain invalid blocks in the
+                // end. A block is visited but valid only if it
+                // is inserted at the start as blocks with no
+                // incoming edges, so it's okay to remove them during the
+                // iteration.
+                expire_set.remove(&index);
             }
         }
 
