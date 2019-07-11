9a5d4cd5df30ed0602688edcd0ed906be5e7eb1c
Peilun Li <peilun.li@conflux-chain.org>
2019-07-11 17:37:05 +0800

    Update version to v0.1.4.

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -60,7 +60,7 @@ use txgen::{
 /// Used in Genesis author to indicate testnet version
 /// Increase by one for every test net reset
 const TESTNET_VERSION: &'static str =
-    "0000000000000000000000000000000000000008";
+    "0000000000000000000000000000000000000009";
 
 pub struct ClientHandle {
     pub debug_rpc_http_server: Option<HttpServer>,
