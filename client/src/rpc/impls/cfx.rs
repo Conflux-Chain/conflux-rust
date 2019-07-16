d21e21e1ec1d4c6363ab347c8c6bf9f743bb0099
FightingManGod <yilin.han.10@gmail.com>
2019-07-16 16:53:06 +0800

    exclude genesis block timestamp

--- client/src/rpc/impls/cfx.rs
+++ client/src/rpc/impls/cfx.rs
@@ -332,7 +332,7 @@ impl RpcImpl {
                 self.consensus.data_man.block_by_hash(key, false)
             {
                 let timestamp = block.block_header.timestamp();
-                if timestamp < min {
+                if timestamp < min  && timstamp >0{
                     min = timestamp;
                 }
                 if timestamp > max {
