c6c01fb9b014792eae5c154c0eebeb21c7954e00
ypliu <resodo.liu@gmail.com>
2019-07-23 16:23:29 +0800

    improve scripts: remove init blocks and txs

--- tests/conflux_eth_replay_benchmark.py
+++ tests/conflux_eth_replay_benchmark.py
@@ -282,51 +282,56 @@ class ConfluxEthReplayTest(ConfluxTestFramework):
         """
 
         total = 4000000
+        expected_elapsed_time = 0
+
         for txs, count in RlpIter(f, tx_batch_size):
             if tx_count > total:
                 break
 
+            elapsed_time = (datetime.datetime.now() - start_time).total_seconds()
+            speed_diff = expected_elapsed_time - elapsed_time
+            if speed_diff > 0:
+                time.sleep(speed_diff)
+
             peers_to_send = range(0, self.num_nodes)
-
             txs_rlp = rlp.codec.length_prefix(len(txs), 192) + txs
-
             for peer_to_send in peers_to_send:
                 self.nodes[peer_to_send].p2p.send_protocol_packet(int_to_bytes(TRANSACTIONS) + txs_rlp)
-            elapsed_time = (datetime.datetime.now() - start_time).total_seconds()
 
-            if tx_count < ConfluxEthReplayTest.INITIALIZE_TXS:
-                expected_elapsed_time = 1.0 * tx_count / ConfluxEthReplayTest.INITIALIZE_TPS
-            else:
-                tx_bytes += len(txs)
-                expected_elapsed_time = \
-                    tx_received_slowdown \
-                    + 1.0 * ConfluxEthReplayTest.INITIALIZE_TXS / ConfluxEthReplayTest.INITIALIZE_TPS \
-                    + ConfluxEthReplayTest.INITIALIZE_SLEEP \
-                    + 1.0 * tx_bytes / ConfluxEthReplayTest.EXPECTED_TX_SIZE_PER_SEC
-            speed_diff = expected_elapsed_time - elapsed_time
+            tx_bytes += len(txs)
+            expected_elapsed_time = tx_received_slowdown + 1.0 * tx_bytes / ConfluxEthReplayTest.EXPECTED_TX_SIZE_PER_SEC
+
             if int(elapsed_time - last_log_elapsed_time) >= 1:
                 txpool_status = self.nodes[0].txpool_status()
                 txpool_received = txpool_status["received"]
-
                 last_log_elapsed_time = elapsed_time
+
                 self.log.info("elapsed %ss,\t sent %s/%s(%s%%) txs", elapsed_time, tx_count, total,
                               tx_count * 100.0 / total)
 
+                final_slow_down = 0
+
                 if txpool_received + 50000 < tx_count:
-                    should_sleep = elapsed_time * (tx_count - txpool_received) / txpool_received + 1
-                    tx_received_slowdown += should_sleep
+                    if txpool_received == 0:
+                        should_sleep = 1
+                    else:
+                        should_sleep = elapsed_time * (tx_count - txpool_received) / txpool_received + 1
+                    final_slow_down = max(final_slow_down, should_sleep)
                     self.log.info("Conflux full node is slow by %s at receiving txs, slow down by %s.",
                                   tx_count - txpool_received, should_sleep)
+
                 txpool_unpacked = txpool_status["unpacked"]
                 unpacked_limit = 300000
                 if txpool_unpacked > unpacked_limit:
-                    should_sleep = elapsed_time * (txpool_unpacked - unpacked_limit) / tx_count + 1
-                    # should_sleep = 1
-                    tx_received_slowdown += should_sleep
+                    if txpool_received - txpool_unpacked == 0:
+                        should_sleep = 1
+                    else:
+                        should_sleep = elapsed_time * (txpool_unpacked - unpacked_limit) / (
+                                    txpool_received - txpool_unpacked) + 1
+                    final_slow_down = max(final_slow_down, should_sleep)
                     self.log.info("Conflux full node has too many unpacked txs %s. sleep %s", txpool_unpacked,
                                   should_sleep)
-            if speed_diff >= 1:
-                time.sleep(speed_diff)
+                tx_received_slowdown += final_slow_down
 
             tx_count += count
 
@@ -344,9 +349,9 @@ class ConfluxEthReplayTest(ConfluxTestFramework):
 
 
 class BlockGenThread(threading.Thread):
-    BLOCK_FREQ = 0.25
-    BLOCK_TX_LIMIT = 3000
-    BLOCK_SIZE_LIMIT = 300000
+    BLOCK_FREQ = 5
+    BLOCK_TX_LIMIT = 60000
+    BLOCK_SIZE_LIMIT = 6000000
     # Seems to be 90bytes + artificial 128b
     # SIMPLE_TX_PER_BLOCK = 700
     SIMPLE_TX_PER_BLOCK = 0
@@ -366,35 +371,37 @@ class BlockGenThread(threading.Thread):
 
     def run(self):
         self.log.info("block gen thread started to run")
-        start_time = datetime.datetime.now()
-        pre_generated_blocks = math.ceil(1.0 * ConfluxEthReplayTest.INITIALIZE_TXS / BlockGenThread.BLOCK_TX_LIMIT)
-        for i in range(0, pre_generated_blocks):
-            if self.stopped:
-                return
-            sleep_sec = 1.0 * i * ConfluxEthReplayTest.INITIALIZE_SLEEP / 2 / pre_generated_blocks + 1.0 * i - (
-                    datetime.datetime.now() - start_time).total_seconds()
-            self.log.info("%s sleep %s at test startup", self.node_id, sleep_sec)
-            if sleep_sec > 0:
-                time.sleep(sleep_sec)
-            if self.node_id == 0:
-                h = self.node.generateoneblock(BlockGenThread.BLOCK_TX_LIMIT, BlockGenThread.BLOCK_SIZE_LIMIT * 10)
-                self.log.info("node %s generated block at test start %s", self.node_id, h)
+        # start_time = datetime.datetime.now()
+        # pre_generated_blocks = math.ceil(1.0 * ConfluxEthReplayTest.INITIALIZE_TXS / BlockGenThread.BLOCK_TX_LIMIT)
+        # for i in range(0, pre_generated_blocks):
+        #     if self.stopped:
+        #         return
+        #     sleep_sec = 1.0 * i * ConfluxEthReplayTest.INITIALIZE_SLEEP / 2 / pre_generated_blocks + 1.0 * i - (
+        #             datetime.datetime.now() - start_time).total_seconds()
+        #     self.log.info("%s sleep %s at test startup", self.node_id, sleep_sec)
+        #     if sleep_sec > 0:
+        #         time.sleep(sleep_sec)
+        #     if self.node_id == 0:
+        #         h = self.node.generateoneblock(BlockGenThread.BLOCK_TX_LIMIT, BlockGenThread.BLOCK_SIZE_LIMIT * 10)
+        #         self.log.info("node %s generated block at test start %s", self.node_id, h)
         # for blocks to propogate.
-        time.sleep(ConfluxEthReplayTest.INITIALIZE_SLEEP / 2)
+        # time.sleep(ConfluxEthReplayTest.INITIALIZE_SLEEP / 2)
 
         start_time = datetime.datetime.now()
         total_mining_sec = 0.0
         while not self.stopped:
             try:
-                mining = BlockGenThread.BLOCK_FREQ * numpy.random.exponential() / self.hashpower_percent
-                # self.log.info("%s sleep %s sec then generate block", self.node_id, mining)
-                total_mining_sec += mining
                 elapsed_sec = (datetime.datetime.now() - start_time).total_seconds()
                 sleep_sec = total_mining_sec - elapsed_sec
                 self.log.info("%s elapsed time %s, total mining time %s sec, actually sleep %s sec",
                               self.node_id, elapsed_sec, total_mining_sec, sleep_sec)
                 if sleep_sec > 0:
                     time.sleep(sleep_sec)
+
+                mining = BlockGenThread.BLOCK_FREQ * numpy.random.exponential() / self.hashpower_percent
+                # self.log.info("%s sleep %s sec then generate block", self.node_id, mining)
+                total_mining_sec += mining
+
                 # TODO: open the flag
                 """ if False:
                     # Use getblockcount to compare with number of generated blocks to compare with expectation, 
