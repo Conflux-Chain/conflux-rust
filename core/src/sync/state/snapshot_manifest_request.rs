97dc018e50fad297a619c9387b60d721558e7065
boqiu <82121246@qq.com>
2019-07-23 11:44:08 +0800

    add missed license declaration

--- core/src/sync/state/snapshot_manifest_request.rs
+++ core/src/sync/state/snapshot_manifest_request.rs
@@ -1,3 +1,7 @@
+// Copyright 2019 Conflux Foundation. All rights reserved.
+// Conflux is free software and distributed under GNU General Public License.
+// See http://www.gnu.org/licenses/
+
 use crate::sync::{
     message::{Message, MsgId},
     msg_sender::send_message,
