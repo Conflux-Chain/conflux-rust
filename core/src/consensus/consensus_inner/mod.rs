d12915cc1d2125a756520665920e16d5cdeaf420
Fan Long <fanl@Fans-New-MacBook-Pro.local>
2019-07-31 21:01:47 +0800

    Fix confirmation meter for checkpoints

--- core/src/consensus/consensus_inner/mod.rs
+++ core/src/consensus/consensus_inner/mod.rs
@@ -1217,11 +1217,12 @@ impl ConsensusGraphInner {
 
         if parent != NULL {
             let era_genesis = self.get_era_block_with_parent(parent, 0);
+            let graph_era_stable_genesis = self.ancestor_at(me, self.cur_era_stable_height);
 
             let weight_in_my_epoch = self.total_weight_in_own_epoch(
                 &self.arena[index].data.blockset_in_own_view_of_epoch,
                 false,
-                None,
+                Some(graph_era_stable_genesis),
             );
             let weight_era_in_my_epoch = self.total_weight_in_own_epoch(
                 &self.arena[index].data.blockset_in_own_view_of_epoch,
