c1c486d1eb548e7a4f943b200c87e629c18c0ba5
Péter Garamvölgyi <th307q@gmail.com>
2019-08-02 14:03:54 +0800

    Divide light protocol handler into two parts: query and sync (#488)
    * divide light protocol handler into two parts: query and sync

---
+++ core/src/light_protocol/handler/mod.rs
@@ -0,0 +1,11 @@
+// Copyright 2019 Conflux Foundation. All rights reserved.
+// Conflux is free software and distributed under GNU General Public License.
+// See http://www.gnu.org/licenses/
+
+mod handler;
+mod query;
+mod sync;
+
+pub(super) use query::QueryResult;
+
+pub use handler::Handler;
