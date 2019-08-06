16865f6e1d2e45c2de803873e7d00a13c925209c
Zhe Yang <yz@conflux-chain.org>
2019-08-06 16:59:06 +0200

    Add missing files.

---
+++ core/src/storage/storage_db/snapshot_mpt.rs
@@ -0,0 +1,43 @@
+// Copyright 2019 Conflux Foundation. All rights reserved.
+// Conflux is free software and distributed under GNU General Public License.
+// See http://www.gnu.org/licenses/
+
+pub trait SnapshotMptTraitReadOnly {
+    fn get_merkle_root(&self) -> &MerkleHash;
+    fn load_node(
+        &self, path: &dyn CompressedPathTrait,
+    ) -> Result<Option<VanillaTrieNode<MerkleHash>>>;
+    fn iterate_subtree_trie_nodes_without_root(
+        &self, path: &dyn CompressedPathTrait,
+    ) -> Box<dyn SnapshotMptIteraterTrait>;
+}
+
+// FIXME: We only need a single writer however we must use ReturnAfterUse in
+// MptMerger FIXME: to be able to be pragmatically comply to the single &mut
+// rule. So for now we FIXME: use &self instead of &mut self.
+pub trait SnapshotMptTrait: SnapshotMptTraitReadOnly {
+    fn delete_node(&self, path: &dyn CompressedPathTrait) -> Result<()>;
+    fn write_node(
+        &self, path: &dyn CompressedPathTrait,
+        trie_node: &VanillaTrieNode<MerkleHash>,
+    ) -> Result<()>;
+}
+
+pub trait SnapshotMptIteraterTrait {
+    fn next(
+        &mut self,
+    ) -> Result<Option<(CompressedPathRaw, VanillaTrieNode<MerkleHash>)>>;
+}
+
+// TODO: A snapshot mpt iterator is suitable to work as base_mpt in MptMerger's
+// TODO: save-as mode, because MptMerger always access nodes in snapshot mpt in
+// TODO: increasing order. we need to make special generalization for MptMerger
+// TODO: to take SnapshotMptIteraterTrait as input.
+
+use super::super::impls::{
+    errors::*,
+    multi_version_merkle_patricia_trie::merkle_patricia_trie::{
+        trie_node::VanillaTrieNode, CompressedPathRaw, CompressedPathTrait,
+    },
+};
+use primitives::MerkleHash;
