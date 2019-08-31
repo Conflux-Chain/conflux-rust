aa5606b1f026750fcfe6ed946e6023fe6515855f
sparkmiw <spark_ren@hotmail.com>
2019-08-31 14:23:26 +0800

    support pubsub (#593)
    * support pubsub
    
    * fix test compiling issue

---
+++ core/src/client/chain_notify.rs
@@ -0,0 +1,52 @@
+use cfx_types::H256;
+use primitives::TransactionWithSignature;
+
+/// Messages to broadcast via chain
+#[allow(dead_code)]
+pub enum ChainMessageType {
+    /// Consensus message
+    Consensus(Vec<u8>),
+    /// Mesage with private transaction
+    PrivateTransaction(H256, Vec<u8>),
+    /// Message with signed private transaction
+    SignedPrivateTransaction(H256, Vec<u8>),
+}
+
+/// Used by `ChainNotify` `new_blocks()`
+#[allow(dead_code)]
+pub struct NewBlocks {}
+
+impl NewBlocks {
+    /// Constructor
+    #[allow(dead_code)]
+    pub fn new() -> NewBlocks { NewBlocks {} }
+}
+
+pub trait ChainNotify: Send + Sync {
+    /// fires when chain has new blocks.
+    fn new_blocks(&self, _new_blocks: NewBlocks) {
+        // does nothing by default
+    }
+
+    /// fires when chain achieves active mode
+    fn start(&self) {
+        // does nothing by default
+    }
+
+    /// fires when chain achieves passive mode
+    fn stop(&self) {
+        // does nothing by default
+    }
+
+    /// fires when chain broadcasts a message
+    fn broadcast(&self, _message_type: ChainMessageType) {
+        // does nothing by default
+    }
+
+    /// fires when new transactions are received from a peer
+    fn transactions_received(
+        &self, _txs: &[TransactionWithSignature], _peer_id: usize,
+    ) {
+        // does nothing by default
+    }
+}
