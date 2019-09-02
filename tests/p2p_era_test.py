70b5787781e857cb3fe84796e107f4b47cdca5be
Peilun Li <peilun.li@conflux-chain.org>
2019-09-02 17:40:26 +0800

    Set longer timeout for node recovery.

--- tests/p2p_era_test.py
+++ tests/p2p_era_test.py
@@ -17,7 +17,7 @@ class P2PTest(ConfluxTestFramework):
         self.conf_parameters["generate_tx"] = "true"
         # Every node generates 1 tx every second
         self.conf_parameters["generate_tx_period_us"] = "100000"
-        self.conf_parameters["log_level"] = "\"debug\""
+        self.conf_parameters["log_level"] = "\"info\""
         self.conf_parameters["era_epoch_count"] = "50"
         self.conf_parameters["era_checkpoint_gap"] = "150"
 
