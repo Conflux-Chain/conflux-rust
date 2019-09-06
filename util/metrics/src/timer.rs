a0685cc46f586bdda79ca91db104f07803a7af08
boqiu <82121246@qq.com>
2019-09-06 18:30:31 +0800

    fix minor issues in metrics

--- util/metrics/src/timer.rs
+++ util/metrics/src/timer.rs
@@ -10,19 +10,34 @@ use std::{
 
 pub trait Timer: Send + Sync {
     fn update(&self, _d: Duration) {}
-    fn update_since(&self, _start: Instant) {}
+
+    fn update_since(&self, start_time: Instant) {
+        self.update(start_time.elapsed());
+    }
 }
 
-pub fn register_timer(name: &'static str) -> Arc<dyn Timer> {
+fn register_timer_exp_decay(
+    group: &str, counter_name: &str, time_name: &str,
+) -> Arc<dyn Timer> {
     if !is_enabled() {
         Arc::new(NoopTimer)
     } else {
         Arc::new(StandardTimer {
-            meter: register_meter_with_group(name, "meter"),
+            meter: register_meter_with_group(group, counter_name),
             histogram: Sample::ExpDecay(0.015)
-                .register_with_group(name, "expdec", 1024),
+                .register_with_group(group, time_name, 1024),
         })
     }
+}
+
+pub fn register_timer(name: &str) -> Arc<dyn Timer> {
+    register_timer_exp_decay(name, "counter", "time_expdec")
+}
+
+pub fn register_timer_with_group(group: &str, name: &str) -> Arc<dyn Timer> {
+    let counter_name = format!("{}_counter", name);
+    let time_name = format!("{}_time_expdec", name);
+    register_timer_exp_decay(group, counter_name.as_str(), time_name.as_str())
 }
 
 struct NoopTimer;
@@ -36,8 +51,6 @@ struct StandardTimer {
 impl Timer for StandardTimer {
     fn update(&self, d: Duration) {
         self.meter.mark(1);
-        self.histogram.update(d.as_millis() as usize);
+        self.histogram.update(d.as_nanos() as u64);
     }
-
-    fn update_since(&self, start: Instant) { self.update(start.elapsed()); }
 }
