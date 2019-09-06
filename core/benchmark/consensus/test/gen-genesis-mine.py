856d777cdb2bf0662e362e0e47c795e9b36e6687
ypliu <resodo.liu@gmail.com>
2019-09-06 15:18:23 +0800

    reformat bench code

--- core/benchmark/consensus/test/gen-genesis-mine.py
+++ core/benchmark/consensus/test/gen-genesis-mine.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 fw = open("genesis-mine.in", "w")
-print("2 3 1000 1000 50000", file = fw)
+print("2 3 1000 1000 50000", file=fw)
 N = 200000
 for i in range(0, N):
-    print(-1, -1, -1, 1, i, file = fw)
+    print(-1, -1, -1, 1, i, file=fw)
 cnt = N
 last_cnt = cnt
 for j in range(0, N):
@@ -12,8 +12,8 @@ for j in range(0, N):
     if j % 2 == 1:
         # print(last_cnt, file = fw, end = " ")
         # print(cnt - 1, file = fw)
-        print(-1, -1, -1, 1, last_cnt, file = fw)
+        print(-1, -1, -1, 1, last_cnt, file=fw)
         last_cnt = cnt
     else:
-        print(-1, -1, -1, 1, 0, file = fw)
+        print(-1, -1, -1, 1, 0, file=fw)
 fw.close()
