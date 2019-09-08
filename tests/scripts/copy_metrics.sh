89b8a3f83016dda711bc0416c853b1d99f698b8f
Bo QIU <35757521+boqiu@users.noreply.github.com>
2019-09-08 13:59:07 +0800

    add profile for latency experiment (#627)
    * add flamegraph in exp
    
    * fix

--- tests/scripts/copy_metrics.sh
+++ tests/scripts/copy_metrics.sh
@@ -3,5 +3,9 @@
 set -e
 
 ip=`head -n 1 ips`
+
 ssh ubuntu@$ip "find /tmp/conflux_test_* -name metrics.log | grep node0 | xargs cat >> metrics.log"
 scp -o "StrictHostKeyChecking no" ubuntu@$ip:metrics.log ${1:-tag}.metrics.log
+
+ssh ubuntu@$ip "find /tmp/conflux_test_* -name conflux.svg | grep node0 | xargs cat >> conflux.svg"
+scp -o "StrictHostKeyChecking no" ubuntu@$ip:conflux.svg ${1:-tag}.conflux.svg
