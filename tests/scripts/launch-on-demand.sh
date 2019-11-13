b07359043c5cf3f19f96437970575906f3dfc8bc
FightingManGod <yilin.han.10@gmail.com>
2019-11-13 15:50:23 +0800

    update exp image

--- tests/scripts/launch-on-demand.sh
+++ tests/scripts/launch-on-demand.sh
@@ -16,7 +16,7 @@ elif [[ -f slave_image ]]; then
     public=""
 else
     # create master instances
-    image="ami-0c6ce85b955599b95" # experiment image
+    image="ami-0ddab4a020ac57bd1" # experiment image
     type="m5.2xlarge"
     public="--public"
 fi
