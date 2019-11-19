2d8ccef30ebc0259648c2bb850438e834475ea4b
Peilun Li <peilun.li@conflux-chain.org>
2019-11-19 16:11:54 +0800

    Install clippy.

--- cargo_fmt.sh
+++ cargo_fmt.sh
@@ -4,6 +4,7 @@ if [[ "$1" == "--install" ]]
 then
     rustup toolchain add nightly-2019-07-03
     rustup component add rustfmt --toolchain nightly-2019-07-03
+    rustup component add clippy
     shift
 fi
 cargo +nightly-2019-07-03 fmt --all $@
