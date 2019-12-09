515d42176655ddf1238ac1ef4d79dc882126d9d8
sparkmiw <spark_ren@hotmail.com>
2019-12-09 23:18:55 +0800

    fix minor comments issue (#843)
    * fix minor comments issue

--- blockgen/src/lib.rs
+++ blockgen/src/lib.rs
@@ -206,7 +206,7 @@ impl BlockGenerator {
             .as_secs();
 
         // Adjust the timestamp of the currently mined block to be later
-        // than its parent's.
+        // than or equal to its parent's.
         // See comments in verify_header_graph_ready_block()
         let my_timestamp = max(parent_timestamp, now);
 
