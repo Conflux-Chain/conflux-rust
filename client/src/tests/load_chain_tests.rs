97683217c556397c53caf0990157509c1d6648e7
Peilun Li <peilun.li@conflux-chain.org>
2020-03-02 20:47:20 +0800

    Add more configuration explaination.

--- client/src/tests/load_chain_tests.rs
+++ client/src/tests/load_chain_tests.rs
@@ -57,14 +57,12 @@ fn test_load_chain() {
     let tmp_dir = TempDir::new("conflux-test").unwrap();
     conf.raw_conf.conflux_data_dir =
         tmp_dir.path().to_str().unwrap().to_string() + "/";
-    conf.raw_conf.db_dir = Some(
-        tmp_dir
-            .path()
-            .join("db")
-            .into_os_string()
-            .into_string()
-            .unwrap(),
-    );
+    conf.raw_conf.block_db_dir = tmp_dir
+        .path()
+        .join("db")
+        .into_os_string()
+        .into_string()
+        .unwrap();
     conf.raw_conf.netconf_dir = Some(
         tmp_dir
             .path()
