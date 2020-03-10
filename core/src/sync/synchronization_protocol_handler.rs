1ca5c54832385eeba87950d80bd44b7d773d6ea2
Peilun Li <peilun.li@conflux-chain.org>
2020-03-10 16:30:06 +0800

    nit.

--- core/src/sync/synchronization_protocol_handler.rs
+++ core/src/sync/synchronization_protocol_handler.rs
@@ -1609,6 +1609,7 @@ impl NetworkProtocolHandler for SynchronizationProtocolHandler {
             }
             EXPIRE_BLOCK_GC_TIMER => {
                 // remove expire blocks every `expire_block_gc_period`
+                // TODO Parameterize this timeout.
                 self.expire_block_gc(io, 120).ok();
             }
             EXPIRE_BFT_EXECUTION_TIMER => {
