ed94555ed85fb973c5e8872bb1aa8ca15f1bad14
Xi Lin <zimpha@gmail.com>
2020-03-17 12:03:13 +0800

    add internal_contract folder

---
+++ internal_contract/contracts/Staking.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.4.15;
+
+contract Staking {
+    function deposit(uint amount) external {
+    }
+
+    function withdraw(uint amount) external {
+    }
+
+    function lock(uint amount, uint duration) external {
+    }
+}
