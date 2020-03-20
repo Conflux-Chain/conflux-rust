64784a12f1b3a4fc57db33c92eeca7177a92675e
sparkmiw <spark_ren@hotmail.com>
2020-03-20 23:45:10 +0800

    fix MallocSizeOf for Arc (#1093)
    * fix MallocSizeOf for Arc

--- util/malloc_size_of/src/lib.rs
+++ util/malloc_size_of/src/lib.rs
@@ -15,7 +15,7 @@ use cfg_if::cfg_if;
 use cfx_types::{H160, H256, H512, U256, U512};
 use slab::Slab;
 use std::{
-    collections::{BinaryHeap, VecDeque},
+    collections::{BinaryHeap, HashSet, VecDeque},
     hash::{BuildHasher, Hash},
     mem::{self, size_of},
     ops::Range,
@@ -36,6 +36,8 @@ pub struct MallocSizeOfOps {
     /// memory measurements will actually be computed estimates rather than
     /// real and accurate measurements.
     pub enclosing_size_of_op: Option<VoidPtrToSizeFn>,
+
+    pub visited: HashSet<usize>,
 }
 
 impl MallocSizeOfOps {
@@ -47,6 +49,7 @@ impl MallocSizeOfOps {
         MallocSizeOfOps {
             size_of_op: size_of,
             enclosing_size_of_op: malloc_enclosing_size_of,
+            visited: HashSet::new(),
         }
     }
 
@@ -287,8 +290,12 @@ impl<T: MallocSizeOf> MallocSizeOf for VecDeque<T> {
 /// This is only for estimating memory size in Cache Manager
 impl<T: MallocSizeOf> MallocSizeOf for Arc<T> {
     fn size_of(&self, ops: &mut MallocSizeOfOps) -> usize {
-        (mem::size_of::<T>() + self.as_ref().size_of(ops))
-            / Arc::strong_count(self)
+        let ptr = self.as_ref() as *const T as usize;
+        if ops.visited.contains(&ptr) {
+            return 0;
+        }
+        ops.visited.insert(ptr);
+        mem::size_of::<T>() + self.as_ref().size_of(ops)
     }
 }
 
