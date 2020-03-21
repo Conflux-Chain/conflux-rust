65e58201b69ec7a4ae63515980a12e63e6ae32a3
fanlong <longfancn@gmail.com>
2020-03-20 21:19:01 -0400

    Add activated count to the statistics (#1096)

--- core/src/consensus/mod.rs
+++ core/src/consensus/mod.rs
@@ -81,6 +81,7 @@ pub struct ConsensusConfig {
 #[derive(Debug)]
 pub struct ConsensusGraphStatistics {
     pub inserted_block_count: usize,
+    pub activated_block_count: usize,
     pub processed_block_count: usize,
 }
 
@@ -88,12 +89,14 @@ impl ConsensusGraphStatistics {
     pub fn new() -> ConsensusGraphStatistics {
         ConsensusGraphStatistics {
             inserted_block_count: 0,
+            activated_block_count: 0,
             processed_block_count: 0,
         }
     }
 
     pub fn clear(&mut self) {
         self.inserted_block_count = 0;
+        self.activated_block_count = 0;
         self.processed_block_count = 0;
     }
 }
