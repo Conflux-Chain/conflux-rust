Conflux-Chain/conflux-rust

commit d36fb5fa0dd7cb15e66cf8a72b4c31e450a1fca1

Fix bug in compressed path merkle computation (#1182)

core/src/storage/impls/merkle_patricia_trie/compressed_path.rs
@@ -4,51 +4,53 @@
 
 pub trait CompressedPathTrait: Debug {
     fn path_slice(&self) -> &[u8];
-    fn end_mask(&self) -> u8;
+    fn path_mask(&self) -> u8;
 
     fn path_size(&self) -> u16 { self.path_slice().len() as u16 }
 
     fn path_steps(&self) -> u16 {
-        self.path_size() * 2
-            - (self.end_mask() != CompressedPathRaw::HAS_SECOND_NIBBLE) as u16
+        CompressedPathRaw::calculate_path_steps(
+            self.path_size(),
+            self.path_mask(),
+        )
     }
 
     fn as_ref(&self) -> CompressedPathRef {
         CompressedPathRef {
             path_slice: self.path_slice(),
-            end_mask: self.end_mask(),
+            path_mask: self.path_mask(),
         }
     }
 
     fn rlp_append(&self, s: &mut RlpStream) {
         s.begin_list(2);
-        s.append(&self.end_mask()).append(&self.path_slice());
+        s.append(&self.path_mask()).append(&self.path_slice());
     }
 }
 
 impl CompressedPathTrait for [u8] {
     fn path_slice(&self) -> &[u8] { self }
 
-    fn end_mask(&self) -> u8 { CompressedPathRaw::HAS_SECOND_NIBBLE }
+    fn path_mask(&self) -> u8 { CompressedPathRaw::NO_MISSING_NIBBLE }
 }
 
 impl<'a> CompressedPathTrait for &'a [u8] {
     fn path_slice(&self) -> &[u8] { self }
 
-    fn end_mask(&self) -> u8 { CompressedPathRaw::HAS_SECOND_NIBBLE }
+    fn path_mask(&self) -> u8 { CompressedPathRaw::NO_MISSING_NIBBLE }
 }
 
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Clone, Debug, Default, PartialEq, Eq, Hash)]
 pub struct CompressedPathRef<'a> {
-    path_slice: &'a [u8],
-    end_mask: u8,
+    pub path_slice: &'a [u8],
+    path_mask: u8,
 }
 
 impl<'a> CompressedPathRef<'a> {
-    pub fn new(path_slice: &'a [u8], end_mask: u8) -> Self {
+    pub fn new(path_slice: &'a [u8], path_mask: u8) -> Self {
         Self {
             path_slice,
-            end_mask,
+            path_mask,
         }
     }
 }
@@ -57,7 +59,7 @@ impl<'a> CompressedPathRef<'a> {
 pub struct CompressedPathRaw {
     path_size: u16,
     path: MaybeInPlaceByteArray,
-    end_mask: u8,
+    path_mask: u8,
     pub byte_array_memory_manager:
         FieldsOffsetMaybeInPlaceByteArrayMemoryManager<
             u16,
@@ -103,20 +105,15 @@ make_parallel_field_maybe_in_place_byte_array_memory_manager!(
 impl CompressedPathRaw {
     const BITS_0_3_MASK: u8 = 0x0f;
     const BITS_4_7_MASK: u8 = 0xf0;
-    pub const HAS_SECOND_NIBBLE: u8 = 0;
+    pub const NO_MISSING_NIBBLE: u8 = 0;
 }
 
 impl<'a> CompressedPathTrait for CompressedPathRef<'a> {
     fn path_slice(&self) -> &[u8] { self.path_slice }
 
-    fn end_mask(&self) -> u8 { self.end_mask }
+    fn path_mask(&self) -> u8 { self.path_mask }
 
     fn path_size(&self) -> u16 { self.path_slice.len() as u16 }
-
-    fn path_steps(&self) -> u16 {
-        (self.path_slice.len() as u16 * 2)
-            - (self.end_mask != CompressedPathRaw::HAS_SECOND_NIBBLE) as u16
-    }
 }
 
 impl CompressedPathTrait for CompressedPathRaw {
@@ -124,31 +121,31 @@ impl CompressedPathTrait for CompressedPathRaw {
         self.path.get_slice(self.path_size as usize)
     }
 
-    fn end_mask(&self) -> u8 { self.end_mask }
+    fn path_mask(&self) -> u8 { self.path_mask }
 }
 
 impl<'a> From<&'a [u8]> for CompressedPathRaw {
     fn from(x: &'a [u8]) -> Self {
-        CompressedPathRaw::new(x, Self::HAS_SECOND_NIBBLE)
+        CompressedPathRaw::new(x, Self::NO_MISSING_NIBBLE)
     }
 }
 
 impl<'a> From<CompressedPathRef<'a>> for CompressedPathRaw {
     fn from(x: CompressedPathRef<'a>) -> Self {
-        CompressedPathRaw::new(x.path_slice, x.end_mask)
+        CompressedPathRaw::new(x.path_slice, x.path_mask)
     }
 }
 
 impl CompressedPathRaw {
-    /// Create a new CompressedPathRaw from valid (path_slice, end_mask)
+    /// Create a new CompressedPathRaw from valid (path_slice, path_mask)
     /// combination.
-    pub fn new(path_slice: &[u8], end_mask: u8) -> Self {
+    pub fn new(path_slice: &[u8], path_mask: u8) -> Self {
         let path_size = path_slice.len();
 
         Self {
             path_size: path_size as u16,
             path: MaybeInPlaceByteArray::copy_from(path_slice, path_size),
-            end_mask,
+            path_mask,
             byte_array_memory_manager: Default::default(),
         }
     }
@@ -163,29 +160,28 @@ impl CompressedPathRaw {
         }
     }
 
-    #[inline]
-    fn apply_mask(&mut self, end_mask: u8) {
-        *self.last_byte_mut() &= end_mask;
-    }
-
-    pub fn new_and_apply_mask(path_slice: &[u8], end_mask: u8) -> Self {
+    pub fn new_and_apply_mask(path_slice: &[u8], path_mask: u8) -> Self {
         let path_size = path_slice.len();
         let mut ret = Self {
             path_size: path_size as u16,
             path: MaybeInPlaceByteArray::copy_from(path_slice, path_size),
-            end_mask,
+            path_mask,
             byte_array_memory_manager: Default::default(),
         };
-        ret.apply_mask(end_mask);
+        // 0xf* -> no second nibble
+        // 0x0* -> has second nibble
+        // 0xf0 -> 0x0f -> &= 0xf0
+        // 0x00 -> 0xff -> &= 0xff
+        *ret.last_byte_mut() &= !Self::first_nibble(path_mask);
 
         ret
     }
 
-    pub fn new_zeroed(path_size: u16, end_mask: u8) -> Self {
+    pub fn new_zeroed(path_size: u16, path_mask: u8) -> Self {
         Self {
             path_size,
             path: MaybeInPlaceByteArray::new_zeroed(path_size as usize),
-            end_mask,
+            path_mask,
             byte_array_memory_manager: Default::default(),
         }
     }
@@ -196,32 +192,59 @@ impl CompressedPathRaw {
     #[inline]
     pub const fn second_nibble_mask() -> u8 { Self::BITS_4_7_MASK }
 
+    #[inline]
+    fn calculate_path_steps(path_size: u16, path_mask: u8) -> u16 {
+        path_size * 2
+            - (Self::clear_second_nibble(path_mask) != 0) as u16
+            - (Self::second_nibble(path_mask) != 0) as u16
+    }
+
+    #[inline]
     pub fn from_first_nibble(x: u8) -> u8 { x << 4 }
 
+    #[inline]
     pub fn first_nibble(x: u8) -> u8 { x >> 4 }
 
+    #[inline]
+    pub fn clear_second_nibble(x: u8) -> u8 { x & Self::BITS_4_7_MASK }
+
+    #[inline]
     pub fn second_nibble(x: u8) -> u8 { x & Self::BITS_0_3_MASK }
 
+    #[inline]
     pub fn set_second_nibble(x: u8, second_nibble: u8) -> u8 {
-        (x & Self::BITS_4_7_MASK) | second_nibble
+        Self::clear_second_nibble(x) | second_nibble
+    }
+
+    #[inline]
+    pub fn has_second_nibble(path_mask: u8) -> bool {
+        Self::clear_second_nibble(path_mask)
+            == CompressedPathRaw::NO_MISSING_NIBBLE
+    }
+
+    #[inline]
+    pub fn no_second_nibble(path_mask: u8) -> bool {
+        Self::clear_second_nibble(path_mask)
+            != CompressedPathRaw::NO_MISSING_NIBBLE
     }
 
     pub fn extend_path<X: CompressedPathTrait>(x: &X, child_index: u8) -> Self {
         let new_size;
-        let end_mask;
+        let path_mask;
         // Need to extend the length.
-        if x.end_mask() == CompressedPathRaw::HAS_SECOND_NIBBLE {
+        let x_path_mask = x.path_mask();
+        if Self::has_second_nibble(x_path_mask) {
             new_size = x.path_size() + 1;
-            end_mask = Self::second_nibble_mask();
+            path_mask = x_path_mask | Self::second_nibble_mask();
         } else {
             new_size = x.path_size();
-            end_mask = CompressedPathRaw::HAS_SECOND_NIBBLE;
+            path_mask = Self::second_nibble(x_path_mask);
         }
-        let mut ret = Self::new_zeroed(new_size, end_mask);
+        let mut ret = Self::new_zeroed(new_size, path_mask);
         ret.path.get_slice_mut(new_size as usize)[0..x.path_size() as usize]
             .copy_from_slice(x.path_slice());
         // The last byte will be a half-byte.
-        if x.end_mask() == CompressedPathRaw::HAS_SECOND_NIBBLE {
+        if Self::has_second_nibble(x_path_mask) {
             *ret.last_byte_mut() = Self::from_first_nibble(child_index);
         } else {
             let last_byte = *ret.last_byte_mut();
@@ -242,6 +265,7 @@ impl CompressedPathRaw {
     ) -> Self {
         let x_slice = x.path_slice();
         let x_slice_len = x_slice.len();
+        let x_path_mask = x.path_mask();
         let y_slice = y.path_slice();
 
         // TODO(yz): it happens to be the same no matter what end_mask of x is,
@@ -274,7 +298,7 @@ impl CompressedPathRaw {
                 }
             }
 
-            if x.end_mask() == CompressedPathRaw::HAS_SECOND_NIBBLE {
+            if Self::has_second_nibble(x_path_mask) {
                 slice[0..x_slice_len].copy_from_slice(x_slice);
             } else {
                 slice[0..x_slice_len - 1]
@@ -290,7 +314,10 @@ impl CompressedPathRaw {
         Self {
             path_size: size as u16,
             path,
-            end_mask: y.end_mask(),
+            path_mask: Self::set_second_nibble(
+                y.path_mask(),
+                CompressedPathRaw::second_nibble(x_path_mask),
+            ),
             byte_array_memory_manager: Default::default(),
         }
     }
@@ -299,7 +326,7 @@ impl CompressedPathRaw {
 impl Clone for CompressedPathRaw {
     fn clone(&self) -> Self {
         Self {
-            end_mask: self.end_mask,
+            path_mask: self.path_mask,
             path_size: self.path_size,
             path: MaybeInPlaceByteArray::clone(
                 &self.path,
@@ -323,7 +350,6 @@ impl Encodable for CompressedPathRaw {
 }
 
 impl Decodable for CompressedPathRaw {
-    // TODO(yz): the format can be optimized.
     fn decode(rlp: &Rlp) -> Result<Self, DecoderError> {
         Ok(CompressedPathRaw::new(
             rlp.val_at::<Vec<u8>>(1)?.as_slice(),

core/src/storage/impls/delta_mpt/cow_node_ref.rs
@@ -253,10 +253,9 @@ impl CowNodeRef {
     {
         if self.owned {
             if guarded_trie_node.as_ref().as_ref().has_value() {
-                assert_eq!(
-                    key_prefix.end_mask(),
-                    CompressedPathRaw::HAS_SECOND_NIBBLE
-                );
+                assert!(CompressedPathRaw::has_second_nibble(
+                    key_prefix.path_mask()
+                ));
                 values.push((
                     key_prefix.path_slice().to_vec(),
                     guarded_trie_node.as_ref().as_ref().value_clone().unwrap(),
@@ -390,12 +389,16 @@ impl CowNodeRef {
     }
 
     /// Get if unowned, compute if owned.
+    ///
+    /// parent_node_path_steps_plus_one is the steps of path from root to the
+    /// compressed_path. e.g. parent_node_path_steps_plus_one is 0 for root
+    /// node, 1 for root node's direct children.
     pub fn get_or_compute_merkle(
         &mut self, trie: &DeltaMpt, owned_node_set: &mut OwnedNodeSet,
         allocator_ref: AllocatorRefRefDeltaMpt,
         db: &mut DeltaDbOwnedReadTraitObj,
         children_merkle_map: &mut ChildrenMerkleMap,
-        parent_node_path_steps: u16,
+        parent_node_path_steps_plus_one: u16,
     ) -> Result<MerkleHash>
     {
         if self.owned {
@@ -405,9 +408,7 @@ impl CowNodeRef {
                     &mut self.node_ref,
                 )
             };
-            // node_path_steps = 2 * parent_node_path_size_in_bytes +
-            // compressed_path_path_steps.
-            let node_path_steps = ((parent_node_path_steps + 1) & (!1))
+            let node_path_steps = parent_node_path_steps_plus_one
                 + trie_node.compressed_path_ref().path_steps();
             let children_merkles = self.get_or_compute_children_merkles(
                 trie,
@@ -421,7 +422,7 @@ impl CowNodeRef {
 
             let merkle = self.set_merkle(
                 children_merkles.as_ref(),
-                (parent_node_path_steps % 2) == 0,
+                (parent_node_path_steps_plus_one % 2) == 1,
                 trie_node,
             );
 
@@ -550,7 +551,9 @@ impl CowNodeRef {
                                 allocator_ref,
                                 db,
                                 children_merkle_map,
-                                node_path_steps,
+                                // +1 for the child_index, see also comment for
+                                // get_or_compute_merkle.
+                                node_path_steps + 1,
                             );
                             // There is no change to the child reference so the
                             // return value is dropped.
@@ -589,10 +592,9 @@ impl CowNodeRef {
     ) -> Result<()>
     {
         if guarded_trie_node.as_ref().as_ref().has_value() {
-            assert_eq!(
-                key_prefix.end_mask(),
-                CompressedPathRaw::HAS_SECOND_NIBBLE
-            );
+            assert!(CompressedPathRaw::has_second_nibble(
+                key_prefix.path_mask()
+            ));
             values.push((
                 key_prefix.path_slice().to_vec(),
                 guarded_trie_node.as_ref().as_ref().value_clone().unwrap(),

core/src/storage/impls/delta_mpt/mem_optimized_trie_node.rs
@@ -29,11 +29,14 @@ pub struct MemOptimizedTrieNode<CacheAlgoDataT: CacheAlgoDataTrait> {
     /// When there is only one half-byte in path and it's the "first half",
     /// path_end_mask is set. When there is only one half-byte in path and
     /// it's the "second half", compressed_path is set to one full byte
-    /// with the missing "first half", and path_end_mask is set to "no
-    /// mask". In comparison it still matches the corresponding byte of the
-    /// key. We don't store path_begin_mask because it isn't used at all in
-    /// comparison.
-    path_end_mask: u8,
+    /// with the missing "first half", and path_begin_mask is set to second
+    /// half, path_end_mask is set to "no mask". In comparison it still
+    /// matches the corresponding byte of the  key.
+    ///
+    /// We combine path_begin_mask and path_end_mask into path_mask.
+    /// 0xf0 -> no end nibble; 0x0f -> no start nibble;
+    /// 0xff -> no start & end nibble; 0x00 -> full bytes;
+    path_mask: u8,
     /// 4 bits per step.
     /// We limit the maximum key steps by u16.
     path_size: u16,
@@ -177,7 +180,7 @@ impl<CacheAlgoDataT: CacheAlgoDataTrait> MemOptimizedTrieNode<CacheAlgoDataT> {
             self.path_memory_manager.drop_value();
         }
         self.path_size = new_path.path_size();
-        self.path_end_mask = new_path.end_mask();
+        self.path_mask = new_path.path_mask();
         let path_slice = new_path.path_slice();
         self.path =
             MaybeInPlaceByteArray::copy_from(path_slice, path_slice.len());
@@ -257,7 +260,7 @@ impl<CacheAlgoDataT: CacheAlgoDataTrait> TrieNodeTrait
         let size = self.path_size;
         CompressedPathRef::new(
             self.path.get_slice(size as usize),
-            self.path_end_mask,
+            self.path_mask,
         )
     }
 
@@ -279,7 +282,7 @@ impl<CacheAlgoDataT: CacheAlgoDataTrait> TrieNodeTrait
     }
 
     fn set_compressed_path(&mut self, mut path: CompressedPathRaw) {
-        self.path_end_mask = path.end_mask();
+        self.path_mask = path.path_mask();
 
         path.byte_array_memory_manager
             .move_to(&mut self.path_memory_manager);
@@ -508,7 +511,7 @@ impl<CacheAlgoDataT: CacheAlgoDataTrait> EntryTrait
                 ^ NodeRefDeltaMptCompact::DIRTY_SLOT_LIMIT,
             children_table: Default::default(),
             merkle_hash: Default::default(),
-            path_end_mask: CompressedPathRaw::HAS_SECOND_NIBBLE,
+            path_mask: CompressedPathRaw::NO_MISSING_NIBBLE,
             path_size: 0,
             path: Default::default(),
             path_memory_manager: Default::default(),

core/src/storage/impls/delta_mpt/subtrie_visitor.rs
@@ -187,8 +187,9 @@ impl<'trie, 'db: 'trie> SubTrieVisitor<'trie, 'db> {
                 let this_node_path_without_first_nibble =
                     path_without_first_nibble;
                 path_without_first_nibble =
-                    trie_node.compressed_path_ref().end_mask()
-                        == CompressedPathRaw::HAS_SECOND_NIBBLE;
+                    CompressedPathRaw::has_second_nibble(
+                        trie_node.compressed_path_ref().path_mask(),
+                    );
                 drop(trie_node);
                 let children_merkles =
                     self.retrieve_children_hashes(children)?;

core/src/storage/impls/merkle_patricia_trie/merkle.rs
@@ -69,14 +69,17 @@ fn compute_path_merkle(
     node_merkle: &MerkleHash,
 ) -> MerkleHash
 {
+    assert_eq!(
+        without_first_nibble,
+        CompressedPathRaw::second_nibble(compressed_path.path_mask())
+            != CompressedPathRaw::NO_MISSING_NIBBLE,
+        "without_first_nibble: {}, path_mask: {}",
+        without_first_nibble,
+        compressed_path.path_mask(),
+    );
+
     // compressed_path is non-empty.
-    //
-    // Trie node without a compressed path which starts with the second
-    // half nibble always has the first half nibble stored
-    // in its compressed_path to help with trie access.
-    // We shouldn't include the compressed path when there isn't
-    // anything.
-    if compressed_path.path_steps() > (without_first_nibble as u16) {
+    if compressed_path.path_steps() > 0 {
         let mut buffer = Vec::with_capacity(
             1 + compressed_path.path_size() as usize
                 + std::mem::size_of::<MerkleHash>(),
@@ -89,8 +92,7 @@ fn compute_path_merkle(
         // 64.
         let path_info_byte = 128u8
             + 64u8 * (without_first_nibble as u8)
-            + (compressed_path.path_steps() as u8 - without_first_nibble as u8)
-                % 63u8;
+            + (compressed_path.path_steps() as u8) % 63u8;
         buffer.push(path_info_byte);
 
         buffer.extend_from_slice(compressed_path.path_slice());

core/src/storage/impls/merkle_patricia_trie/mpt_cursor.rs
@@ -143,7 +143,7 @@ impl<Mpt: GetReadMpt, PathNode: PathNodeTrait<Mpt>> MptCursor<Mpt, PathNode> {
                 // The beginning of compressed_path is always aligned at full
                 // byte.
                 let aligned_path_start_offset = started_steps / 2;
-                if aligned_path_start_offset * 2
+                if started_steps
                     + last_trie_node.compressed_path_ref().path_steps()
                     == match_stopped_steps
                 {
@@ -163,35 +163,33 @@ impl<Mpt: GetReadMpt, PathNode: PathNodeTrait<Mpt>> MptCursor<Mpt, PathNode> {
                             WalkStop::path_diverted_uninitialized(),
                         ))
                     } else {
+                        let original_compressed_path_ref =
+                            last_trie_node.compressed_path_ref();
+                        let original_compressed_path_ref_mask =
+                            original_compressed_path_ref.path_mask();
                         let actual_matched_path = CompressedPathRaw::new(
                             &matched_path.path_slice()
                                 [aligned_path_start_offset as usize..],
-                            matched_path.end_mask(),
+                            CompressedPathRaw::set_second_nibble(
+                                matched_path.path_mask(),
+                                CompressedPathRaw::second_nibble(
+                                    original_compressed_path_ref_mask,
+                                ),
+                            ),
                         );
-                        let original_compressed_path_ref =
-                            last_trie_node.compressed_path_ref();
                         let actual_unmatched_path_remaining =
-                            if original_compressed_path_ref.end_mask()
-                                != CompressedPathRaw::HAS_SECOND_NIBBLE
-                            {
-                                CompressedPathRaw::new_and_apply_mask(
-                                    &unmatched_path_remaining.path_slice()[0
-                                        ..(original_compressed_path_ref
-                                            .path_size()
-                                            - actual_matched_path.path_size())
-                                            as usize],
-                                    original_compressed_path_ref.end_mask(),
-                                )
-                            } else {
-                                CompressedPathRaw::new(
-                                    &unmatched_path_remaining.path_slice()[0
-                                        ..(original_compressed_path_ref
-                                            .path_size()
-                                            - actual_matched_path.path_size())
-                                            as usize],
-                                    original_compressed_path_ref.end_mask(),
-                                )
-                            };
+                            CompressedPathRaw::new_and_apply_mask(
+                                &unmatched_path_remaining.path_slice()[0
+                                    ..(original_compressed_path_ref.path_size()
+                                        - actual_matched_path.path_size())
+                                        as usize],
+                                CompressedPathRaw::set_second_nibble(
+                                    original_compressed_path_ref_mask,
+                                    CompressedPathRaw::second_nibble(
+                                        unmatched_path_remaining.path_mask(),
+                                    ),
+                                ),
+                            );
 
                         Ok(CursorPopNodesTerminal::PathDiverted(
                             WalkStop::PathDiverted {
@@ -243,7 +241,7 @@ impl<Mpt: GetReadMpt, PathNode: PathNodeTrait<Mpt>> MptCursor<Mpt, PathNode> {
                     };
 
                     let next_step = walk::<AM, _>(
-                        key_remaining,
+                        key_remaining.path_slice,
                         &new_node
                             .get_basic_path_node()
                             .trie_node
@@ -268,7 +266,7 @@ impl<Mpt: GetReadMpt, PathNode: PathNodeTrait<Mpt>> MptCursor<Mpt, PathNode> {
                         } => {
                             self.path_nodes.push(new_node);
                             child_index = *new_child_index;
-                            key_remaining = *new_key_remaining;
+                            key_remaining = new_key_remaining.clone();
                             continue;
                         }
                         WalkStop::PathDiverted { .. } => {
@@ -1542,7 +1540,7 @@ impl<'node> GetChildTrait<'node> for MptCursorGetChild {
 pub enum CursorPopNodesTerminal<'key> {
     Arrived,
     Descent {
-        key_remaining: &'key [u8],
+        key_remaining: CompressedPathRef<'key>,
         child_index: u8,
     },
     PathDiverted(WalkStop<'key, ()>),
@@ -1551,7 +1549,7 @@ pub enum CursorPopNodesTerminal<'key> {
 pub enum CursorOpenPathTerminal<'key> {
     Arrived,
     ChildNotFound {
-        key_remaining: &'key [u8],
+        key_remaining: CompressedPathRef<'key>,
         child_index: u8,
     },
     PathDiverted(WalkStop<'key, ()>),
@@ -1588,15 +1586,20 @@ pub fn rlp_key_value_len(key_len: u16, value_len: usize) -> u64 {
     rlp_str_len(key_len.into()) + rlp_str_len(value_len)
 }
 
-use super::{
-    super::{super::storage_db::snapshot_mpt::*, errors::*},
-    children_table::*,
-    compressed_path::CompressedPathTrait,
-    mpt_value::MptValue,
-    trie_node::{TrieNodeTrait, VanillaTrieNode},
-    trie_proof::*,
-    walk::*,
-    CompressedPathRaw,
+use crate::storage::{
+    impls::{
+        errors::*,
+        merkle_patricia_trie::{
+            children_table::*,
+            compressed_path::CompressedPathTrait,
+            mpt_value::MptValue,
+            trie_node::{TrieNodeTrait, VanillaTrieNode},
+            trie_proof::*,
+            walk::*,
+            CompressedPathRaw, CompressedPathRef,
+        },
+    },
+    storage_db::snapshot_mpt::*,
 };
 use primitives::{MerkleHash, MERKLE_NULL_NODE};
 use std::{

core/src/storage/impls/merkle_patricia_trie/tests/trie_proof_test.rs
@@ -13,7 +13,10 @@ fn test_rlp() {
     let node1 = TrieProofNode::new(
         Default::default(),
         Some(Box::new([0x03, 0x04, 0x05])),
-        (&[0x00, 0x01, 0x02][..]).into(),
+        CompressedPathRaw::new(
+            &[0x00, 0x01, 0x02],
+            CompressedPathRaw::first_nibble_mask(),
+        ),
         /* path_without_first_nibble = */ true,
     );
     assert_eq!(node1, rlp::decode(&rlp::encode(&node1)).unwrap());
@@ -62,14 +65,20 @@ fn test_proofs() {
     let leaf1 = TrieProofNode::new(
         Default::default(),
         Some(Box::new(value1)),
-        (&[0x20u8][..]).into(),
+        CompressedPathRaw::new(
+            &[0x20u8][..],
+            CompressedPathRaw::first_nibble_mask(),
+        ),
         /* path_without_first_nibble = */ true,
     );
 
     let leaf2 = TrieProofNode::new(
         Default::default(),
         Some(Box::new(value2)),
-        (&[0x30u8][..]).into(),
+        CompressedPathRaw::new(
+            &[0x30u8][..],
+            CompressedPathRaw::first_nibble_mask(),
+        ),
         /* path_without_first_nibble = */ true,
     );
 
@@ -82,7 +91,10 @@ fn test_proofs() {
             // There must be some value for this node otherwise it contradicts
             // with path compression.
             Some(Default::default()),
-            (&[0x00u8, 0x00u8][..]).into(),
+            CompressedPathRaw::new(
+                &[0x00u8, 0x00u8][..],
+                CompressedPathRaw::first_nibble_mask(),
+            ),
             /* path_without_first_nibble = */ true,
         )
     };

commit 448a1068ee35f295ac844b32f381aea8c60e3fbe

Bug fix in #1182. (#1228)
The bug is caused in #1182 when I merged two cases for CompressedPathRaw creation, however new_and_apply_mask assumes that the length of the compressed path is at least one, thus the bug.

I didn't modify the changelog because it's the bug introduced and fixed within the same version.

The bug triggers only in debug test mode. I'm thinking about running debug tests periodically as well.

core/src/storage/impls/merkle_patricia_trie/compressed_path.rs
@@ -168,11 +168,13 @@ impl CompressedPathRaw {
             path_mask,
             byte_array_memory_manager: Default::default(),
         };
-        // 0xf* -> no second nibble
-        // 0x0* -> has second nibble
-        // 0xf0 -> 0x0f -> &= 0xf0
-        // 0x00 -> 0xff -> &= 0xff
-        *ret.last_byte_mut() &= !Self::first_nibble(path_mask);
+        if path_size > 0 {
+            // 0xf* -> no second nibble
+            // 0x0* -> has second nibble
+            // 0xf0 -> 0x0f -> &= 0xf0
+            // 0x00 -> 0xff -> &= 0xff
+            *ret.last_byte_mut() &= !Self::first_nibble(path_mask);
+        }
 
         ret
     }