65a6ec948a055ec9778c4b2c0685a982cdde258a
Peilun Li <peilun.li@conflux-chain.org>
2020-04-30 00:04:52 +0800

    Fmt.

--- build.rs
+++ build.rs
@@ -1,7 +1,5 @@
-fn main()
-{
-    if cfg!(target_os = "windows")
-    {
+fn main() {
+    if cfg!(target_os = "windows") {
         println!("cargo:rustc-link-lib=dylib=ShLwApi");
         println!("cargo:rustc-link-lib=dylib=Rpcrt4");
     }
