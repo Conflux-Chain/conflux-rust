16bd3db6c2684632f0c2d35ff28107ec7938bca7
Fan Long <longfancn@gmail.com>
2020-05-13 01:04:06 -0400

    fix flasky test

--- tests/rpc/test_get_block_reward_info.py
+++ tests/rpc/test_get_block_reward_info.py
@@ -13,22 +13,22 @@ class TestGetBlockRewardInfo(RpcClient):
         f_pivot = max(f1, f2)
         f_ref = min(f1, f2)
         b = self.generate_block()
-        for i in range(0, 12):
+        for i in range(0, 10):
             self.generate_block()
         time.sleep(1)
 
         epoch = self.epoch_number(self.EPOCH_LATEST_MINED)
         res = self.get_block_reward_info(self.EPOCH_LATEST_MINED)
         assert(len(res) == 0)
-        res = self.get_block_reward_info(self.EPOCH_NUM(epoch - 12))
+        res = self.get_block_reward_info(self.EPOCH_NUM(epoch - 10))
         assert(len(res) == 0)
 
-        for i in range(0, 5):
+        for i in range(0, 7):
             self.generate_block()
         time.sleep(1)
 
         # After 12 (REWARD_EPOCH_COUNT) + 5 (DEFERRED_STATE_COUNT) epochs, rewards should always be available
-        res = self.get_block_reward_info(self.EPOCH_NUM(epoch - 12))
+        res = self.get_block_reward_info(self.EPOCH_NUM(epoch - 10))
         print(res)
         assert(len(res) == 2)
         for reward_info in res:
