4ac216b832e2a99647fbb4b84c4992abd48351aa
Thegaram <th307q@gmail.com>
2020-06-04 16:23:20 +0200

    fix storage proof tests

--- core/src/storage/tests/proofs.rs
+++ core/src/storage/tests/proofs.rs
@@ -1,11 +1,189 @@
-// Copyright 2019 Conflux Foundation. All rights reserved.
+// Copyright 2020 Conflux Foundation. All rights reserved.
 // Conflux is free software and distributed under GNU General Public License.
 // See http://www.gnu.org/licenses/
 
-fn get_invalid_hash(rng: &mut ChaChaRng, mut hash: H256) -> H256 {
-    hash.as_bytes_mut()[rng.gen_range(0, 32)] = 0x00;
-    hash.as_bytes_mut()[rng.gen_range(0, 32)] = 0x00;
-    hash
+/// Divide `v` into `n` portions and return the `k`th (starting from 0).
+/// ```
+/// let x = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+/// assert_eq!(portion(&x, 0, 5), &[1, 2]);
+/// assert_eq!(portion(&x, 1, 5), &[3, 4]);
+/// assert_eq!(portion(&x, 2, 5), &[5, 6]);
+/// assert_eq!(portion(&x, 3, 5), &[7, 8]);
+/// assert_eq!(portion(&x, 4, 5), &[9, 10]);
+/// ```
+fn portion<T>(v: &Vec<T>, k: usize, n: usize) -> &[T] {
+    assert!(k < n);
+    let from = k * v.len() / n;
+    let to = (k + 1) * v.len() / n;
+    &v[from..to]
+}
+
+// generate state with snapshot, intermediate and delta tries
+//                    [snapshot] [intermediate] [delta]
+// 1/7 of keys are in     X
+// 1/7 of keys are in     X            X
+// 1/7 of keys are in     X                        X
+// 1/7 of keys are in     X            X           X
+// 1/7 of keys are in                  X
+// 1/7 of keys are in                  X           X
+// 1/7 of keys are in                              X
+fn generate_random_state(
+    rng: &mut ChaChaRng,
+) -> (FakeStateManager, State, DeltaMptKeyPadding, Vec<Vec<u8>>) {
+    let snapshot_epoch_count = 1;
+    let state_manager =
+        new_state_manager_for_unit_test_with_snapshot_epoch_count(
+            snapshot_epoch_count,
+        );
+
+    // TODO: test other key types as well, not just AccountKey
+
+    let mut keys: Vec<Vec<u8>> = generate_account_keys(TEST_NUMBER_OF_KEYS)
+        .into_iter()
+        .filter(|_| rng.gen_bool(0.2))
+        .collect();
+
+    // insert 0th, 1st, 2nd, 3rd 1/7 portions into state-0
+    let mut state_0 = state_manager.get_state_for_genesis_write();
+
+    for key in portion(&keys, 0, 7) {
+        state_0
+            .set(StorageKey::AccountKey(key), key[..].into())
+            .expect("Inserting k-v should succeed");
+    }
+
+    for key in portion(&keys, 1, 7) {
+        state_0
+            .set(StorageKey::AccountKey(key), key[..].into())
+            .expect("Inserting k-v should succeed");
+    }
+
+    for key in portion(&keys, 2, 7) {
+        state_0
+            .set(StorageKey::AccountKey(key), key[..].into())
+            .expect("Inserting k-v should succeed");
+    }
+
+    for key in portion(&keys, 3, 7) {
+        state_0
+            .set(StorageKey::AccountKey(key), key[..].into())
+            .expect("Inserting k-v should succeed");
+    }
+
+    let mut epoch_id_0 = H256::default();
+    epoch_id_0.as_bytes_mut()[0] = 1;
+    let root_0 = state_0.compute_state_root().unwrap();
+    state_0.commit(epoch_id_0).unwrap();
+
+    // insert 1st, 3rd, 4th, 5th 1/7 portions into state-1
+    let mut state_1 = state_manager
+        .get_state_for_next_epoch(StateIndex::new_for_next_epoch(
+            &epoch_id_0,
+            &root_0,
+            1,
+            snapshot_epoch_count,
+        ))
+        .unwrap()
+        .unwrap();
+
+    for key in portion(&keys, 1, 7) {
+        state_1
+            .set(StorageKey::AccountKey(key), key[..].into())
+            .expect("Inserting k-v should succeed");
+    }
+
+    for key in portion(&keys, 3, 7) {
+        state_1
+            .set(StorageKey::AccountKey(key), key[..].into())
+            .expect("Inserting k-v should succeed");
+    }
+
+    for key in portion(&keys, 4, 7) {
+        state_1
+            .set(StorageKey::AccountKey(key), key[..].into())
+            .expect("Inserting k-v should succeed");
+    }
+
+    for key in portion(&keys, 5, 7) {
+        state_1
+            .set(StorageKey::AccountKey(key), key[..].into())
+            .expect("Inserting k-v should succeed");
+    }
+
+    let mut epoch_id_1 = H256::default();
+    epoch_id_1.as_bytes_mut()[0] = 2;
+    let root_1 = state_1.compute_state_root().unwrap();
+    state_1.commit(epoch_id_1).unwrap();
+
+    let intermediate_padding = StorageKey::delta_mpt_padding(
+        &root_1.state_root.snapshot_root,
+        &root_1.state_root.intermediate_delta_root,
+    );
+
+    // insert 2nd, 3rd, 5th, 6th 1/7 portions into state-2
+    let mut state_2 = state_manager
+        .get_state_for_next_epoch(StateIndex::new_for_next_epoch(
+            &epoch_id_1,
+            &root_1,
+            2,
+            snapshot_epoch_count,
+        ))
+        .unwrap()
+        .unwrap();
+
+    for key in portion(&keys, 2, 7) {
+        state_2
+            .set(StorageKey::AccountKey(key), key[..].into())
+            .expect("Inserting k-v should succeed");
+    }
+
+    for key in portion(&keys, 3, 7) {
+        state_2
+            .set(StorageKey::AccountKey(key), key[..].into())
+            .expect("Inserting k-v should succeed");
+    }
+
+    for key in portion(&keys, 5, 7) {
+        state_2
+            .set(StorageKey::AccountKey(key), key[..].into())
+            .expect("Inserting k-v should succeed");
+    }
+
+    for key in portion(&keys, 6, 7) {
+        state_2
+            .set(StorageKey::AccountKey(key), key[..].into())
+            .expect("Inserting k-v should succeed");
+    }
+
+    let mut epoch_id_2 = H256::default();
+    epoch_id_2.as_bytes_mut()[0] = 3;
+    let _root_2 = state_2.compute_state_root().unwrap();
+    state_2.commit(epoch_id_2).unwrap();
+    // println!("root = {:?}", root);
+
+    keys.shuffle(rng);
+
+    (state_manager, state_2, intermediate_padding, keys)
+}
+
+fn generate_nonexistent_keys(
+    rng: &mut ChaChaRng, existing_keys: Vec<Vec<u8>>,
+) -> Vec<Vec<u8>> {
+    generate_account_keys(TEST_NUMBER_OF_KEYS)
+        .into_iter()
+        .filter(|_| rng.gen_bool(0.1)) // use fewer keys for now so that tests won't run forever
+        .filter(|k| !existing_keys.contains(k))
+        .collect()
+}
+
+fn get_invalid_hash(rng: &mut ChaChaRng, hash: H256) -> H256 {
+    let mut new_hash = hash;
+
+    while new_hash == hash {
+        new_hash.as_bytes_mut()[rng.gen_range(0, 32)] = rng.gen::<u8>();
+    }
+
+    new_hash
 }
 
 fn get_invalid_state_root(rng: &mut ChaChaRng, root: &StateRoot) -> StateRoot {
@@ -30,50 +208,26 @@ fn get_invalid_merkle_triplet(
     }
 }
 
-fn generate_random_state(
-    rng: &mut ChaChaRng,
-) -> (FakeStateManager, State, StateRoot, Vec<Vec<u8>>) {
-    let state_manager = new_state_manager_for_unit_test();
-    let mut state = state_manager.get_state_for_genesis_write();
+fn get_invalid_delta_padding(
+    padding: &DeltaMptKeyPadding,
+) -> DeltaMptKeyPadding {
+    let mut new_padding = padding.clone();
 
-    let mut keys: Vec<Vec<u8>> = generate_keys(TEST_NUMBER_OF_KEYS)
-        .into_iter()
-        .filter(|_| rng.gen_bool(0.1)) // use fewer keys for now so that tests won't run forever
-        .collect();
+    // note: we cannot randomly mutate as we only use portion of the padding
+    new_padding[0] = 0x00;
+    new_padding[1] = 0x00;
+    new_padding[2] = 0x00;
 
-    for key in &keys {
-        state
-            .set(StorageKey::AccountKey(key), key[..].into())
-            .expect("Inserting k-v should succeed");
-    }
-
-    let mut epoch_id = H256::default();
-    epoch_id.as_bytes_mut()[0] = 1;
-    let root = state.compute_state_root().unwrap();
-    state.commit(epoch_id).unwrap();
-
-    keys.shuffle(rng);
-
-    (state_manager, state, root.state_root, keys)
-
-    // TODO: test other key types as well, not just AccountKey
-    // TODO: insert keys through multiple snapshot periods
-}
-
-fn generate_nonexistent_keys(
-    rng: &mut ChaChaRng, existing_keys: Vec<Vec<u8>>,
-) -> Vec<Vec<u8>> {
-    generate_keys(TEST_NUMBER_OF_KEYS)
-        .into_iter()
-        .filter(|_| rng.gen_bool(0.1)) // use fewer keys for now so that tests won't run forever
-        .filter(|k| !existing_keys.contains(k))
-        .collect()
+    new_padding
 }
 
 #[test]
 fn test_valid_state_proof_for_existing_key() {
     let mut rng = get_rng_for_test();
-    let (_, state, root, keys) = generate_random_state(&mut rng);
+
+    // note: do not drop state_manager (_mgr)
+    let (_mgr, state, _, keys) = generate_random_state(&mut rng);
+    let root = state.get_state_root().unwrap().state_root;
 
     for key in keys {
         let (value, proof) = state
@@ -95,7 +249,10 @@ fn test_valid_state_proof_for_existing_key() {
 #[test]
 fn test_valid_state_proof_for_nonexistent_key() {
     let mut rng = get_rng_for_test();
-    let (_, state, root, keys) = generate_random_state(&mut rng);
+
+    // note: do not drop state_manager (_mgr)
+    let (_mgr, state, _, keys) = generate_random_state(&mut rng);
+    let root = state.get_state_root().unwrap().state_root;
     let keys = generate_nonexistent_keys(&mut rng, keys);
 
     for key in keys {
@@ -117,7 +274,10 @@ fn test_valid_state_proof_for_nonexistent_key() {
 #[test]
 fn test_invalid_state_proof() {
     let mut rng = get_rng_for_test();
-    let (_, state, root, keys) = generate_random_state(&mut rng);
+
+    // note: do not drop state_manager (_mgr)
+    let (_mgr, state, _, keys) = generate_random_state(&mut rng);
+    let root = state.get_state_root().unwrap().state_root;
 
     for key in keys {
         let (value, proof) = state
@@ -145,7 +305,10 @@ fn test_invalid_state_proof() {
 #[test]
 fn test_valid_node_merkle_proof_for_existing_key() {
     let mut rng = get_rng_for_test();
-    let (_, state, root, keys) = generate_random_state(&mut rng);
+
+    // note: do not drop state_manager (_mgr)
+    let (_mgr, state, padding, keys) = generate_random_state(&mut rng);
+    let root = state.get_state_root().unwrap().state_root;
 
     for key in keys {
         let (triplet, proof) = state
@@ -165,7 +328,7 @@ fn test_valid_node_merkle_proof_for_existing_key() {
             key,
             triplet.clone(),
             root.clone(),
-            None // TODO: use a proper padding here
+            Some(padding.clone()),
         ));
 
         // proof should be serializable
@@ -176,7 +339,10 @@ fn test_valid_node_merkle_proof_for_existing_key() {
 #[test]
 fn test_valid_node_merkle_proof_for_nonexistent_key() {
     let mut rng = get_rng_for_test();
-    let (_, state, root, keys) = generate_random_state(&mut rng);
+
+    // note: do not drop state_manager (_mgr)
+    let (_mgr, state, padding, keys) = generate_random_state(&mut rng);
+    let root = state.get_state_root().unwrap().state_root;
     let keys = generate_nonexistent_keys(&mut rng, keys);
 
     for key in keys {
@@ -195,7 +361,7 @@ fn test_valid_node_merkle_proof_for_nonexistent_key() {
             key,
             triplet.clone(),
             root.clone(),
-            None // TODO: use a proper padding here
+            Some(padding.clone()),
         ));
 
         // proof should be serializable
@@ -206,7 +372,10 @@ fn test_valid_node_merkle_proof_for_nonexistent_key() {
 #[test]
 fn test_invalid_node_merkle_proof() {
     let mut rng = get_rng_for_test();
-    let (_, state, root, keys) = generate_random_state(&mut rng);
+
+    // note: do not drop state_manager (_mgr)
+    let (_mgr, state, padding, keys) = generate_random_state(&mut rng);
+    let root = state.get_state_root().unwrap().state_root;
 
     for key in keys {
         let (triplet, proof) = state
@@ -228,19 +397,37 @@ fn test_invalid_node_merkle_proof() {
             key,
             triplet.clone(),
             invalid_root,
-            None // TODO: use a proper padding here
+            Some(padding.clone()),
         ));
 
         // checking proof with invalid triplet should fail
-        assert!(triplet.delta.is_some());
         let invalid_triplet = get_invalid_merkle_triplet(&mut rng, &triplet);
 
         assert!(!proof.is_valid_triplet(
             key,
             invalid_triplet,
             root.clone(),
-            None // TODO: use a proper padding here
+            Some(padding.clone()),
         ));
+
+        // checking proof with invalid padding should fail
+        if triplet.intermediate.is_some() {
+            let invalid_padding = get_invalid_delta_padding(&padding);
+
+            assert!(!proof.is_valid_triplet(
+                key,
+                triplet.clone(),
+                root.clone(),
+                Some(invalid_padding),
+            ));
+
+            assert!(!proof.is_valid_triplet(
+                key,
+                triplet.clone(),
+                root.clone(),
+                None,
+            ));
+        }
 
         // checking valid existence proof as non-existence proof should fail
         let empty_triplet = NodeMerkleTriplet {
@@ -253,17 +440,20 @@ fn test_invalid_node_merkle_proof() {
             key,
             empty_triplet,
             root.clone(),
-            None // TODO: use a proper padding here
+            Some(padding.clone()),
         ));
     }
 }
 
 use super::{
     super::{state::*, state_manager::*},
-    generate_keys, get_rng_for_test, new_state_manager_for_unit_test,
+    generate_account_keys, get_rng_for_test,
+    new_state_manager_for_unit_test_with_snapshot_epoch_count,
 };
 use crate::storage::tests::{FakeStateManager, TEST_NUMBER_OF_KEYS};
 use cfx_types::H256;
-use primitives::{NodeMerkleTriplet, StateRoot, StorageKey};
+use primitives::{
+    DeltaMptKeyPadding, NodeMerkleTriplet, StateRoot, StorageKey,
+};
 use rand::{seq::SliceRandom, Rng};
 use rand_chacha::ChaChaRng;
