bd7060e548fe5d61e2b1c8a64e4b629f1a974162
peilun-conflux <48905552+peilun-conflux@users.noreply.github.com>
2020-07-23 21:33:59 +0800

    Add metrics for node statistics. (#1703)

--- core/src/statistics/mod.rs
+++ core/src/statistics/mod.rs
@@ -1,6 +1,24 @@
 use crate::{consensus::ConsensusGraphStatistics, sync::SyncGraphStatistics};
+use metrics::{Gauge, GaugeUsize};
 use parking_lot::RwLock;
 use std::sync::Arc;
+lazy_static! {
+    static ref SYNC_INSERTED_HEADER_COUNT: Arc<dyn Gauge<usize>> =
+        GaugeUsize::register_with_group(
+            "graph_statistic",
+            "sync_graph_inserted_header_count"
+        );
+    static ref SYNC_INSERTED_BLOCK_COUNT: Arc<dyn Gauge<usize>> =
+        GaugeUsize::register_with_group(
+            "graph_statistic",
+            "sync_graph_inserted_block_count"
+        );
+    static ref CONSENSUS_INSERTED_BLOCK_COUNT: Arc<dyn Gauge<usize>> =
+        GaugeUsize::register_with_group(
+            "graph_statistic",
+            "consensus_graph_inserted_header_count"
+        );
+}
 
 pub type SharedStatistics = Arc<Statistics>;
 
@@ -31,11 +49,16 @@ impl Statistics {
     }
 
     pub fn inc_sync_graph_inserted_block_count(&self) {
-        self.inner.write().sync_graph.inserted_block_count += 1;
+        let mut inner = self.inner.write();
+        inner.sync_graph.inserted_block_count += 1;
+        SYNC_INSERTED_BLOCK_COUNT.update(inner.sync_graph.inserted_block_count)
     }
 
     pub fn inc_sync_graph_inserted_header_count(&self) {
-        self.inner.write().sync_graph.inserted_header_count += 1;
+        let mut inner = self.inner.write();
+        inner.sync_graph.inserted_header_count += 1;
+        SYNC_INSERTED_HEADER_COUNT
+            .update(inner.sync_graph.inserted_header_count)
     }
 
     pub fn inc_consensus_graph_processed_block_count(&self) {
@@ -47,7 +70,10 @@ impl Statistics {
     }
 
     pub fn set_consensus_graph_inserted_block_count(&self, count: usize) {
-        self.inner.write().consensus_graph.inserted_block_count = count;
+        let mut inner = self.inner.write();
+        inner.consensus_graph.inserted_block_count = count;
+        CONSENSUS_INSERTED_BLOCK_COUNT
+            .update(inner.consensus_graph.inserted_block_count)
     }
 
     pub fn get_consensus_graph_processed_block_count(&self) -> usize {
