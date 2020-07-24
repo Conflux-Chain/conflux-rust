c046eaedadcb73bf6d9a1843c5fcfb7e0091c86c
Fan Long <longfancn@gmail.com>
2020-07-24 15:44:46 +0800

    Update block reward according to CIP

--- core/src/parameters.rs
+++ core/src/parameters.rs
@@ -44,14 +44,15 @@ pub mod consensus_internal {
     /// The maximum number of blocks to be executed in each epoch
     pub const EPOCH_EXECUTED_BLOCK_BOUND: usize = 200;
     // The initial base mining reward in uCFX.
-    pub const INITIAL_BASE_MINING_REWARD_IN_UCFX: u64 = 11_300_000;
+    pub const INITIAL_BASE_MINING_REWARD_IN_UCFX: u64 = 7_000_000;
     // The ultimate base mining reward in uCFX.
-    pub const ULTIMATE_BASE_MINING_REWARD_IN_UCFX: u64 = 2_030_000;
+    pub const ULTIMATE_BASE_MINING_REWARD_IN_UCFX: u64 = 1_750_000;
+    pub const INITIAL_NO_DECAY_PERIOD: u64 = 252288000;
     // The average number of blocks mined per quarter.
     pub const MINED_BLOCK_COUNT_PER_QUARTER: u64 = 15768000;
     pub const MINING_REWARD_DECAY_RATIO_PER_QUARTER: f64 = 0.958;
     // How many quarters that the mining reward keep decaying.
-    pub const MINING_REWARD_DECAY_PERIOD_IN_QUARTER: usize = 40;
+    pub const MINING_REWARD_DECAY_PERIOD_IN_QUARTER: usize = 32;
     pub const GAS_PRICE_BLOCK_SAMPLE_SIZE: usize = 100;
     pub const GAS_PRICE_TRANSACTION_SAMPLE_SIZE: usize = 10000;
 
