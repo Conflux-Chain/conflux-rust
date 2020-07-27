1fb675fa9b9928c58cfb3696cac0e4b5552bfb1f
Ming Wu <ming@conflux-chain.org>
2020-07-27 14:37:09 +0800

    fix test

--- core/src/sync/message/message.rs
+++ core/src/sync/message/message.rs
@@ -187,6 +187,7 @@ pub fn handle_rlp_message(
 ) -> Result<bool, Error> {
     match id {
         msgid::STATUS_V2 => handle_message::<StatusV2>(ctx, rlp)?,
+        msgid::STATUS_V3 => handle_message::<StatusV3>(ctx, rlp)?,
         msgid::NEW_BLOCK => handle_message::<NewBlock>(ctx, rlp)?,
         msgid::NEW_BLOCK_HASHES => {
             handle_message::<NewBlockHashes>(ctx, rlp)?;
