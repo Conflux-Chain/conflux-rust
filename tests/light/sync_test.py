577ded677f3c0081fc3d267412e57db7fe616443
Thegaram <th307q@gmail.com>
2020-08-04 15:07:13 +0200

    update light node sync test

--- tests/light/sync_test.py
+++ tests/light/sync_test.py
@@ -2,20 +2,28 @@
 
 # allow imports from parent directory
 # source: https://stackoverflow.com/a/11158224
-import os, sys
+import os, sys, random
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
+from conflux.config import default_config
+from conflux.filter import Filter
 from conflux.rpc import RpcClient
+from conflux.utils import sha3 as keccak, priv_to_addr
+from eth_utils import encode_hex
+from test_framework.blocktools import encode_hex_0x
 from test_framework.test_framework import ConfluxTestFramework
-from test_framework.mininode import *
-from test_framework.util import *
+from test_framework.util import assert_equal, connect_nodes, sync_blocks
 
 FULLNODE0 = 0
 FULLNODE1 = 1
 LIGHTNODE = 2
 
 ERA_EPOCH_COUNT = 100
-CHAIN_LENGTH = 5000
+CHAIN_LENGTH = 100 # TODO: set higher
+BLAME_CHECK_OFFSET = 30
+
+CONTRACT_PATH = "../contracts/EventsTestContract_bytecode.dat"
+FOO_TOPIC = encode_hex_0x(keccak(b"Foo(address,uint32)"))
 
 class LightSyncTest(ConfluxTestFramework):
     def set_test_params(self):
@@ -55,14 +63,119 @@ class LightSyncTest(ConfluxTestFramework):
         self.nodes[FULLNODE1].wait_for_phase(["NormalSyncPhase"])
 
     def run_test(self):
-        self.rpc[FULLNODE0].generate_blocks(CHAIN_LENGTH)
-        sync_blocks(self.nodes)
+        # generate blocks and sync
+        self.log.info(f"Generating blocks...")
+        self.generate_blocks()
+
+        self.check_headers_synced()
+        self.check_witnesses_synced()
+
         self.log.info(f"Pass 1 -- keep up")
 
+        # crash light node (with db reset)
+        self.log.info(f"Restarting light node with db reset...")
         self.stop_node(LIGHTNODE, clean=True)
-        self.start_node(LIGHTNODE, wait_time=120, phase_to_wait=None)
-        sync_blocks(self.nodes)
+        self.start_node(LIGHTNODE, phase_to_wait=None)
+
+        self.check_headers_synced()
+        self.check_witnesses_synced()
+
         self.log.info(f"Pass 2 -- catch up")
+
+        # crash light node (no db reset)
+        self.log.info(f"Restarting light node without db reset...")
+        self.stop_node(LIGHTNODE, clean=False)
+        self.start_node(LIGHTNODE, phase_to_wait=None)
+
+        self.check_headers_synced()
+        self.check_witnesses_synced()
+
+        self.log.info(f"Pass 3 -- recover from db")
+
+    def check_headers_synced(self):
+        sync_blocks(self.nodes)
+
+    def check_witnesses_synced(self):
+        latest_epoch = self.rpc[FULLNODE0].epoch_number()
+
+        # scan all blocks for receipts
+        # we need to have all correct roots to do this
+        filter = Filter(from_epoch="earliest", to_epoch=hex(latest_epoch - BLAME_CHECK_OFFSET), topics=[FOO_TOPIC])
+
+        logs_full = self.rpc[FULLNODE0].get_logs(filter)
+        logs_light = self.rpc[LIGHTNODE].get_logs(filter)
+        assert_equal(logs_full, logs_light)
+
+    def deploy_contract(self, sender, priv_key, data_hex):
+        tx = self.rpc[FULLNODE0].new_contract_tx(receiver="", data_hex=data_hex, sender=sender, priv_key=priv_key, storage_limit=1000)
+        assert_equal(self.rpc[FULLNODE0].send_tx(tx, True), tx.hash_hex())
+        receipt = self.rpc[FULLNODE0].get_transaction_receipt(tx.hash_hex())
+        assert_equal(receipt["outcomeStatus"], "0x0")
+        address = receipt["contractCreated"]
+        return receipt, address
+
+    def generate_blocks(self):
+        priv_key = default_config["GENESIS_PRI_KEY"]
+        sender = encode_hex(priv_to_addr(priv_key))
+
+        # deploy contract
+        bytecode_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), CONTRACT_PATH)
+        assert(os.path.isfile(bytecode_file))
+        bytecode = open(bytecode_file).read()
+        _, contract_addr = self.deploy_contract(sender, priv_key, bytecode)
+        self.log.info("Contract deployed")
+
+        parent_hash = self.rpc[FULLNODE0].block_by_epoch("latest_mined")['hash']
+        nonce = self.rpc[FULLNODE0].get_nonce(sender)
+
+        hashes = []
+        num_events = 0
+        num_blamed = 0
+
+        for _ in range(0, CHAIN_LENGTH):
+            rnd = random.random()
+
+            # ~20% of all block have events
+            if rnd < 0.2:
+                tx = self.rpc[FULLNODE0].new_contract_tx(
+                    receiver=contract_addr,
+                    data_hex=encode_hex_0x(keccak(b"foo()")),
+                    sender=sender, priv_key=priv_key,
+                    storage_limit=64,
+                    nonce = nonce
+                )
+
+                parent_hash = self.rpc[FULLNODE0].generate_custom_block(parent_hash=parent_hash, txs=[tx], referee=[])
+
+                nonce += 1
+                hashes.append(tx.hash_hex())
+                num_events += 1
+
+            # ~10% of all blocks are blamed
+            elif rnd < 0.3:
+                blame_info = {}
+                blame_info['blame'] = "0x1"
+                blame_info['deferredStateRoot'] = "0x1111111111111111111111111111111111111111111111111111111111111111"
+                parent_hash = self.nodes[FULLNODE0].test_generateblockwithblameinfo(1, 0, blame_info)
+
+                num_blamed += 1
+
+            # the rest are empty
+            else:
+                parent_hash = self.rpc[FULLNODE0].generate_block_with_parent(parent_hash=parent_hash)
+
+        # TODO: generate blamed blocks with txs in them (overlap)
+
+        # mine some more blocks to keep blame check offset
+        for _ in range(0, BLAME_CHECK_OFFSET):
+            parent_hash = self.rpc[FULLNODE0].generate_custom_block(parent_hash=parent_hash, txs=[], referee=[])
+
+        # check if all txs have been executed successfully
+        for hash in hashes:
+            receipt = self.rpc[FULLNODE0].get_transaction_receipt(hash)
+            assert_equal(receipt["outcomeStatus"], "0x0")
+
+        self.log.info(f"Generated {CHAIN_LENGTH + 30} blocks with {num_events} events and {num_blamed} incorrect blocks")
 
 
 if __name__ == "__main__":
