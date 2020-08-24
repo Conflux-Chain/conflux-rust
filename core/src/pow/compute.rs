cd0e906851bb20896e50fb083639ee73f1dfc7a3
Xiaoxu Guo <ftiasch0@gmail.com>
2020-08-24 16:32:19 +0800

    added new way to compute w

--- core/src/pow/compute.rs
+++ core/src/pow/compute.rs
@@ -158,10 +158,40 @@ fn hash_compute(
     let v3 = as_u64_le(&header_hash[24..32]);
     let mut d: [u32; POW_N as usize] = [0; POW_N as usize];
 
+    fn power_mod(aa: u32, nn: u32) -> u32 {
+        let mut a = aa as u64;
+        let mut n = nn;
+        let mut result: u32 = 1;
+        while n > 0 {
+            if n % 2 == 1 {
+                result = ((result as u64) * a % POW_MOD64) as u32;
+            }
+            a = a * a % POW_MOD64;
+            n >>= 1;
+        }
+        return result;
+    }
+
+    fn gcd(a: u32, b: u32) -> u32 {
+        if b == 0 {
+          return a;
+        } else {
+          return gcd(b, a % b);
+        }
+    }
+
     let a = v0 % (POW_MOD64 - 1) + 1;
     let b = v1 % POW_MOD64;
     let c = v2 % POW_MOD64;
-    let w = v3 % POW_MOD64;
+    let mut e = (v3 % (POW_MOD64 - 2) + 1) as u32;
+    loop {
+       let g = gcd(e, POW_MOD - 1);
+       if g == 1 {
+         break
+       }
+       e /= g
+    }
+    let w = power_mod(POW_MOD_B, e);
 
     let warp_id = nonce / POW_WARP_SIZE;
     for i in 0..POW_WARP_SIZE {
