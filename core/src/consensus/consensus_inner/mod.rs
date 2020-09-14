db8929a58193b93dc2c442d1911f3657f99273c3
Peilun Li <peilun.li@conflux-chain.org>
2020-09-14 14:02:01 +0800

    Fix issue #1830.

--- core/src/consensus/consensus_inner/mod.rs
+++ core/src/consensus/consensus_inner/mod.rs
@@ -1204,10 +1204,12 @@ impl ConsensusGraphInner {
             }
             my_past.add(index as u32);
             let idx_parent = self.arena[index].parent;
-            if anticone.contains(idx_parent as u32)
-                || self.arena[idx_parent].era_block == NULL
-            {
-                queue.push_back(idx_parent);
+            if idx_parent != NULL {
+                if anticone.contains(idx_parent as u32)
+                    || self.arena[idx_parent].era_block == NULL
+                {
+                    queue.push_back(idx_parent);
+                }
             }
             for referee in &self.arena[index].referees {
                 if anticone.contains(*referee as u32)
