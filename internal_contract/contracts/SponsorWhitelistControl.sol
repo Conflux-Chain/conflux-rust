da119ef2ce5134bcf9fa0b04f68f605f85b6bb58
ChenxingLi <lylcx2007@gmail.com>
2020-09-24 17:07:24 +0800

    Enable internal interface v2

--- internal_contract/contracts/SponsorWhitelistControl.sol
+++ internal_contract/contracts/SponsorWhitelistControl.sol
@@ -1,28 +1,83 @@
 pragma solidity >=0.4.15;
 
 contract SponsorWhitelistControl {
-    // ------------------------------------------------------------------------
-    // Someone will sponsor the gas cost for contract `contract_addr` with an
-    // `upper_bound` for a single transaction.
-    // ------------------------------------------------------------------------
-    function set_sponsor_for_gas(address contract_addr, uint upper_bound) public payable {
-    }
+    /*** Query Functions ***/
+    /**
+     * @dev get gas sponsor address of specific contract
+     * @param contractAddr The address of the sponsored contract
+     */
+    function getSponsorForGas(address contractAddr) public returns (address) {}
+
+    /**
+     * @dev get current Sponsored Balance for gas
+     * @param contractAddr The address of the sponsored contract
+     */
+    function getSponsoredBalanceForGas(address contractAddr) public returns (uint) {}
+
+    /**
+     * @dev get current Sponsored Gas fee upper bound
+     * @param contractAddr The address of the sponsored contract
+     */
+    function getSponsoredGasFeeUpperBound(address contractAddr) public returns (uint) {}
+
+    /**
+     * @dev get collateral sponsor address
+     * @param contractAddr The address of the sponsored contract
+     */
+    function getSponsorForCollateral(address contractAddr) public returns (address) {}
+
+    /**
+     * @dev get current Sponsored Balance for collateral
+     * @param contractAddr The address of the sponsored contract
+     */
+    function getSponsoredBalanceForCollateral(address contractAddr) public returns (uint) {}
+
+    /**
+     * @dev check if a user is in a contract's whitelist
+     * @param contractAddr The address of the sponsored contract
+     * @param user The address of contract user
+     */
+    function isWhitelisted(address contractAddr, address user) public returns (bool) {}
+
+    /**
+     * @dev check if all users are in a contract's whitelist
+     * @param contractAddr The address of the sponsored contract
+     */
+    function isAllWhitelisted(address contractAddr) public returns (bool) {}
+
+    /*** for contract admin only **/
+    /**
+     * @dev contract admin add user to whitelist
+     * @param contractAddr The address of the sponsored contract
+     * @param addresses The user address array
+     */
+    function addPrivilegeByAdmin(address contractAddr, address[] memory addresses) public {}
+
+    /**
+     * @dev contract admin remove user from whitelist
+     * @param contractAddr The address of the sponsored contract
+     * @param addresses The user address array
+     */
+    function removePrivilegeByAdmin(address contractAddr, address[] memory addresses) public {}
 
     // ------------------------------------------------------------------------
-    // Someone will sponsor the storage collateral for contract `contract_addr`.
+    // Someone will sponsor the gas cost for contract `contractAddr` with an
+    // `upper_bound` for a single transaction.
     // ------------------------------------------------------------------------
-    function set_sponsor_for_collateral(address contract_addr) public payable {
-    }
+    function setSponsorForGas(address contractAddr, uint upper_bound) public payable {}
+
+    // ------------------------------------------------------------------------
+    // Someone will sponsor the storage collateral for contract `contractAddr`.
+    // ------------------------------------------------------------------------
+    function setSponsorForCollateral(address contractAddr) public payable {}
 
     // ------------------------------------------------------------------------
     // Add commission privilege for address `user` to some contract.
     // ------------------------------------------------------------------------
-    function add_privilege(address[] memory) public {
-    }
+    function addPrivilege(address[] memory) public {}
 
     // ------------------------------------------------------------------------
     // Remove commission privilege for address `user` from some contract.
     // ------------------------------------------------------------------------
-    function remove_privilege(address[] memory) public {
-    }
+    function removePrivilege(address[] memory) public {}
 }
--- internal_contract/contracts/Staking.sol
+++ internal_contract/contracts/Staking.sol
@@ -1,12 +1,39 @@
 pragma solidity >=0.4.15;
 
 contract Staking {
-    function deposit(uint amount) external {
-    }
+    /*** Query Functions ***/
+    /**
+     * @dev get user's staking balance
+     * @param user The address of specific user
+     */
+    function getStakingBalance(address user) public returns (uint) {}
 
-    function withdraw(uint amount) external {
-    }
+    /**
+     * @dev get user's locked staking balance at given blockNumber
+     * @param user The address of specific user
+     * @param blockNumber The blockNumber as index.
+     */
+    // ------------------------------------------------------------------------
+    // Note: if the blockNumber is less than the current block number, function
+    // will return current locked staking balance.
+    // ------------------------------------------------------------------------
+    function getLockedStakingBalance(address user, uint blockNumber) public returns (uint) {}
 
-    function vote_lock(uint amount, uint unlock_block_number) external {
-    }
+
+    /**
+     * @dev get user's vote power staking balance at given blockNumber
+     * @param user The address of specific user
+     * @param blockNumber The blockNumber as index.
+     */
+    // ------------------------------------------------------------------------
+    // Note: if the blockNumber is less than the current block number, function
+    // will return current vote power.
+    // ------------------------------------------------------------------------
+    function getVotePower(address user, uint blockNumber) public returns (uint) {}
+
+    function deposit(uint amount) external {}
+
+    function withdraw(uint amount) external {}
+
+    function voteLock(uint amount, uint unlockBlockNumber) external {}
 }
