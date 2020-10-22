Conflux-Chain/conflux-rust

commit 26f52ed6d9970f20a7b679588dbc5ee1ce099c93

Let AdminContract regard sender (other than original sender) as requester (#1887)
* Let AdminContract regard sender as requester

* Admin must be normal address

core/src/executive/internal_contract/impls/admin.rs
@@ -54,7 +54,7 @@ pub fn set_admin(
 ) -> vm::Result<()>
 {
     Ok(state.set_admin(
-        &params.original_sender,
+        &params.sender,
         &contract_address,
         &new_admin_address,
     )?)
@@ -69,7 +69,7 @@ pub fn destroy(
 {
     debug!("contract_address={:?}", contract_address);
 
-    let requester = &params.original_sender;
+    let requester = &params.sender;
     let admin = state.admin(&contract_address)?;
     if admin == *requester {
         suicide(&contract_address, &admin, state, spec, substate)

commit f149e3406fb820aeb51bb297cea903e1402fae8d

Allow clear admin in contract creation.
The ability to do so regressed from #1887.

Add python tests to fully test the SetAdmin code:
- Clear admin after the contract creation by non-admin is not allowed;
- Set admin in a transaction sent by the admin indirectly through a call is not allowed;
- Clear admin in a contract created through create2factory is allowed (SetAdmin requester is contract);
- Clear admin in contract creation is allowed (SetAdmin requester is caller);
- Clear admin in a subcall of contract creation is not allowed.

client/src/rpc/types/receipt.rs
@@ -31,7 +31,7 @@ pub struct Receipt {
     pub gas_used: U256,
     /// The gas fee charged in the execution of the transaction.
     pub gas_fee: U256,
-    /// Address of contracts created during execution of transaction.
+    /// Address of contract created if the transaction action is create.
     pub contract_created: Option<Address>,
     /// Array of log objects, which this transaction generated.
     pub logs: Vec<Log>,

core/src/executive/executive.rs
@@ -178,6 +178,7 @@ impl<'a> CallCreateExecutive<'a> {
         params: ActionParams, env: &'a Env, machine: &'a Machine,
         spec: &'a Spec, factory: &'a VmFactory, depth: usize,
         stack_depth: usize, parent_static_flag: bool,
+        parent_contract_in_creation: Option<Address>,
         internal_contract_map: &'a InternalContractMap,
         contracts_in_callstack: Rc<RefCell<CallStackInfo>>,
     ) -> Self
@@ -186,7 +187,7 @@ impl<'a> CallCreateExecutive<'a> {
             "Executive::call(params={:?}) self.env={:?}, parent_static={}",
             params,
             env,
-            parent_static_flag
+            parent_static_flag,
         );
 
         let gas = params.gas;
@@ -213,14 +214,22 @@ impl<'a> CallCreateExecutive<'a> {
             );
             CallCreateExecutiveKind::CallInternalContract(
                 params,
-                Substate::with_call_stack(contracts_in_callstack),
+                Substate::with_call_stack(contracts_in_callstack)
+                    .update_contract_in_creation_call(
+                        parent_contract_in_creation,
+                        /* is_internal_contract = */ true,
+                    ),
             )
         } else {
             if params.code.is_some() {
                 trace!("ExecCall");
                 CallCreateExecutiveKind::ExecCall(
                     params,
-                    Substate::with_call_stack(contracts_in_callstack),
+                    Substate::with_call_stack(contracts_in_callstack)
+                        .update_contract_in_creation_call(
+                            parent_contract_in_creation,
+                            /* is_internal_contract = */ false,
+                        ),
                 )
             } else {
                 trace!("Transfer");
@@ -259,10 +268,12 @@ impl<'a> CallCreateExecutive<'a> {
         );
 
         let gas = params.gas;
+        let contract_in_creation = params.code_address;
 
         let kind = CallCreateExecutiveKind::ExecCreate(
             params,
-            Substate::with_call_stack(contracts_in_callstack),
+            Substate::with_call_stack(contracts_in_callstack)
+                .set_contract_in_creation_create(contract_in_creation),
         );
 
         Self {
@@ -499,7 +510,9 @@ impl<'a> CallCreateExecutive<'a> {
     pub fn exec(
         mut self, state: &mut State, substate: &mut Substate,
     ) -> ExecutiveTrapResult<'a, FinalizationResult> {
-        match self.kind {
+        let kind =
+            std::mem::replace(&mut self.kind, CallCreateExecutiveKind::Moved);
+        match kind {
             CallCreateExecutiveKind::Transfer(ref params) => {
                 assert!(!self.is_create);
 
@@ -645,7 +658,6 @@ impl<'a> CallCreateExecutive<'a> {
                     self.internal_contract_map,
                 );
                 let out = Ok(result.finalize(context));
-                self.kind = CallCreateExecutiveKind::Moved;
                 self.enact_output(
                     out,
                     origin,
@@ -708,7 +720,6 @@ impl<'a> CallCreateExecutive<'a> {
                     }
                 };
 
-                self.kind = CallCreateExecutiveKind::Moved;
                 self.enact_output(
                     out,
                     origin,
@@ -779,7 +790,6 @@ impl<'a> CallCreateExecutive<'a> {
                     }
                 };
 
-                self.kind = CallCreateExecutiveKind::Moved;
                 self.enact_output(
                     out,
                     origin,
@@ -1002,7 +1012,12 @@ impl<'a> CallCreateExecutive<'a> {
                         None => return val,
                     }
                 }
-                Some((_, _, Err(TrapError::Call(subparams, resume)))) => {
+                Some((_, _, Err(TrapError::Call(subparams, mut resume)))) => {
+                    let maybe_parent_contract_in_creation = resume
+                        .unconfirmed_substate()
+                        .map_or(None, |substate| {
+                            substate.contract_in_creation().cloned()
+                        });
                     let sub_exec = CallCreateExecutive::new_call_raw(
                         subparams,
                         resume.env,
@@ -1012,6 +1027,7 @@ impl<'a> CallCreateExecutive<'a> {
                         resume.depth + 1,
                         resume.stack_depth,
                         resume.static_flag,
+                        maybe_parent_contract_in_creation,
                         resume.internal_contract_map,
                         top_substate.contracts_in_callstack.clone(),
                     );
@@ -1172,6 +1188,7 @@ impl<'a> Executive<'a> {
             self.depth,
             stack_depth,
             self.static_flag,
+            None,
             self.internal_contract_map,
             substate.contracts_in_callstack.clone(),
         )

core/src/executive/internal_contract/contracts/admin.rs
@@ -44,10 +44,16 @@ rename_interface! {
 impl ExecutionTrait for SetAdmin {
     fn execute_inner(
         &self, inputs: (Address, Address), params: &ActionParams, _spec: &Spec,
-        state: &mut State, _substate: &mut Substate,
+        state: &mut State, substate: &mut Substate,
     ) -> vm::Result<()>
     {
-        set_admin(inputs.0, inputs.1, params, state)
+        set_admin(
+            inputs.0,
+            inputs.1,
+            substate.contract_in_creation(),
+            params,
+            state,
+        )
     }
 }
 

core/src/executive/internal_contract/impls/admin.rs
@@ -3,7 +3,7 @@
 // See http://www.gnu.org/licenses/
 
 use crate::{
-    state::{State, Substate},
+    state::{OverlayAccount, RequireCache, State, Substate},
     vm::{self, ActionParams, Spec},
 };
 use cfx_types::{address_util::AddressUtil, Address};
@@ -50,14 +50,35 @@ pub fn suicide(
 /// `new_admin_address`
 pub fn set_admin(
     contract_address: Address, new_admin_address: Address,
-    params: &ActionParams, state: &mut State,
+    contract_in_creation: Option<&Address>, params: &ActionParams,
+    state: &mut State,
 ) -> vm::Result<()>
 {
-    Ok(state.set_admin(
-        &params.sender,
+    let requester = &params.sender;
+    debug!(
+        "set_admin requester {:?} contract {:?}, \
+         new_admin {:?}, contract_in_creation {:?}",
+        requester, contract_address, new_admin_address, contract_in_creation,
+    );
+    let fn_can_set_admin = |acc: &OverlayAccount| {
+        acc.is_contract()
+            // Allow set admin if requester matches or in contract creation to clear admin.
+            && (acc.admin() == requester
+                || contract_in_creation == Some(&contract_address) && new_admin_address.is_null_address())
+            // Only allow user account to be admin, if not to clear admin.
+            && (new_admin_address.is_user_account_address()
+                || new_admin_address.is_null_address())
+    };
+    if state.ensure_account_loaded(
         &contract_address,
-        &new_admin_address,
-    )?)
+        RequireCache::None,
+        |acc| acc.map_or(false, fn_can_set_admin),
+    )? {
+        debug!("set_admin to {:?}", new_admin_address);
+        // Admin is cleared by set new_admin_address to null address.
+        state.set_admin(&contract_address, &new_admin_address)?;
+    }
+    Ok(())
 }
 
 /// Implementation of `destroy(address)`.

core/src/executive/internal_contract/mod.rs
@@ -6,6 +6,10 @@ mod contracts;
 pub mod function;
 mod impls;
 
+pub use self::{contracts::InternalContractMap, impls::suicide};
+pub use solidity_abi::ABIDecodeError;
+
+use self::contracts::SolFnTable;
 use crate::{
     bytes::Bytes,
     hash::keccak,
@@ -14,12 +18,6 @@ use crate::{
 };
 use cfx_types::{Address, H256};
 use std::sync::Arc;
-
-use self::contracts::SolFnTable;
-
-pub use self::{contracts::InternalContractMap, impls::suicide};
-
-pub use solidity_abi::ABIDecodeError;
 
 lazy_static! {
     static ref INTERNAL_CONTRACT_CODE: Arc<Bytes> =

core/src/genesis.rs
@@ -185,11 +185,7 @@ pub fn genesis_block(
             &genesis_transactions[1].as_ref().data,
         );
         state
-            .set_admin(
-                &genesis_account_address,
-                &create2factory_contract_address,
-                &Address::zero(),
-            )
+            .set_admin(&create2factory_contract_address, &Address::zero())
             .expect("");
     }
     state.clean_account(&genesis_account_address);

core/src/state/mod.rs
@@ -49,7 +49,7 @@ mod state_tests;
 mod substate;
 
 #[derive(Copy, Clone)]
-enum RequireCache {
+pub enum RequireCache {
     None,
     Code,
     DepositList,
@@ -570,23 +570,10 @@ impl<StateDbStorage: StorageStateTrait> StateGeneric<StateDbStorage> {
     }
 
     pub fn set_admin(
-        &mut self, requester: &Address, contract_address: &Address,
-        admin: &Address,
-    ) -> DbResult<()>
-    {
-        let fn_can_set_admin = |acc: &OverlayAccount| {
-            acc.is_contract()
-                && acc.admin() == requester
-                && (admin.is_user_account_address() || admin.is_null_address())
-        };
-        if self.ensure_account_loaded(
-            contract_address,
-            RequireCache::None,
-            |acc| acc.map_or(false, fn_can_set_admin),
-        )? {
-            self.require_exists(&contract_address, false)?
-                .set_admin(admin);
-        }
+        &mut self, contract_address: &Address, admin: &Address,
+    ) -> DbResult<()> {
+        self.require_exists(&contract_address, false)?
+            .set_admin(admin);
         Ok(())
     }
 
@@ -1509,7 +1496,7 @@ impl<StateDbStorage: StorageStateTrait> StateGeneric<StateDbStorage> {
         }
     }
 
-    fn ensure_account_loaded<F, U>(
+    pub fn ensure_account_loaded<F, U>(
         &self, address: &Address, require: RequireCache, f: F,
     ) -> DbResult<U>
     where F: Fn(Option<&OverlayAccount>) -> U {

core/src/state/substate.rs
@@ -104,6 +104,14 @@ pub struct Substate {
     /// and passed back to caller when callee returns,
     /// through mem::swap.
     pub contracts_in_callstack: Rc<RefCell<CallStackInfo>>,
+    /// The contract which is being constructed. The contract address is set at
+    /// the beginning of the constructor. When an internal contract is called
+    /// from the contract constructor, the contract_in_creation is inherited
+    /// from the constructor as the contract address.
+    /// The contract address is set to None when calling a normal account.
+    /// When a new contract constructor is called, the contract_in_creation
+    /// address is set to the new contract address.
+    contract_in_creation: Option<Address>,
 }
 
 impl Substate {
@@ -123,6 +131,42 @@ impl Substate {
     #[inline]
     pub fn pop_callstack(&self) {
         self.contracts_in_callstack.borrow_mut().pop();
+    }
+
+    // Returns whether we are running the contract creation code in its own
+    // frame, or in its immediate call of Builtin / InternalContract.
+    // Note that if the constructor calls other contract, the return value will
+    // be false.
+    pub fn contract_in_creation(&self) -> Option<&Address> {
+        debug!("contract_in_creation {:?}", self.contract_in_creation);
+        self.contract_in_creation.as_ref()
+    }
+
+    pub fn update_contract_in_creation_call(
+        mut self, parent_contract_in_creation: Option<Address>,
+        is_internal_contract: bool,
+    ) -> Self
+    {
+        debug!(
+            "update_contract_in_creation_call {:?}, is_internal_contract {}",
+            parent_contract_in_creation, is_internal_contract
+        );
+        if is_internal_contract {
+            self.contract_in_creation = parent_contract_in_creation;
+        } else {
+            self.contract_in_creation = None;
+        }
+
+        self
+    }
+
+    pub fn set_contract_in_creation_create(
+        mut self, contract_in_creation: Address,
+    ) -> Self {
+        debug!("set_contract_in_creation_call {:?}", contract_in_creation);
+        self.contract_in_creation = Some(contract_in_creation);
+
+        self
     }
 
     /// Merge secondary substate `s` into self, accruing each element

tests/admin_at_creation_test.py
@@ -1,0 +1,125 @@
+#!/usr/bin/env python3
+from eth_utils import decode_hex
+
+from conflux.rpc import RpcClient
+from conflux.transactions import CONTRACT_DEFAULT_GAS, COLLATERAL_UNIT_IN_DRIP
+from conflux.utils import encode_hex, priv_to_addr, parse_as_int
+from test_framework.block_gen_thread import BlockGenThread
+from test_framework.blocktools import create_transaction, encode_hex_0x
+from test_framework.test_framework import ConfluxTestFramework
+from test_framework.mininode import *
+from test_framework.util import *
+from web3 import Web3
+
+class ClearAdminTest(ConfluxTestFramework):
+    def set_test_params(self):
+        self.num_nodes = 8
+
+    def setup_network(self):
+        self.setup_nodes()
+        connect_sample_nodes(self.nodes, self.log)
+        sync_blocks(self.nodes)
+        self.rpc = RpcClient(self.nodes[0])
+
+    def run_test(self):
+        start_p2p_connection(self.nodes)
+        block_gen_thread = BlockGenThread(self.nodes, self.log)
+        block_gen_thread.start()
+
+        gas_price = 1
+        gas = CONTRACT_DEFAULT_GAS
+        genesis_key = default_config["GENESIS_PRI_KEY"]
+        genesis_addr = encode_hex_0x(priv_to_addr(genesis_key))
+        nonce = 0
+        test_account_key = default_config["GENESIS_PRI_KEY_2"]
+        test_account_addr = encode_hex_0x(priv_to_addr(test_account_key))
+        null_addr = "0x0000000000000000000000000000000000000000"
+
+        file_dir = os.path.dirname(os.path.realpath(__file__))
+        control_contract_file_path =os.path.join(file_dir, "..", "internal_contract", "metadata", "AdminControl.json")
+        control_contract_dict = json.loads(open(control_contract_file_path, "r").read())
+        admin_control_contract = get_contract_instance(contract_dict=control_contract_dict)
+        admin_control_contract_addr = "0x0888000000000000000000000000000000000000"
+
+        # Deploy a new instance of the create2factory other than the genesis block,
+        # so that the admin is the genesis_addr, in order to test hijackAdmin function
+        # in clear_admin_test_contract_addr.sol.
+        self.tx_conf = {"from":Web3.toChecksumAddress(genesis_addr), "nonce":int_to_hex(nonce), "gas":int_to_hex(gas), "gasPrice":int_to_hex(gas_price), "chainId":0}
+        create2factory = get_contract_instance(
+            abi_file = os.path.join(file_dir, "contracts/create2factory_abi.json"),
+            bytecode_file = os.path.join(file_dir, "contracts/create2factory_bytecode.dat"),
+        )
+        raw_create = create2factory.constructor().buildTransaction(self.tx_conf)
+        tx_data = decode_hex(raw_create["data"])
+        tx_create = create_transaction(pri_key=genesis_key, receiver=b'', nonce=nonce, gas_price=gas_price, data=tx_data, gas=gas, value=0, storage_limit=1920)
+        nonce += 1
+        self.rpc.send_tx(tx_create, True)
+        receipt = self.rpc.get_transaction_receipt(tx_create.hash_hex())
+        create2factory_addr = receipt['contractCreated']
+
+        # Clear admin by non-admin (fail)
+        self.log.info("Test unable to clear admin by non-admin.")
+        set_admin = admin_control_contract.functions \
+            .setAdmin(Web3.toChecksumAddress(create2factory_addr), null_addr) \
+            .buildTransaction({"to":admin_control_contract_addr, **self.tx_conf})
+        tx_data = set_admin["data"]
+        self.call_contract(test_account_addr, test_account_key, admin_control_contract_addr, tx_data)
+        assert_equal(self.rpc.get_admin(create2factory_addr), genesis_addr)
+
+        clear_admin_test_contract = get_contract_instance(
+            abi_file = os.path.join(file_dir, "contracts/clear_admin_at_creation.json"),
+            bytecode_file = os.path.join(file_dir, "contracts/clear_admin_at_creation.bytecode"),
+        )
+
+        self.log.info("Test contract creation by itself")
+        raw_create = clear_admin_test_contract.constructor().buildTransaction(self.tx_conf)
+        tx_data = decode_hex(raw_create["data"])
+        tx_create = create_transaction(pri_key=genesis_key, receiver=b'', nonce=nonce, gas_price=gas_price, data=tx_data, gas=gas, value=0, storage_limit=1920)
+        nonce += 1
+        self.rpc.send_tx(tx_create, True)
+        receipt = self.rpc.get_transaction_receipt(tx_create.hash_hex())
+        address = receipt["contractCreated"]
+        self.log.info("  contract created at %s" % address)
+        assert(address is not None)
+
+        self.log.info("Test clear admin at contract creation through create2factory")
+        create_data = raw_create["data"]
+        salt = 0
+        data = create2factory.functions.deploy(create_data, salt).buildTransaction({"to":create2factory_addr, **self.tx_conf})["data"]
+        # Compute the contract address.
+        clear_admin_test_contract_addr = Web3.toChecksumAddress("0x" + self.rpc.call(create2factory_addr, data)[-40:])
+        # Deploy the contract.
+        self.call_contract(genesis_addr, genesis_key, create2factory_addr, data, value=0)
+        assert_equal(self.rpc.get_admin(clear_admin_test_contract_addr), null_addr)
+        # The owner of create2factory_addr isn't hijacked.
+        self.log.info("Test unable to hijack set admin.")
+        assert_equal(self.rpc.get_admin(create2factory_addr), genesis_addr)
+
+        self.log.info("Test unable to hijack owner through deployAndHijackAdmin")
+        # Create a new contract through deployAndHijackAdmin.
+        new_contract_to_deploy = get_contract_instance(
+            abi_file = os.path.join(file_dir, "contracts/blackhole.json"),
+            bytecode_file = os.path.join(file_dir, "contracts/blackhole.bytecode"),
+        )
+        self.tx_conf["nonce"] = 1
+        self.tx_conf["from"] = Web3.toChecksumAddress(test_account_addr)
+        new_raw_create = new_contract_to_deploy.constructor().buildTransaction(self.tx_conf)
+        create_data = new_raw_create["data"]
+        data = clear_admin_test_contract.functions.deployAndHijackAdmin(create_data).buildTransaction({"to":clear_admin_test_contract_addr, **self.tx_conf})["data"]
+        new_contract_addr = "0x" + self.rpc.call(clear_admin_test_contract_addr, data)[-40:]
+        self.call_contract(test_account_addr, test_account_key, clear_admin_test_contract_addr, data, value=123)
+        # Check owner of the new contract isn't the "evil address"
+        assert_equal(self.rpc.get_admin(new_contract_addr), test_account_addr)
+
+        self.log.info("Pass")
+
+    def call_contract(self, sender, priv_key, contract, data_hex, value=0, storage_limit=10000, gas=CONTRACT_DEFAULT_GAS):
+        c0 = self.rpc.get_collateral_for_storage(sender)
+        tx = self.rpc.new_contract_tx(receiver=contract, data_hex=data_hex, sender=sender, priv_key=priv_key, value=value, storage_limit=storage_limit, gas=gas)
+        assert_equal(self.rpc.send_tx(tx, True), tx.hash_hex())
+        receipt = self.rpc.get_transaction_receipt(tx.hash_hex())
+        self.log.info("call_contract storage collateral change={}".format((self.rpc.get_collateral_for_storage(sender) - c0) // COLLATERAL_UNIT_IN_DRIP))
+        return receipt
+
+if __name__ == "__main__":
+    ClearAdminTest().main()

tests/contracts/blackhole.bytecode
@@ -1,0 +1,1 @@
+608060405260458060116000396000f3fe608060405236600a57005b600080fdfea2646970667358221220204204cf5868fda7bc6c0023413db96ec2ee6b82205abe49450a0cd3f24507a764736f6c63430007010033

tests/contracts/blackhole.sol
@@ -1,0 +1,10 @@
+pragma solidity >=0.7.0;
+
+contract BlackHole {
+    constructor() payable {
+    }
+
+    receive() external payable {
+        // Accept payments.
+    }
+}

tests/contracts/clear_admin_at_creation.bytecode
@@ -1,0 +1,1 @@
+608060405234801561001057600080fd5b50600073088800000000000000000000000000000000000090506100386105f360201b60201c565b73100000000000000000000000000000000000000073ffffffffffffffffffffffffffffffffffffffff168173ffffffffffffffffffffffffffffffffffffffff166364efb22b306040518263ffffffff1660e01b8152600401808273ffffffffffffffffffffffffffffffffffffffff16815260200191505060206040518083038186803b1580156100ca57600080fd5b505afa1580156100de573d6000803e3d6000fd5b505050506040513d60208110156100f457600080fd5b810190808051906020019092919050505073ffffffffffffffffffffffffffffffffffffffff16141561018f576040517f08c379a00000000000000000000000000000000000000000000000000000000081526004018080602001828103825260158152602001807f726571756972652061646d696e20213d206576696c000000000000000000000081525060200191505060405180910390fd5b3073ffffffffffffffffffffffffffffffffffffffff166040516024016040516020818303038152906040527f8b0b3140000000000000000000000000000000000000000000000000000000007bffffffffffffffffffffffffffffffffffffffffffffffffffffffff19166020820180517bffffffffffffffffffffffffffffffffffffffffffffffffffffffff83818316178352505050506040518082805190602001908083835b6020831061025c5780518252602082019150602081019050602083039250610239565b6001836020036101000a0380198251168184511680821785525050505050509050019150506000604051808303816000865af19150503d80600081146102be576040519150601f19603f3d011682016040523d82523d6000602084013e6102c3565b606091505b505050600073ffffffffffffffffffffffffffffffffffffffff168173ffffffffffffffffffffffffffffffffffffffff166364efb22b306040518263ffffffff1660e01b8152600401808273ffffffffffffffffffffffffffffffffffffffff16815260200191505060206040518083038186803b15801561034557600080fd5b505afa158015610359573d6000803e3d6000fd5b505050506040513d602081101561036f57600080fd5b810190808051906020019092919050505073ffffffffffffffffffffffffffffffffffffffff16141561040a576040517f08c379a00000000000000000000000000000000000000000000000000000000081526004018080602001828103825260158152602001807f726571756972652061646d696e20213d206e756c6c000000000000000000000081525060200191505060405180910390fd5b8073ffffffffffffffffffffffffffffffffffffffff1663c55b6bb73060006040518363ffffffff1660e01b8152600401808373ffffffffffffffffffffffffffffffffffffffff1681526020018273ffffffffffffffffffffffffffffffffffffffff16815260200192505050600060405180830381600087803b15801561049257600080fd5b505af11580156104a6573d6000803e3d6000fd5b50505050600073ffffffffffffffffffffffffffffffffffffffff168173ffffffffffffffffffffffffffffffffffffffff166364efb22b306040518263ffffffff1660e01b8152600401808273ffffffffffffffffffffffffffffffffffffffff16815260200191505060206040518083038186803b15801561052957600080fd5b505afa15801561053d573d6000803e3d6000fd5b505050506040513d602081101561055357600080fd5b810190808051906020019092919050505073ffffffffffffffffffffffffffffffffffffffff16146105ed576040517f08c379a00000000000000000000000000000000000000000000000000000000081526004018080602001828103825260158152602001807f726571756972652061646d696e203d3d206e756c6c000000000000000000000081525060200191505060405180910390fd5b506106c2565b600073088800000000000000000000000000000000000090508073ffffffffffffffffffffffffffffffffffffffff1663c55b6bb7337310000000000000000000000000000000000000006040518363ffffffff1660e01b8152600401808373ffffffffffffffffffffffffffffffffffffffff1681526020018273ffffffffffffffffffffffffffffffffffffffff16815260200192505050600060405180830381600087803b1580156106a757600080fd5b505af11580156106bb573d6000803e3d6000fd5b5050505050565b6104d2806106d16000396000f3fe60806040526004361061003f5760003560e01c80638b0b3140146100445780638ef99ff51461005b578063b61b2fe914610140578063ce0a10db1461014a575b600080fd5b34801561005057600080fd5b5061005961018b565b005b6101146004803603602081101561007157600080fd5b810190808035906020019064010000000081111561008e57600080fd5b8201836020820111156100a057600080fd5b803590602001918460018302840111640100000000831117156100c257600080fd5b91908080601f016020809104026020016040519081016040528093929190818152602001838380828437600081840152601f19601f820116905080830192505050505050509192919290505050610247565b604051808273ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390f35b6101486103b5565b005b34801561015657600080fd5b5061015f610484565b604051808273ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390f35b600073088800000000000000000000000000000000000090508073ffffffffffffffffffffffffffffffffffffffff1663c55b6bb73060006040518363ffffffff1660e01b8152600401808373ffffffffffffffffffffffffffffffffffffffff1681526020018273ffffffffffffffffffffffffffffffffffffffff16815260200192505050600060405180830381600087803b15801561022c57600080fd5b505af1158015610240573d6000803e3d6000fd5b5050505050565b600080600060019050600034905084516020860182f09250823b61026a57600091505b816102dd576040517f08c379a000000000000000000000000000000000000000000000000000000000815260040180806020018281038252600d8152602001807f637265617465206661696c65640000000000000000000000000000000000000081525060200191505060405180910390fd5b600073088800000000000000000000000000000000000090508073ffffffffffffffffffffffffffffffffffffffff1663c55b6bb7857310000000000000000000000000000000000000006040518363ffffffff1660e01b8152600401808373ffffffffffffffffffffffffffffffffffffffff1681526020018273ffffffffffffffffffffffffffffffffffffffff16815260200192505050600060405180830381600087803b15801561039157600080fd5b505af11580156103a5573d6000803e3d6000fd5b5050505083945050505050919050565b600073088800000000000000000000000000000000000090508073ffffffffffffffffffffffffffffffffffffffff1663c55b6bb7337310000000000000000000000000000000000000006040518363ffffffff1660e01b8152600401808373ffffffffffffffffffffffffffffffffffffffff1681526020018273ffffffffffffffffffffffffffffffffffffffff16815260200192505050600060405180830381600087803b15801561046957600080fd5b505af115801561047d573d6000803e3d6000fd5b5050505050565b7310000000000000000000000000000000000000008156fea2646970667358221220aa738509566a2a1c4cd20db9f8a309bd6961f8975b6cfe3d2cd6e0063550a9e264736f6c63430007010033

tests/contracts/clear_admin_at_creation.sol
@@ -1,0 +1,50 @@
+pragma solidity >=0.7.0;
+
+import "internal/AdminControl.sol";
+
+contract AdminTestContract {
+    address public constant EVIL_ADDR = 0x1000000000000000000000000000000000000000;
+    address constant ADMIN_CONTROL_ADDR = 0x0888000000000000000000000000000000000000;
+
+    constructor() {
+        AdminControl ac = AdminControl(ADMIN_CONTROL_ADDR);
+        // This should fail
+        hijackAdmin();
+        require(ac.getAdmin(address(this)) != EVIL_ADDR, "require admin != evil");
+        // This should fail
+        address(this).call(abi.encodeWithSignature("clearAdmin()"));
+        require(ac.getAdmin(address(this)) != address(0), "require admin != null");
+        // This should succeed
+        ac.setAdmin(address(this), address(0));
+        require(ac.getAdmin(address(this)) == address(0), "require admin == null");
+    }
+
+    function deployAndHijackAdmin(bytes memory code) external payable returns (address) {
+        address addr;
+        bool success = true;
+        uint value = msg.value;
+        assembly {
+            addr := create(value, add(code, 0x20), mload(code))
+            if iszero(extcodesize(addr)) {
+              success := 0
+            }
+        }
+        require(success, "create failed");
+
+        // Hijack the admin to an evil party.
+        AdminControl ac = AdminControl(ADMIN_CONTROL_ADDR);
+        ac.setAdmin(addr, EVIL_ADDR);
+        return addr;
+    }
+
+    function clearAdmin() public {
+        AdminControl ac = AdminControl(ADMIN_CONTROL_ADDR);
+        ac.setAdmin(address(this), address(0));
+    }
+
+    function hijackAdmin() public payable {
+        // Hijack the admin to an evil party.
+        AdminControl ac = AdminControl(ADMIN_CONTROL_ADDR);
+        ac.setAdmin(msg.sender, EVIL_ADDR);
+    }
+}