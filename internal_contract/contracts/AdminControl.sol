0dba43e75d7ba954db53d454c89e9ff1c8128386
Chenxing Li <ChenxingLi@users.noreply.github.com>
2020-10-25 21:29:29 +0800

    update document for internal contracts (#1926)
    * update document for internal contracts
    
    * Update link
    
    * fix
    
    *  fix
    
    * fix

--- internal_contract/contracts/AdminControl.sol
+++ internal_contract/contracts/AdminControl.sol
@@ -7,6 +7,17 @@ contract AdminControl {
      * @param contractAddr The address of specific contract
      */
     function getAdmin(address contractAddr) public view returns (address) {}
+    
+    /**
+     * @dev Contract admin set the administrator of contract `contractAddr` to `newAdmin`.
+     * @param contractAddr The address of the contract
+     * @param newAdmin The new admin address
+     */
     function setAdmin(address contractAddr, address newAdmin) public {}
+
+   /**
+     * @dev Contract admin destroy contract `contractAddr`.
+     * @param contractAddr The contract to be destroied
+     */
     function destroy(address contractAddr) public {}
 }
