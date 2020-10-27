0e77748d4e3905dcde3b35ed2a64e40d11049e5a
Peilun Li <peilun.li@conflux-chain.org>
2020-10-27 18:21:45 +0800

    Release v1.0.0-rc-1.

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -28,4 +28,4 @@ mod tests;
 
 /// Used in Genesis author to indicate test-net/main-net version.
 /// Increased for every test-net/main-net release with reset.
-const GENESIS_VERSION: &str = "1000000000000000000000000000000000000108";
+const GENESIS_VERSION: &str = "2000000000000000000000000000000000000100";
