e1e0426b1ec6c933eadf1d9ebec075678aab04c0
peilun-conflux <48905552+peilun-conflux@users.noreply.github.com>
2020-10-29 15:07:42 +0800

    Fix contract test. (#1931)
    * Fix assert check. Now vote_test fails with Reverted.
    
    * Add back,
    
    * Fix vote_token.

--- tests/fixed_supply_token_test.py
+++ tests/fixed_supply_token_test.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import time
+
 from conflux.utils import priv_to_addr
 from test_framework.smart_contract_bench_base import SmartContractBenchBase
 from web3 import Web3
@@ -28,14 +30,14 @@ class FixedTokenSupplyTokenTest(SmartContractBenchBase):
     def generate_transactions(self, _):
         self.call_contract_function(self.contract, "transfer",
                                     [Web3.toChecksumAddress(priv_to_addr(self.accounts[0])), 1000],
-                                    self.default_account_key, self.contract_address, True, True, storage_limit=64)
+                                    self.default_account_key, self.contract_address, True, True, storage_limit=512)
         self.call_contract_function(self.contract, "approve",
                                     [Web3.toChecksumAddress(priv_to_addr(self.accounts[1])), 500],
-                                    self.accounts[0], self.contract_address, True, True, storage_limit=128)
+                                    self.accounts[0], self.contract_address, True, True, storage_limit=512)
         self.call_contract_function(self.contract, "transferFrom",
                                     [Web3.toChecksumAddress(priv_to_addr(self.accounts[0])),
                                      Web3.toChecksumAddress(priv_to_addr(self.default_account_key)), 300],
-                                    self.accounts[1], self.contract_address, True, True, storage_limit=64)
+                                    self.accounts[1], self.contract_address, True, True, storage_limit=512)
 
 
 if __name__ == "__main__":
