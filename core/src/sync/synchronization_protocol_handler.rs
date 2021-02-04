69af0eba1f61597f3f5f296d1ebbbcb112b0027e
Peilun Li <peilun.li@conflux-chain.org>
2021-02-04 12:49:39 +0800

    Remove `resolve_outside_dependencies`.
    Now we have checked the on-disk LocalBlockInfo in
    `new_to_be_graph_ready`, so we do not need to do this periodically.

--- core/src/sync/synchronization_protocol_handler.rs
+++ core/src/sync/synchronization_protocol_handler.rs
@@ -1696,20 +1696,15 @@ impl SynchronizationProtocolHandler {
     }
 
     pub fn expire_block_gc(
-        &self, io: &dyn NetworkContext, timeout: u64,
+        &self, _io: &dyn NetworkContext, timeout: u64,
     ) -> Result<(), Error> {
         if self.in_recover_from_db_phase() {
             // In recover_from_db phase, this will be done at the end of
-            // recovery, and if we allow `resolve_outside_dependencies` here,
-            // it will cause inconsistency.
+            // recovery.
             return Ok(());
         }
-        // TODO This may not be needed now, but we should double check it.
-        let need_to_relay = self
-            .graph
-            .resolve_outside_dependencies(self.insert_header_to_consensus());
         self.graph.remove_expire_blocks(timeout);
-        self.relay_blocks(io, need_to_relay)
+        Ok(())
     }
 
     pub fn is_block_queue_full(&self) -> bool {
