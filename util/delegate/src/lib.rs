ca21749a125e8521443f1e2f9393d3a054f8776e
Peilun Li <peilun.li@conflux-chain.org>
2021-03-23 12:57:29 +0800

    Cargo fmt.

--- util/delegate/src/lib.rs
+++ util/delegate/src/lib.rs
@@ -97,9 +97,7 @@ use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
 use std::collections::HashMap;
-use syn::{
-    self, parse::ParseStream, spanned::Spanned, Error,
-};
+use syn::{self, parse::ParseStream, spanned::Spanned, Error};
 
 mod kw {
     syn::custom_keyword!(to);
