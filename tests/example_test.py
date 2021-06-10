0dfe03702fc24059b3cd294f80868f4b46927c75
Peilun Li <peilun.li@confluxnetwork.org>
2021-06-10 13:38:31 +0800

    Return pos_reference in RPC.

--- tests/example_test.py
+++ tests/example_test.py
@@ -22,11 +22,21 @@ class ExampleTest(ConfluxTestFramework):
         self.nodes[0].generate_empty_blocks(1)
         assert (self.nodes[0].getblockcount() == 2)
 
+        latest_pos_ref = self.latest_pos_ref()
         for _ in range(10):
-            self.nodes[0].generate_empty_blocks(6000)
+            # Generate enough PoW block for PoS to progress
+            self.nodes[0].generate_empty_blocks(600)
+            # Leave some time for PoS to reach consensus
             time.sleep(1)
+            self.nodes[0].generate_empty_blocks(1)
+            new_pos_ref = self.latest_pos_ref()
+            assert_ne(latest_pos_ref, new_pos_ref)
         # assert (self.nodes[0].getblockcount() == 6002)
 
+    def latest_pos_ref(self):
+        best_hash = self.nodes[0].best_block_hash()
+        block = self.nodes[0].cfx_getBlockByHash(best_hash, False)
+        return block["posReference"]
 
 if __name__ == '__main__':
     ExampleTest().main()
