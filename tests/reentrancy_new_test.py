65906a86ac4b9c679cb9b253d5af961729923b0f
Chenxing Li <ChenxingLi@users.noreply.github.com>
2021-07-06 18:37:24 +0800

    Add test case

---
+++ tests/reentrancy_new_test.py
@@ -0,0 +1,4 @@
+from reentrancy_test_template import ReentrancyTest, NEW_MODE
+
+if __name__ == '__main__':
+    ReentrancyTest(NEW_MODE).main()
--- tests/reentrancy_test_template.py
+++ tests/reentrancy_test_template.py
@@ -11,6 +11,12 @@ from test_framework.block_gen_thread import BlockGenThread
 from eth_utils import decode_hex
 from web3 import Web3
 
+OLD_MODE = 0
+NEW_MODE = 1
+NO_PROTECTION = 2
+CFX = 10 ** 18
+
+
 class ReentrancyTest(ConfluxTestFramework):
     REQUEST_BASE = {
         'gas': CONTRACT_DEFAULT_GAS,
@@ -18,7 +24,9 @@ class ReentrancyTest(ConfluxTestFramework):
         'chainId': 1,
     }
 
-    def __init__(self):
+    def __init__(self, mode):
+        self.mode = mode
+
         super().__init__()
 
         self.nonce_map = {}
@@ -28,7 +36,8 @@ class ReentrancyTest(ConfluxTestFramework):
 
     def set_test_params(self):
         self.num_nodes = 1
-        self.conf_parameters["unnamed_21autumn_cip71_deferred_transition"] = 1_000_000_000
+        if self.mode == OLD_MODE:
+            self.conf_parameters["unnamed_21autumn_cip71_deferred_transition"] = 1_000_000_000
 
     def setup_network(self):
         self.setup_nodes()
@@ -56,7 +65,7 @@ class ReentrancyTest(ConfluxTestFramework):
             func = getattr(contract, name)
         attrs = {
             'nonce': self.get_nonce(priv_to_addr(sender_key)),
-            ** ReentrancyTest.REQUEST_BASE
+            **ReentrancyTest.REQUEST_BASE
         }
         if contract_addr:
             attrs['receiver'] = decode_hex(contract_addr)
@@ -86,13 +95,20 @@ class ReentrancyTest(ConfluxTestFramework):
         file_dir = os.path.dirname(os.path.realpath(__file__))
 
         self.log.info("Initializing contract")
-       
+
         self.buggy_contract = get_contract_instance(
             source=os.path.join(file_dir, "contracts/reentrancy.sol"),
             contract_name="Reentrance")
         self.exploit_contract = get_contract_instance(
             source=os.path.join(file_dir, "contracts/reentrancy_exploit.sol"),
             contract_name="ReentranceExploit")
+
+        reentrancy_config_addr = Web3.toChecksumAddress("0888000000000000000000000000000000000003")
+        file_dir = os.path.dirname(os.path.realpath(__file__))
+        control_contract_file_path = os.path.join(file_dir, "..", "internal_contract", "metadata",
+                                                  "ReentrancyConfig.json")
+        control_contract_dict = json.loads(open(control_contract_file_path, "r").read())
+        control_contract = get_contract_instance(contract_dict=control_contract_dict)
 
         user1, _ = ec_random_keys()
         user1_addr = priv_to_addr(user1)
@@ -124,37 +140,63 @@ class ReentrancyTest(ConfluxTestFramework):
         user2_balance_before_contract_construction = client.get_balance(user2_addr_hex)
         assert_equal(user2_balance_before_contract_construction, value)
 
-        transaction = self.call_contract_function(self.buggy_contract, "constructor", [], self.genesis_priv_key, storage_limit=20000)
-        contract_addr = self.wait_for_tx([transaction], True)[0]['contractCreated']
+        transaction = self.call_contract_function(self.buggy_contract, "constructor", [], self.genesis_priv_key,
+                                                  storage_limit=20000)
+        buggy_addr = self.wait_for_tx([transaction], True)[0]['contractCreated']
 
         transaction = self.call_contract_function(self.exploit_contract, "constructor", [], user2, storage_limit=200000)
         exploit_addr = self.wait_for_tx([transaction], True)[0]['contractCreated']
+
+        if self.mode == NO_PROTECTION:
+            self.log.info("Disabling anti-reentrancy")
+            self.call_contract_function(
+                contract=control_contract,
+                name="allowReentrancyByAdmin",
+                args=[Web3.toChecksumAddress(buggy_addr), True],
+                sender_key=self.genesis_priv_key,
+                contract_addr=reentrancy_config_addr,
+                storage_limit=64,
+                wait=True,
+                check_status=True)
+            self.call_contract_function(
+                contract=control_contract,
+                name="allowReentrancyByAdmin",
+                args=[Web3.toChecksumAddress(exploit_addr), True],
+                sender_key=user2,
+                contract_addr=reentrancy_config_addr,
+                storage_limit=64,
+                wait=True,
+                check_status=True)
+
         user2_balance_after_contract_construction = client.get_balance(user2_addr_hex)
         self.log.debug("user2 balance contract created %s" % user2_balance_after_contract_construction)
-        assert_greater_than_or_equal(user2_balance_before_contract_construction, user2_balance_after_contract_construction)
+        assert_greater_than_or_equal(user2_balance_before_contract_construction,
+                                     user2_balance_after_contract_construction)
         user2_refund_upper_bound = \
             user2_balance_before_contract_construction - \
             user2_balance_after_contract_construction
 
         transaction = self.call_contract_function(self.buggy_contract, "addBalance", [], user1, 10 ** 18,
-                                                  contract_addr, True, True, storage_limit=128)
-        transaction = self.call_contract_function(self.exploit_contract, "deposit", [Web3.toChecksumAddress(contract_addr)], user2, 10 ** 18,
+                                                  buggy_addr, True, True, storage_limit=128)
+        transaction = self.call_contract_function(self.exploit_contract, "deposit",
+                                                  [Web3.toChecksumAddress(buggy_addr)], user2, 10 ** 18,
                                                   exploit_addr, True, True, storage_limit=128)
 
         user1_balance = client.get_balance(user1_addr_hex)
         assert_greater_than_or_equal(user1_balance, 899999999999999999999999950000000)
         user2_balance_after_deposit = client.get_balance(user2_addr_hex)
         # User2 paid storage collateral `vulnerable_contract` in deposit call.
-        user2_refund_upper_bound += 10 ** 18 // 16
+        user2_refund_upper_bound += 3 * 10 ** 18 // 16
         self.log.debug("user2 balance after deposit %s" % user2_balance_after_deposit)
         assert_greater_than_or_equal(user2_balance_after_contract_construction, user2_balance_after_deposit + 10 ** 18)
         assert_greater_than_or_equal(user2_balance_after_deposit, 899999999999999999999999900000000)
-        contract_balance = client.get_balance(contract_addr)
+        contract_balance = client.get_balance(buggy_addr)
         assert_equal(contract_balance, 2 * 10 ** 18)
         user2_balance_in_contract = RpcClient(self.nodes[0]).call(
-            contract_addr,
+            buggy_addr,
             self.buggy_contract.functions.balanceOf(Web3.toChecksumAddress(exploit_addr)).buildTransaction(
-                {"from":user2_addr_hex, "to":contract_addr, "gas":int_to_hex(CONTRACT_DEFAULT_GAS), "gasPrice":int_to_hex(1), "chainId":0}
+                {"from": user2_addr_hex, "to": buggy_addr, "gas": int_to_hex(CONTRACT_DEFAULT_GAS),
+                 "gasPrice": int_to_hex(1), "chainId": 0}
             )["data"])
         assert_equal(parse_as_int(user2_balance_in_contract), 10 ** 18)
 
@@ -165,26 +207,37 @@ class ReentrancyTest(ConfluxTestFramework):
 
         user1_balance = client.get_balance(user1_addr_hex)
         assert_greater_than_or_equal(user1_balance, 899999999999999999999999950000000)
-        contract_balance = client.get_balance(contract_addr)
-        assert_equal(contract_balance, 1 * 10 ** 18)
+        contract_balance = client.get_balance(buggy_addr)
+        attack_benefit = 0
+        if self.mode == OLD_MODE:
+            # In the old mode, the second withdraw_balance will fail.
+            rest_balance, attacker_rest = 1 * CFX, 0
+        elif self.mode == NEW_MODE:
+            # In the new mode, the fist withdraw_balance will fail because changing attack mode triggers anti-reentrancy.
+            rest_balance, attacker_rest = 2 * CFX, 1 * CFX
+        elif self.mode == NO_PROTECTION:
+            # If there is no protection
+            rest_balance, attacker_rest = 0, 0
+            attack_benefit = 1 * CFX
+        else:
+            raise Exception("Unrecognized reentrancy test mode")
+        assert_equal(contract_balance, rest_balance)
 
         user2_balance_in_contract = RpcClient(self.nodes[0]).call(
-            contract_addr,
+            buggy_addr,
             self.buggy_contract.functions.balanceOf(Web3.toChecksumAddress(exploit_addr)).buildTransaction(
-                {"from":user2_addr_hex, "to":contract_addr, "gas":int_to_hex(CONTRACT_DEFAULT_GAS), "gasPrice":int_to_hex(1), "chainId":0}
+                {"from": user2_addr_hex, "to": buggy_addr, "gas": int_to_hex(CONTRACT_DEFAULT_GAS),
+                 "gasPrice": int_to_hex(1), "chainId": 0}
             )["data"])
 
-        assert_equal(parse_as_int(user2_balance_in_contract), 0)
+        assert_equal(parse_as_int(user2_balance_in_contract), attacker_rest)
         self.log.debug("user2 balance in contract %s" % user2_balance_in_contract)
         user2_balance_after_contract_destruct = client.get_balance(user2_addr_hex)
         self.log.debug("user2 balance after contract destruct %s" % user2_balance_after_contract_destruct)
         assert_greater_than_or_equal(
+            user2_balance_after_deposit + user2_refund_upper_bound + 10 ** 18 - attacker_rest + attack_benefit,
             user2_balance_after_contract_destruct,
-            user2_balance_after_deposit + user2_refund_upper_bound + 10 ** 18
-            )
+        )
 
         block_gen_thread.stop()
         block_gen_thread.join()
-
-if __name__ == '__main__':
-    ReentrancyTest().main()
