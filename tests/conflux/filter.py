126b2e3ae7ae68a0a5098cd8cdb1d1697015bedb
Thegaram <th307q@gmail.com>
2021-07-14 21:10:25 +0800

    fix python tests

--- tests/conflux/filter.py
+++ tests/conflux/filter.py
@@ -2,7 +2,7 @@ from conflux.address import hex_to_b32_address
 
 
 class Filter():
-    def __init__(self, from_epoch="earliest", to_epoch="latest_state", from_block = None, to_block = None, block_hashes = None, address = None, topics = [],
+    def __init__(self, from_epoch=None, to_epoch=None, from_block = None, to_block = None, block_hashes = None, address = None, topics = [],
                  offset = None, limit = None, encode_address=True):
         if encode_address and address is not None:
             if isinstance(address, list):
