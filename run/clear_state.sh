abb88a8d75a559c4301eda4759d46bc839297654
Peilun Li <peilun.li@confluxnetwork.org>
2021-09-07 14:23:01 +0800

    Update configuration.

--- run/clear_state.sh
+++ run/clear_state.sh
@@ -4,4 +4,4 @@ rm -rf log
 rm -rf pos_db
 rm pos.log
 rm -rf pos_config/private_keys
-cd pos_config && ls | grep -v pos_config.yaml | grep -v pos_key | xargs rm
+cd pos_config && ls | grep -v pos_config.yaml | grep -v pos_key |grep -v initial_nodes.json| grep -v genesis_file|xargs rm
