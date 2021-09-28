992607d7e2b428f36ca41afcc1931567358a82bc
Peilun Li <peilun.li@confluxnetwork.org>
2021-09-28 13:59:24 +0800

    Update test parameters to large voting powers.

--- tools/pos-genesis-tool/main.rs
+++ tools/pos-genesis-tool/main.rs
@@ -229,7 +229,7 @@ where
         let mut rng = StdRng::from_seed([0u8; 32]);
         let mut genesis_nodes = Vec::new();
 
-        let voting_power = 100;
+        let voting_power = 2_000_000;
         for i in 0..num_validator {
             let pow_keypair: KeyPair = Random.generate().unwrap();
             let private_key = ConsensusPrivateKey::generate(&mut rng);
