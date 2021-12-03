791b3525cc228ed66ca9f2c69dfcfde1c7234f0f
Peilun Li <peilun.li@confluxnetwork.org>
2021-12-03 11:30:00 +0800

    Move PoS tx state verification before signature verification.

--- core/src/pos/mempool/shared_mempool/transaction_validator.rs
+++ core/src/pos/mempool/shared_mempool/transaction_validator.rs
@@ -16,14 +16,17 @@ impl TransactionValidator {
     pub fn validate_transaction(
         &self, tx: &SignedTransaction, pos_state: Arc<PosState>,
     ) -> Option<VMValidatorResult> {
-        // check signature
-        if tx.clone().check_signature().is_err() {
-            return Some(VMValidatorResult::new(
-                Some(DiscardedVMStatus::INVALID_SIGNATURE),
-                0,
-                GovernanceRole::Validator,
-            ));
-        }
+        // This check is cheaper than signature verification, so we do not
+        // need to verify signatures for old transactions.
+        let result = match tx.payload() {
+            TransactionPayload::Election(election_payload) => {
+                pos_state.validate_election_simple(election_payload)
+            }
+            TransactionPayload::PivotDecision(pivot_decision) => {
+                pos_state.validate_pivot_decision_simple(pivot_decision)
+            }
+            _ => None,
+        };
 
         match tx.authenticator() {
             TransactionAuthenticator::BLS { .. } => {}
@@ -36,15 +39,14 @@ impl TransactionValidator {
             }
         }
 
-        let result = match tx.payload() {
-            TransactionPayload::Election(election_payload) => {
-                pos_state.validate_election_simple(election_payload)
-            }
-            TransactionPayload::PivotDecision(pivot_decision) => {
-                pos_state.validate_pivot_decision_simple(pivot_decision)
-            }
-            _ => None,
-        };
+        // check signature
+        if tx.clone().check_signature().is_err() {
+            return Some(VMValidatorResult::new(
+                Some(DiscardedVMStatus::INVALID_SIGNATURE),
+                0,
+                GovernanceRole::Validator,
+            ));
+        }
 
         Some(VMValidatorResult::new(result, 0, GovernanceRole::Validator))
     }
