5b9b5aed92a0c87cb374cbec790c0499ee28e1b0
Peilun Li <peilun.li@confluxnetwork.org>
2021-12-24 19:04:35 +0800

    Fix warning.

--- run/clear_state.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -rf blockchain_data
-rm -rf log
-rm -rf pos_db
-rm pos.log
-rm -rf pos_config/private_keys
-cd pos_config && ls | grep -v pos_config.yaml | grep -v pos_key |grep -v initial_nodes.json| grep -v genesis_file|xargs rm
