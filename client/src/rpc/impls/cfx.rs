e4a62b7dcacaf9a933a9419589abcf4776273189
Chenxing Li <ChenxingLi@users.noreply.github.com>
2022-01-04 01:38:13 +0800

    Fix warning

--- client/src/rpc/impls/cfx.rs
+++ client/src/rpc/impls/cfx.rs
@@ -11,8 +11,7 @@ use blockgen::BlockGenerator;
 use cfx_state::state_trait::StateOpsTrait;
 use cfx_statedb::{StateDbExt, StateDbGetOriginalMethods};
 use cfx_types::{
-    Address, AddressSpaceUtil, BigEndianHash, Space, H256, H520, U128, U256,
-    U64,
+    Address, AddressSpaceUtil, BigEndianHash, H256, H520, U128, U256, U64,
 };
 use cfxcore::{
     executive::{ExecutionError, ExecutionOutcome, TxDropError},
