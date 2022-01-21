c5ead6ca04fdd6a26ee4c8b9c960064ce427527d
Peilun Li <peilun.li@confluxnetwork.org>
2022-01-21 18:30:36 +0800

    Update bootnodes.

--- tests/tools/host_gen.py
+++ tests/tools/host_gen.py
@@ -6,6 +6,9 @@ from eth_utils import decode_hex
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from conflux.utils import ec_random_keys, encode_hex, priv_to_pub
 
+priv = "04ac85eea85ee2e3c15fedad6f450d1d8fee06ce95e05f97f064ea28fc8bcbdd"
+print(encode_hex(priv_to_pub(decode_hex(priv))))
+exit()
 NUM_NODES = 2
 for _ in range(NUM_NODES):
     pri_key_raw, _ = ec_random_keys()
