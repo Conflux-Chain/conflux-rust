023fec796b4f9d6be9ca2021a74a8d044603f048
Peilun Li <peilun.li@confluxnetwork.org>
2022-02-16 17:18:40 +0800

    Set transaction_hash for eSpace tx traces.

--- tests/evm_space/trace_test.py
+++ tests/evm_space/trace_test.py
@@ -84,13 +84,13 @@ class TraceTest(Web3Base):
         traces = self.nodes[0].ethrpc.trace_block(epoch_a)
         assert_equal(len(traces), 1)
         assert_ne(traces[0]["result"], None)
-        assert_equal(traces[0]["transactionHash"], None)
+        assert_equal(traces[0]["transactionHash"], eth_tx.hash())
         assert_equal(traces[0]["transactionPosition"], None)
         block_a_txs_evm = self.nodes[0].eth_getBlockByHash(block_a, False)["transactions"]
         traces = self.nodes[0].ethrpc.trace_transaction(block_a_txs_evm[0])
         assert_equal(len(traces), 1)
         assert_ne(traces[0]["result"], None)
-        assert_equal(traces[0]["transactionHash"], None)
+        assert_equal(traces[0]["transactionHash"], eth_tx.hash())
         assert_equal(traces[0]["transactionPosition"], None)
 
         self.log.info("Pass")
