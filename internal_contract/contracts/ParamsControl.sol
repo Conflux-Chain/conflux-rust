07a14ea106ec9e2d32b74f80413520657ef146eb
Peilun Li <peilun.li@confluxnetwork.org>
2022-04-14 16:49:31 +0800

    Add internal contract interface.

---
+++ internal_contract/contracts/ParamsControl.sol
@@ -0,0 +1,24 @@
+pragma solidity >=0.4.15;
+
+
+contract AdminControl {
+    struct Vote {
+        uint8 index;
+        uint8 opt_index;
+        uint256 votes;
+    }
+
+    /*** Query Functions ***/
+    /**
+     * @dev cast vote for parameters
+     * @param version The parameter version number to vote for
+     * @param vote_data The list of votes to cast
+     */
+    function castVote(uint64 version, Vote[] calldata vote_data) public {}
+
+    /**
+     * @dev read the vote data of an account
+     * @param addr The address of the account to read
+     */
+    function readVote(address addr) public view returns (Vote[] memory) {}
+}
