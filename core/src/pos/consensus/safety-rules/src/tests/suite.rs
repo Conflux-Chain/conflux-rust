20d8dd3c955c1d92fd87e2930ac209468f8916db
Peilun Li <peilun.li@confluxnetwork.org>
2022-06-16 17:28:20 +0800

    nit.

--- core/src/pos/consensus/safety-rules/src/tests/suite.rs
+++ core/src/pos/consensus/safety-rules/src/tests/suite.rs
@@ -840,7 +840,7 @@ fn test_validator_not_in_set(safety_rules: &Callback) {
     proof
         .ledger_info_with_sigs
         .push(a2.block().quorum_cert().ledger_info().clone());
-    safety_rules.initialize(&proof).ok();
+    safety_rules.initialize(&proof).unwrap();
 
     let state = safety_rules.consensus_state().unwrap();
     assert_eq!(state.in_validator_set(), false);
