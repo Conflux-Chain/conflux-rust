dc35e9f647579076cc6f823a7a1217a13cfe6762
Joel <liuyuanvic@gmail.com>
2023-04-24 06:04:58 +0000

    fix build error

--- core/storage/src/tests/snapshot/verifier.rs
+++ core/storage/src/tests/snapshot/verifier.rs
@@ -496,7 +496,7 @@ impl SnapshotDbManagerTrait for FakeSnapshotDbManager {
 
     fn scan_persist_state(
         &self, _snapshot_info_map: &HashMap<EpochId, SnapshotInfo>,
-    ) -> Result<(Vec<EpochId>, EpochId)> {
+    ) -> Result<(Vec<EpochId>, EpochId, u64)> {
         unreachable!()
     }
 
