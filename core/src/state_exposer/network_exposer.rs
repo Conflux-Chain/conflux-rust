8f164723168a0616f193c84a6ff1cb2e1624c113
PanaW <pana.wang@outlook.com>
2024-02-28 21:52:53 +0800

    move core and util into crates folder

--- core/src/state_exposer/network_exposer.rs
+++
@@ -1,7 +0,0 @@
-// Copyright 2019 Conflux Foundation. All rights reserved.
-// Conflux is free software and distributed under GNU General Public License.
-// See http://www.gnu.org/licenses/
-
-/// This struct maintains some inner state of network layer.
-#[derive(Default)]
-pub struct NetworkExposer {}
