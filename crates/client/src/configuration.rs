0b257413f06a8049728b077d6a7cfa3b399c24bc
Chenxing Li <ChenxingLi@users.noreply.github.com>
2024-05-06 13:26:45 +0800

    CIP-130: Aligning Gas Limit with Transaction Size

--- crates/client/src/configuration.rs
+++ crates/client/src/configuration.rs
@@ -162,6 +162,7 @@ build_config! {
         (cip118_transition_number, (Option<u64>), None)
         (cip119_transition_number, (Option<u64>), None)
         (next_hardfork_transition_number, (Option<u64>), None)
+        (next_hardfork_transition_height, (Option<u64>), None)
         (referee_bound, (usize), REFEREE_DEFAULT_BOUND)
         (params_dao_vote_period, (u64), DAO_PARAMETER_VOTE_PERIOD)
         (timer_chain_beta, (u64), TIMER_CHAIN_DEFAULT_BETA)
@@ -1346,6 +1347,10 @@ impl Configuration {
             .unwrap_or(non_genesis_default_transition_time);
         params.transition_heights.cip112 =
             *CIP112_TRANSITION_HEIGHT.get().expect("initialized");
+        params.transition_heights.cip130 = self
+            .raw_conf
+            .next_hardfork_transition_height
+            .unwrap_or(default_transition_time);
         params.params_dao_vote_period = self.raw_conf.params_dao_vote_period;
 
         let mut base_block_rewards = BTreeMap::new();
