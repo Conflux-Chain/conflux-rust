3207971491b8f1f85a8584027208b3274b53c627
Peilun Li <48905552+peilun-conflux@users.noreply.github.com>
2024-05-09 17:44:00 +0800

    Add CIP136 parameters.

--- crates/cfxcore/core/src/pos/types/src/term_state/pos_state_config.rs
+++ crates/cfxcore/core/src/pos/types/src/term_state/pos_state_config.rs
@@ -8,6 +8,7 @@ use crate::{
 use diem_crypto::_once_cell::sync::OnceCell;
 
 const CIP99_FORCE_RETIRE_EPOCH_COUNT: u64 = 3;
+const CIP136_FORCE_RETIRE_EPOCH_COUNT: u64 = 6;
 
 #[derive(Clone, Debug)]
 pub struct PosStateConfig {
@@ -21,14 +22,19 @@ pub struct PosStateConfig {
     cip99_out_queue_locked_views: u64,
     cip99_in_queue_locked_views: u64,
 
+    cip136_transition_view: u64,
+    cip136_out_queue_locked_views: u64,
+    cip136_in_queue_locked_views: u64,
+    cip136_round_per_term: u64,
+
     nonce_limit_transition_view: u64,
     max_nonce_per_account: u64,
 }
 
 pub trait PosStateConfigTrait {
-    fn round_per_term(&self) -> Round;
-    fn election_term_start_round(&self) -> Round;
-    fn election_term_end_round(&self) -> Round;
+    fn round_per_term(&self, view: u64) -> Round;
+    fn election_term_start_round(&self, view: u64) -> Round;
+    fn election_term_end_round(&self, view: u64) -> Round;
     fn first_start_election_view(&self) -> u64;
     fn first_end_election_view(&self) -> u64;
     fn term_max_size(&self) -> usize;
@@ -39,6 +45,8 @@ pub trait PosStateConfigTrait {
 
     fn force_retire_check_epoch_count(&self, view: u64) -> u64;
     fn max_nonce_per_account(&self, view: u64) -> u64;
+    fn get_term_view(&self, view: u64) -> (u64, u64);
+    fn get_starting_view_for_term(&self, term: u64) -> Option<u64>;
 }
 
 impl PosStateConfig {
@@ -47,7 +55,9 @@ impl PosStateConfig {
         in_queue_locked_views: u64, out_queue_locked_views: u64,
         cip99_transition_view: u64, cip99_in_queue_locked_views: u64,
         cip99_out_queue_locked_views: u64, nonce_limit_transition_view: u64,
-        max_nonce_per_account: u64,
+        max_nonce_per_account: u64, cip136_transition_view: u64,
+        cip136_in_queue_locked_views: u64, cip136_out_queue_locked_views: u64,
+        cip136_round_per_term: u64,
     ) -> Self {
         Self {
             round_per_term,
@@ -58,6 +68,10 @@ impl PosStateConfig {
             cip99_transition_view,
             cip99_out_queue_locked_views,
             cip99_in_queue_locked_views,
+            cip136_transition_view,
+            cip136_out_queue_locked_views,
+            cip136_in_queue_locked_views,
+            cip136_round_per_term,
             nonce_limit_transition_view,
             max_nonce_per_account,
         }
@@ -65,25 +79,34 @@ impl PosStateConfig {
 }
 
 impl PosStateConfigTrait for OnceCell<PosStateConfig> {
-    fn round_per_term(&self) -> Round { self.get().unwrap().round_per_term }
+    fn round_per_term(&self, view: u64) -> Round {
+        let conf = self.get().unwrap();
+        if view < conf.cip136_transition_view {
+            conf.round_per_term
+        } else {
+            conf.cip136_round_per_term
+        }
+    }
 
     /// A term `n` is open for election in the view range
     /// `(n * ROUND_PER_TERM - ELECTION_TERM_START_ROUND, n * ROUND_PER_TERM -
     /// ELECTION_TERM_END_ROUND]`
-    fn election_term_start_round(&self) -> Round {
-        self.round_per_term() / 2 * 3
+    fn election_term_start_round(&self, view: u64) -> Round {
+        self.round_per_term(view) / 2 * 3
     }
 
-    fn election_term_end_round(&self) -> Round { self.round_per_term() / 2 }
+    fn election_term_end_round(&self, view: u64) -> Round {
+        self.round_per_term(view) / 2
+    }
 
     fn first_start_election_view(&self) -> u64 {
-        TERM_LIST_LEN as u64 * self.round_per_term()
-            - self.election_term_start_round()
+        TERM_LIST_LEN as u64 * self.round_per_term(0)
+            - self.election_term_start_round(0)
     }
 
     fn first_end_election_view(&self) -> u64 {
-        TERM_LIST_LEN as u64 * self.round_per_term()
-            - self.election_term_end_round()
+        TERM_LIST_LEN as u64 * self.round_per_term(0)
+            - self.election_term_end_round(0)
     }
 
     fn term_max_size(&self) -> usize { self.get().unwrap().term_max_size }
@@ -94,8 +117,12 @@ impl PosStateConfigTrait for OnceCell<PosStateConfig> {
 
     fn in_queue_locked_views(&self, view: u64) -> u64 {
         let conf = self.get().unwrap();
-        if view >= conf.cip99_transition_view {
+        if view >= conf.cip99_transition_view
+            && view < conf.cip136_transition_view
+        {
             conf.cip99_in_queue_locked_views
+        } else if view >= conf.cip136_transition_view {
+            conf.cip136_in_queue_locked_views
         } else {
             conf.in_queue_locked_views
         }
@@ -103,8 +130,12 @@ impl PosStateConfigTrait for OnceCell<PosStateConfig> {
 
     fn out_queue_locked_views(&self, view: u64) -> u64 {
         let conf = self.get().unwrap();
-        if view >= conf.cip99_transition_view {
+        if view >= conf.cip99_transition_view
+            && view < conf.cip136_transition_view
+        {
             conf.cip99_out_queue_locked_views
+        } else if view >= conf.cip136_transition_view {
+            conf.cip136_out_queue_locked_views
         } else {
             conf.out_queue_locked_views
         }
@@ -116,10 +147,14 @@ impl PosStateConfigTrait for OnceCell<PosStateConfig> {
 
     fn force_retire_check_epoch_count(&self, view: u64) -> u64 {
         let conf = self.get().unwrap();
-        if view >= conf.cip99_transition_view {
+        if view >= conf.cip99_transition_view
+            && view < conf.cip136_transition_view
+        {
             // This is set according to the value of `TERM_LIST_LEN`.
             // Since `TERM_LIST_LEN` is hardcoded, we do not parameterize this.
             CIP99_FORCE_RETIRE_EPOCH_COUNT
+        } else if view > conf.cip136_transition_view {
+            CIP136_FORCE_RETIRE_EPOCH_COUNT
         } else {
             1
         }
@@ -133,6 +168,10 @@ impl PosStateConfigTrait for OnceCell<PosStateConfig> {
             u64::MAX
         }
     }
+
+    fn get_term_view(&self, view: u64) -> (u64, u64) { todo!() }
+
+    fn get_starting_view_for_term(&self, term: u64) -> Option<u64> { todo!() }
 }
 
 pub static POS_STATE_CONFIG: OnceCell<PosStateConfig> = OnceCell::new();
@@ -148,6 +187,10 @@ impl Default for PosStateConfig {
             cip99_transition_view: u64::MAX,
             cip99_out_queue_locked_views: IN_QUEUE_LOCKED_VIEWS,
             cip99_in_queue_locked_views: OUT_QUEUE_LOCKED_VIEWS,
+            cip136_transition_view: u64::MAX,
+            cip136_out_queue_locked_views: IN_QUEUE_LOCKED_VIEWS,
+            cip136_in_queue_locked_views: OUT_QUEUE_LOCKED_VIEWS,
+            cip136_round_per_term: ROUND_PER_TERM,
             nonce_limit_transition_view: u64::MAX,
             max_nonce_per_account: u64::MAX,
         }
