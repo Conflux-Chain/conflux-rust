4c08fc19ea4397572cb246048014c8c0b7693656
pudongair <744355276@qq.com>
2024-07-19 19:41:02 +0800

    chore: fix some comments
    Signed-off-by: pudongair <744355276@qq.com>

--- dev-support/ccov.sh
+++ dev-support/ccov.sh
@@ -27,5 +27,5 @@ mkdir ccov
 zip -0 ccov/ccov.zip `find . \( -name "*.gc*" \) -print`
 grcov ccov/ccov.zip -s . -t html --llvm --branch --ignore-not-existing --ignore "/*" -o ccov
 echo "Code coverage result is saved to directory 'ccov'. 
-You can open 'ccov/index.html' with a web brower to start.
+You can open 'ccov/index.html' with a web browser to start.
 
