7f0eb25719e6fb39fa24cc6e4cd5589f885662d5
darwintree <17946284+darwintree@users.noreply.github.com>
2024-11-07 09:47:34 +0000

    fix dependency issues

--- dev-support/dep_pip3.sh
+++ dev-support/dep_pip3.sh
@@ -2,10 +2,9 @@
 
 set -e
 
-pip3 install cfx-account eth-utils py-ecc trie coincurve safe-pysha3 web3==7.4.0 py-solc-x jsonrpcclient==3.3.6 asyncio websockets pyyaml numpy
+pip3 install cfx-account eth-utils py-ecc rlp trie coincurve safe-pysha3 web3==7.4.0 py-solc-x jsonrpcclient==3.3.6 asyncio websockets pyyaml numpy
 
-
-# python3 -m solcx.install v0.5.17
+python3 -m solcx.install v0.5.17
 
 # TODO cross platform
 #yum install clang snappy snappy-devel zlib zlib-devel bzip2 bzip2-devel lz4-devel
