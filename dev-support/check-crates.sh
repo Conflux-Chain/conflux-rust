05304163d56ef72fa2cd65026da0d1163155aa47
Pana <pana.wang@outlook.com>
2024-11-12 14:45:39 +0800

    change cargo check line

--- dev-support/check-crates.sh
+++ dev-support/check-crates.sh
@@ -3,6 +3,6 @@
 grep -oP '^\s*\K[\w-]+(?=\s*=\s*{ path)' Cargo.toml | \
 xargs -I {} sh -c \
     'echo "\n\033[1;36m    Checking individual crate {}\033[0m\n" && \
-    cargo check -p cfx-addr --no-default-features --tests && \
     cargo check -p {} && \
     cargo check --tests -p {}' 
+cargo check -p cfx-addr --no-default-features --tests
