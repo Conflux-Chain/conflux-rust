c864dda1dfc97875e721b1f7371344fece215062
Pana <pana.wang@outlook.com>
2024-11-27 14:34:55 +0800

    remove useless crate

--- crates/util/channel_utils/src/lib.rs
+++
@@ -1,5 +0,0 @@
-// Copyright 2020 Conflux Foundation. All rights reserved.
-// Conflux is free software and distributed under GNU General Public License.
-// See http://www.gnu.org/licenses/
-
-pub mod cancellable_task_channel;
