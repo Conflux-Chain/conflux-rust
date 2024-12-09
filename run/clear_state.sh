bc2040935f7367772c2e8d49f3a1c427d585eed9
dayong <wangdayong228@163.com>
2024-12-09 10:54:42 +0800

    update JSONRPC changelog

--- run/clear_state.sh
+++ run/clear_state.sh
@@ -7,4 +7,4 @@ rm -rf pos_db
 rm pos.log
 rm -rf pos_config/private_keys
 
-cd pos_config && ls | grep -v pos_config.yaml | grep -v pos_key | xargs rm
+cd pos_config && ls | grep -v pos_config.yaml | grep -v pos_key | grep -v genesis_file | grep -v initial_nodes.json | xargs rm -rf
