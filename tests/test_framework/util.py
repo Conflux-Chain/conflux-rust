d6dc647679d821b2c81528fc1bcceffd66b7e85b
Joel <liuyuanvic@gmail.com>
2025-02-06 14:31:04 +0800

    fix comments

--- tests/test_framework/util.py
+++ tests/test_framework/util.py
@@ -675,7 +675,6 @@ def connect_sample_nodes(nodes, log, sample=3, latency_min=0, latency_max=300, t
     """
     peer = [[] for _ in nodes]
     latencies = [{} for _ in nodes]
-    threads = []
     num_nodes = len(nodes)
     sample = min(num_nodes - 1, sample)
 
@@ -699,6 +698,7 @@ def connect_sample_nodes(nodes, log, sample=3, latency_min=0, latency_max=300, t
 
     for i in range(0, num_nodes, max_parallel):
         batch = range(i, min(i + max_parallel, num_nodes))
+        threads = []
         for j in batch:
             t = ConnectThread(nodes, j, peer[j], latencies, log, min_peers=sample)
             t.start()
