49c44079042b0feefd509764e8cb1da65b375156
dayong <wangdayong228@163.com>
2025-02-08 18:44:42 +0800

    migrate phantom tests done

--- integration_tests/tests/cross_space/conftest.py
+++ integration_tests/tests/cross_space/conftest.py
@@ -15,7 +15,7 @@ def framework_class() -> Type[ConfluxTestFramework]:
     class DefaultFramework(ConfluxTestFramework):
         def set_test_params(self):
             self.num_nodes = 1
-            self.conf_parameters["min_native_base_price"] = 10000
+            # self.conf_parameters["min_native_base_price"] = 10000
             self.conf_parameters["next_hardfork_transition_height"] = 1
             self.conf_parameters["next_hardfork_transition_number"] = 1
             self.conf_parameters["public_evm_rpc_async_apis"] = '"all"'  # open all async apis
