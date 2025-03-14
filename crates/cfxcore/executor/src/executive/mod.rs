77655edd9d9e1705074bf90537e09cce3d4afcab
Chenxing Li <ChenxingLi@users.noreply.github.com>
2025-03-14 16:57:10 +0800

    Fix EIP-3860

--- crates/cfxcore/executor/src/executive/mod.rs
+++ crates/cfxcore/executor/src/executive/mod.rs
@@ -117,7 +117,8 @@ pub fn gas_required_for(
     let data_gas: u64 = data.iter().map(byte_gas).sum();
     let data_word = (data.len() as u64 + 31) / 32;
 
-    let initcode_gas = if spec.cip645 {
+    // CIP-645i: EIP-3860: Limit and meter initcode
+    let initcode_gas = if spec.cip645 && is_create {
         data_word * spec.init_code_word_gas as u64
     } else {
         0
