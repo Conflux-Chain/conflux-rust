e9404f9c5f7804aa0c2f3e284c2ad15e57f391de
Pana <Pana@users.noreply.github.com>
2025-04-10 02:10:25 +0000

    deploy: f82f73fb82ac3f8a23e68379608ff21d5ff9aca5

--- trait.impl/storage_interface/trait.DbReader.js
+++ trait.impl/storage_interface/trait.DbReader.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"pos_ledger_db":[["impl DbReader for <a class=\"struct\" href=\"pos_ledger_db/struct.PosLedgerDB.html\" title=\"struct pos_ledger_db::PosLedgerDB\">PosLedgerDB</a>"]],
+"pos_ledger_db":[["impl <a class=\"trait\" href=\"storage_interface/trait.DbReader.html\" title=\"trait storage_interface::DbReader\">DbReader</a> for <a class=\"struct\" href=\"pos_ledger_db/struct.PosLedgerDB.html\" title=\"struct pos_ledger_db::PosLedgerDB\">PosLedgerDB</a>"]],
 "storage_interface":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
