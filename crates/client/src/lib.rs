5179ffbdca8e5cd613f3906f3379f95c8ddfa04f
Pana <pana.wang@outlook.com>
2025-04-22 18:06:57 +0800

    direct use global genesis account constant

--- crates/client/src/lib.rs
+++ crates/client/src/lib.rs
@@ -15,7 +15,3 @@ pub mod configuration;
 mod node_types;
 pub mod rpc;
 pub use node_types::{archive, full, light};
-
-/// Used in Genesis author to indicate test-net/main-net version.
-/// Increased for every test-net/main-net release with reset.
-const GENESIS_VERSION: &str = "1949000000000000000000000000000000001001";
