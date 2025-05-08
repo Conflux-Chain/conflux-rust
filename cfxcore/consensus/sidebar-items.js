09466ebb838d78fab6429ac161f7d528c2b1b17a
ChenxingLi <ChenxingLi@users.noreply.github.com>
2025-05-08 09:43:31 +0000

    deploy: c601e66c338fd0bc2952fb0f920bb2d2219fd00c

--- cfxcore/consensus/sidebar-items.js
+++ cfxcore/consensus/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["consensus_inner","consensus_trait","debug_recompute","pivot_hint","pos_handler"],"struct":["BestInformation","ConsensusConfig","ConsensusGraph","ConsensusGraphStatistics","MaybeExecutedTxExtraInfo","TransactionInfo"]};
+window.SIDEBAR_ITEMS = {"mod":["consensus_inner","debug_recompute","pivot_hint","pos_handler"],"struct":["BestInformation","ConsensusConfig","ConsensusGraph","ConsensusGraphStatistics","MaybeExecutedTxExtraInfo","TransactionInfo"],"type":["SharedConsensusGraph"]};
