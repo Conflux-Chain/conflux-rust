35ae2cb4849604e9dbb186f865563d93e9f29f84
ChenxingLi <ChenxingLi@users.noreply.github.com>
2025-05-08 05:15:36 +0000

    deploy: fccfe400ec68b5e88f9fc1c10f22842bad72bae6

--- trait.impl/cfx_rpc_eth_api/web3/trait.Web3ApiServer.js
+++ trait.impl/cfx_rpc_eth_api/web3/trait.Web3ApiServer.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"cfx_rpc":[["impl <a class=\"trait\" href=\"cfx_rpc_eth_api/web3/trait.Web3ApiServer.html\" title=\"trait cfx_rpc_eth_api::web3::Web3ApiServer\">Web3ApiServer</a> for <a class=\"struct\" href=\"cfx_rpc/struct.Web3Api.html\" title=\"struct cfx_rpc::Web3Api\">Web3Api</a>"]]
+"cfx_rpc":[["impl Web3ApiServer for <a class=\"struct\" href=\"cfx_rpc/struct.Web3Api.html\" title=\"struct cfx_rpc::Web3Api\">Web3Api</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
