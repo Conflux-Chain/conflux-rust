72920eb87e2d292f4ef186a928cacd393b7cf324
peilun-conflux <peilun-conflux@users.noreply.github.com>
2025-05-09 08:30:46 +0000

    deploy: eb37dc9aa8dddca59e3b914e55394dbd5af8ae50

--- trait.impl/cfx_rpc_eth_api/net/trait.NetApiServer.js
+++ trait.impl/cfx_rpc_eth_api/net/trait.NetApiServer.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"cfx_rpc":[["impl NetApiServer for <a class=\"struct\" href=\"cfx_rpc/struct.NetApi.html\" title=\"struct cfx_rpc::NetApi\">NetApi</a>"]]
+"cfx_rpc":[["impl <a class=\"trait\" href=\"cfx_rpc_eth_api/net/trait.NetApiServer.html\" title=\"trait cfx_rpc_eth_api::net::NetApiServer\">NetApiServer</a> for <a class=\"struct\" href=\"cfx_rpc/struct.NetApi.html\" title=\"struct cfx_rpc::NetApi\">NetApi</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- trait.impl/cfx_rpc_eth_api/trace/trait.TraceApiServer.js
+++ trait.impl/cfx_rpc_eth_api/trace/trait.TraceApiServer.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"cfx_rpc":[["impl TraceApiServer for <a class=\"struct\" href=\"cfx_rpc/struct.TraceApi.html\" title=\"struct cfx_rpc::TraceApi\">TraceApi</a>"]]
+"cfx_rpc":[["impl <a class=\"trait\" href=\"cfx_rpc_eth_api/trace/trait.TraceApiServer.html\" title=\"trait cfx_rpc_eth_api::trace::TraceApiServer\">TraceApiServer</a> for <a class=\"struct\" href=\"cfx_rpc/struct.TraceApi.html\" title=\"struct cfx_rpc::TraceApi\">TraceApi</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
