55e08b5edffcb2a6086a2f9d5e150bf0cd46bc42
iosh <iosh@users.noreply.github.com>
2025-05-20 06:06:23 +0000

    deploy: 61d98c09e3b473681fa4361ee076cd220689608a

--- trait.impl/cfx_executor/observer/as_tracer/trait.AsTracer.js
+++ trait.impl/cfx_executor/observer/as_tracer/trait.AsTracer.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"cfx_execute_helper":[["impl <a class=\"trait\" href=\"cfx_executor/observer/as_tracer/trait.AsTracer.html\" title=\"trait cfx_executor::observer::as_tracer::AsTracer\">AsTracer</a> for <a class=\"struct\" href=\"cfx_execute_helper/observer/struct.Observer.html\" title=\"struct cfx_execute_helper::observer::Observer\">Observer</a>"]],
+"cfx_execute_helper":[["impl AsTracer for <a class=\"struct\" href=\"cfx_execute_helper/observer/struct.Observer.html\" title=\"struct cfx_execute_helper::observer::Observer\">Observer</a>"]],
 "cfx_executor":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
