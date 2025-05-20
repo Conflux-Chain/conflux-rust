aa78aa1af269230bb784c16b6e7b338f33298001
Pana <Pana@users.noreply.github.com>
2025-05-20 06:14:50 +0000

    deploy: 48ac153478407dee27ded49bb824e97d76f1c383

--- trait.impl/diem_crypto/traits/trait.ValidCryptoMaterial.js
+++ trait.impl/diem_crypto/traits/trait.ValidCryptoMaterial.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "diem_crypto":[],
-"diem_types":[["impl <a class=\"trait\" href=\"diem_crypto/traits/trait.ValidCryptoMaterial.html\" title=\"trait diem_crypto::traits::ValidCryptoMaterial\">ValidCryptoMaterial</a> for <a class=\"struct\" href=\"diem_types/transaction/authenticator/struct.AuthenticationKey.html\" title=\"struct diem_types::transaction::authenticator::AuthenticationKey\">AuthenticationKey</a>"]]
+"diem_types":[["impl ValidCryptoMaterial for <a class=\"struct\" href=\"diem_types/transaction/authenticator/struct.AuthenticationKey.html\" title=\"struct diem_types::transaction::authenticator::AuthenticationKey\">AuthenticationKey</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- trait.impl/io/trait.IoHandler.js
+++ trait.impl/io/trait.IoHandler.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"network":[["impl <a class=\"trait\" href=\"io/trait.IoHandler.html\" title=\"trait io::IoHandler\">IoHandler</a>&lt;<a class=\"enum\" href=\"network/enum.NetworkIoMessage.html\" title=\"enum network::NetworkIoMessage\">NetworkIoMessage</a>&gt; for <a class=\"struct\" href=\"network/service/struct.NetworkServiceInner.html\" title=\"struct network::service::NetworkServiceInner\">NetworkServiceInner</a>"]]
+"network":[["impl IoHandler&lt;<a class=\"enum\" href=\"network/enum.NetworkIoMessage.html\" title=\"enum network::NetworkIoMessage\">NetworkIoMessage</a>&gt; for <a class=\"struct\" href=\"network/service/struct.NetworkServiceInner.html\" title=\"struct network::service::NetworkServiceInner\">NetworkServiceInner</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
