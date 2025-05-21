a4ad4c6c4a5765f85aec633b8b28c5acd0078636
iosh <ioshx@foxmail.com>
2025-05-21 21:52:55 +0800

    chore: upgrade revm-interpreter  version

--- crates/execution/executor/src/context/revm.rs
+++ crates/execution/executor/src/context/revm.rs
@@ -5,7 +5,9 @@ use alloy_type_conversions::*;
 use cfx_statedb::Result as DbResult;
 use cfx_types::AddressSpaceUtil;
 use cfx_vm_types::Context as ContextTrait;
-use revm_interpreter::primitives::{Address, Bytes, Env, Log, B256, U256};
+use revm_context_interface::journaled_state::AccountLoad;
+use revm_interpreter::{SStoreResult, SelfDestructResult, StateLoad};
+use revm_primitives::{Address, Bytes, Log, B256, U256};
 
 pub(crate) struct EvmHost<'a> {
     context: Context<'a>,
@@ -35,30 +37,36 @@ fn unwrap_db_error(e: cfx_vm_types::Error) -> cfx_statedb::Error {
 const COLD: bool = true;
 
 impl<'a> revm_interpreter::Host for EvmHost<'a> {
-    fn env(&self) -> &Env { todo!() }
+    fn basefee(&self) -> U256 { todo!() }
 
-    fn env_mut(&mut self) -> &mut Env { todo!() }
+    fn blob_gasprice(&self) -> U256 { todo!() }
 
-    fn load_account(
-        &mut self, address: Address,
-    ) -> Option<revm_interpreter::LoadAccountResult> {
-        match self
-            .context
-            .exists_and_not_null(&from_alloy_address(address))
-        {
-            Ok(exists) => Some(revm_interpreter::LoadAccountResult {
-                is_cold: COLD,
-                is_empty: !exists,
-            }),
-            Err(e) => {
-                self.error = Err(unwrap_db_error(e));
-                None
-            }
-        }
-    }
+    fn gas_limit(&self) -> U256 { todo!() }
 
-    fn block_hash(&mut self, number: U256) -> Option<B256> {
-        match self.context.blockhash(&from_alloy_u256(number)) {
+    fn difficulty(&self) -> U256 { todo!() }
+
+    fn prevrandao(&self) -> Option<U256> { todo!() }
+
+    fn block_number(&self) -> u64 { todo!() }
+
+    fn timestamp(&self) -> U256 { todo!() }
+
+    fn beneficiary(&self) -> Address { todo!() }
+
+    fn chain_id(&self) -> U256 { todo!() }
+
+    fn effective_gas_price(&self) -> U256 { todo!() }
+
+    fn caller(&self) -> Address { todo!() }
+
+    fn blob_hash(&self, number: usize) -> Option<U256> { todo!() }
+
+    fn initcode_by_hash(&mut self, hash: B256) -> Option<Bytes> { todo!() }
+
+    fn max_initcode_size(&self) -> usize { todo!() }
+
+    fn block_hash(&mut self, number: u64) -> Option<B256> {
+        match self.context.blockhash(&cfx_types::U256::from(number)) {
             Ok(hash) => Some(to_alloy_h256(hash)),
             Err(e) => {
                 self.error = Err(unwrap_db_error(e));
@@ -67,66 +75,55 @@ impl<'a> revm_interpreter::Host for EvmHost<'a> {
         }
     }
 
-    fn balance(&mut self, address: Address) -> Option<(U256, bool)> {
-        match self.context.balance(&from_alloy_address(address)) {
-            Ok(balance) => Some((to_alloy_u256(balance), COLD)),
-            Err(e) => {
-                self.error = Err(unwrap_db_error(e));
-                None
-            }
-        }
-    }
-
-    fn code(&mut self, address: Address) -> Option<(Bytes, bool)> {
-        match self.context.extcode(&from_alloy_address(address)) {
-            Ok(None) => Some((Bytes::new(), COLD)),
-            Ok(Some(code)) => Some((Bytes::copy_from_slice(&**code), COLD)),
-            Err(e) => {
-                self.error = Err(unwrap_db_error(e));
-                None
-            }
-        }
-    }
-
-    fn code_hash(&mut self, address: Address) -> Option<(B256, bool)> {
-        match self.context.extcodehash(&from_alloy_address(address)) {
-            Ok(hash) => Some((to_alloy_h256(hash), COLD)),
-            Err(e) => {
-                self.error = Err(unwrap_db_error(e));
-                None
-            }
-        }
-    }
-
-    fn sload(&mut self, address: Address, index: U256) -> Option<(U256, bool)> {
-        let receiver =
-            from_alloy_address(address).with_space(self.context.space);
-        let key = index.to_be_bytes::<32>();
-        match self.context.state.storage_at(&receiver, &key) {
-            Ok(value) => Some((to_alloy_u256(value), COLD)),
-            Err(e) => {
-                self.error = Err(e);
-                None
-            }
-        }
-    }
-
-    fn sstore(
-        &mut self, address: Address, index: U256, value: U256,
-    ) -> Option<revm_interpreter::SStoreResult> {
-        // TODO: who checks static flag in revm?
+    fn selfdestruct(
+        &mut self, address: Address, target: Address,
+    ) -> Option<StateLoad<SelfDestructResult>> {
         todo!()
     }
 
-    fn tload(&mut self, address: Address, index: U256) -> U256 { todo!() }
-
-    fn tstore(&mut self, address: Address, index: U256, value: U256) { todo!() }
-
     fn log(&mut self, log: Log) { todo!() }
 
-    fn selfdestruct(
-        &mut self, address: Address, target: Address,
-    ) -> Option<revm_interpreter::SelfDestructResult> {
+    fn sstore(
+        &mut self, address: Address, key: U256, value: U256,
+    ) -> Option<StateLoad<SStoreResult>> {
+        todo!()
+    }
+
+    fn sload(
+        &mut self, address: Address, key: U256,
+    ) -> Option<StateLoad<U256>> {
+        todo!()
+    }
+
+    fn tstore(&mut self, address: Address, key: U256, value: U256) { todo!() }
+
+    fn tload(&mut self, address: Address, key: U256) -> U256 { todo!() }
+
+    fn balance(&mut self, address: Address) -> Option<StateLoad<U256>> {
+        match self.context.balance(&from_alloy_address(address)) {
+            Ok(balance) => Some(StateLoad::new(to_alloy_u256(balance), COLD)),
+            Err(e) => {
+                self.error = Err(unwrap_db_error(e));
+                None
+            }
+        }
+    }
+
+    fn load_account_delegated(
+        &mut self, address: Address,
+    ) -> Option<StateLoad<AccountLoad>> {
+        todo!()
+    }
+
+    fn load_account_code(
+        &mut self, address: Address,
+    ) -> Option<StateLoad<Bytes>> {
+        todo!()
+    }
+
+    fn load_account_code_hash(
+        &mut self, address: Address,
+    ) -> Option<StateLoad<B256>> {
         todo!()
     }
 }
