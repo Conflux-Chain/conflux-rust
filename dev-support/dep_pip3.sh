2027098878824709bccbdebedcfce297f2cb236a
darwintree <17946284+darwintree@users.noreply.github.com>
2025-06-03 16:26:47 +0800

    refactor: update web3 version and remove viem deps

--- dev-support/dep_pip3.sh
+++ dev-support/dep_pip3.sh
@@ -13,7 +13,7 @@ pip3 install \
     trie \
     coincurve \
     safe-pysha3 \
-    conflux-web3==1.4.0b5 \
+    conflux-web3==1.4.3 \
     web3 \
     jsonrpcclient==4.0.3 \
     requests \
@@ -27,16 +27,6 @@ pip3 install \
     git+https://github.com/ethereum/execution-spec-tests.git@e04edbe1f9c0b932b628165d12af8d244e4da776
 # set global solc version which is required by execution-spec-tests
 solc-select use 0.8.24 --always-install
-
-################################################################################
-# temporary solution to use eip7702 signing from viem implementation
-# as upstream web3.py does not support it yet
-#
-# Will remove it and switch to python implementation after web3.py supports it
-
-# set server path to fix ci issue
-export PATH="/home/ubuntu/.nvm/versions/node/v20.18.3/bin:$PATH"
-cd integration_tests/test_framework/util/eip7702/viem_scripts && npm install &&cd ../../../../../
 
 # TODO cross platform
 #yum install clang snappy snappy-devel zlib zlib-devel bzip2 bzip2-devel lz4-devel
