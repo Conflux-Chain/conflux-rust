3e07600e8454d7ba8ec2b16823bbd004a907436b
Pana <pana.wang@outlook.com>
2025-06-09 16:30:57 +0800

    add selfdestruct test contract

--- integration_tests/tests/rpc/espace/test_trace_rpc.py
+++ integration_tests/tests/rpc/espace/test_trace_rpc.py
@@ -3,6 +3,7 @@ from web3.tracing import Tracing
 from web3 import Web3
 from typing import List
 from web3.types import FilterTrace, TxReceipt
+from integration_tests.test_framework.util import load_contract_metadata
 
 
 def verify_erc20_token_transfer_trace(traces: List[FilterTrace], receipt: TxReceipt):
@@ -43,3 +44,35 @@ def test_trace_transaction(ew3_tracing, erc20_token_transfer):
 
     traces = ew3_tracing.trace_transaction(tx_hash)
     verify_erc20_token_transfer_trace(traces, receipt)
+
+def test_trace_suicide(ew3, evm_accounts, ew3_tracing):
+    destroyable_contract_meta = load_contract_metadata("ContractCanBeDestroyed")
+    # destroy_tester_meta = load_contract_metadata("SelfDestructTester")
+    account = evm_accounts[0]
+
+    # deploy contract
+    Contract = ew3.eth.contract(
+        abi=destroyable_contract_meta["abi"], bytecode=destroyable_contract_meta["bytecode"]
+    )
+    tx_hash = Contract.constructor().transact()
+    ew3.eth.wait_for_transaction_receipt(tx_hash)
+
+    deploy_receipt = ew3.eth.get_transaction_receipt(tx_hash)
+    assert deploy_receipt["status"] == 1
+    contract_address = deploy_receipt["contractAddress"]
+
+    # transfer 1 ether to the new deployed contract
+    tx_hash = ew3.eth.send_transaction({
+        "from": account.address,
+        "value": ew3.to_wei(1, 'ether'),
+        "to": contract_address
+    })
+    ew3.eth.wait_for_transaction_receipt(tx_hash)
+
+    contract = ew3.eth.contract(address=contract_address, abi=destroyable_contract_meta["abi"])
+
+    tx_hash = contract.destroy(account.address).transact()
+    ew3.eth.wait_for_transaction_receipt(tx_hash)
+
+    traces = ew3_tracing.trace_transaction(tx_hash)
+    assert len(traces) == 1
