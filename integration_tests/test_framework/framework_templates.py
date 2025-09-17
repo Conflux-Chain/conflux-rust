71cca5c492328f5f6a068b9e691de1e80a1872e6
darwintree <17946284+darwintree@users.noreply.github.com>
2025-09-17 16:55:08 +0800

    tests: add DefaultDevFramework to speedup integration tests

--- integration_tests/test_framework/framework_templates.py
+++ integration_tests/test_framework/framework_templates.py
@@ -14,6 +14,25 @@ class DefaultFramework(ConfluxTestFramework):
         self.setup_nodes()
         self.rpc = RpcClient(self.nodes[0])
 
+
+# dev framework runs a single node in dev mode
+# when recieveing a tx, it will pack and execute it(generate 5 blocks) immediately
+class DefaultDevFramework(ConfluxTestFramework):
+    def set_test_params(self):
+        self.num_nodes = 1
+        self.conf_parameters["mode"] = "'dev'"
+        self.conf_parameters["min_native_base_price"] = 10000
+        self.conf_parameters["base_fee_burn_transition_height"] = 1
+        self.conf_parameters["base_fee_burn_transition_number"] = 1
+
+    def setup_network(self):
+        self.setup_nodes()
+        self.rpc = RpcClient(self.nodes[0])
+        
+    # do nothing because in dev mode
+    def _setup_w3_block_control_middleware(self):
+        pass
+
 class DefaultPoSFramework(ConfluxTestFramework):
     def set_test_params(self):
         self.num_nodes = 4        
