a0c7b21d69801660d3b7f77e20f1efe61818a312
darwintree <17946284+darwintree@users.noreply.github.com>
2025-09-17 16:55:08 +0800

    tests: add DefaultDevFramework to speedup integration tests

--- integration_tests/tests/rpc/conftest.py
+++ integration_tests/tests/rpc/conftest.py
@@ -1,7 +1,13 @@
 import pytest
 from integration_tests.test_framework.test_framework import ConfluxTestFramework
 from integration_tests.conflux.rpc import RpcClient
+from integration_tests.test_framework.framework_templates import DefaultDevFramework
+from typing import Type
 
 @pytest.fixture(scope="module")
 def client(network: ConfluxTestFramework) -> RpcClient:
     return network.client
+
+@pytest.fixture(scope="module")
+def framework_class():
+    return DefaultDevFramework
