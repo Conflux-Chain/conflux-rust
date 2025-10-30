4e6fac9b419309f0d80e2e0398a13adf435b5c0c
Pana <pana.wang@outlook.com>
2025-10-30 17:25:23 +0800

    disable eip7939 by default

--- crates/config/src/configuration.rs
+++ crates/config/src/configuration.rs
@@ -202,7 +202,7 @@ build_config! {
         // For test only
         (align_evm_transition_height, (u64), u64::MAX)
         // V3.1
-        (eip7939_transition_number, (Option<u64>), None)
+        (eip7939_transition_height, (Option<u64>), None)
 
 
         // Mining section.
@@ -1552,10 +1552,8 @@ impl Configuration {
             self.raw_conf.align_evm_transition_height;
 
         // hardfork (V3.1)
-        params.transition_heights.eip7939 = self
-            .raw_conf
-            .eip7939_transition_number
-            .unwrap_or(default_transition_time);
+        params.transition_heights.eip7939 =
+            self.raw_conf.eip7939_transition_height.unwrap_or(u64::MAX); // Disabled by default
     }
 }
 
