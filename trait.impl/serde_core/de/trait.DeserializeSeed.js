29bffaba0e08accdcb082290e8d52a9db48569a0
Pana <Pana@users.noreply.github.com>
2025-12-09 06:14:13 +0000

    deploy: 483cce710f6aa9b4e6d64cd2e3f6df896e175485

---
+++ trait.impl/serde_core/de/trait.DeserializeSeed.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["move_core_types",[["impl&lt;'d&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/de/trait.DeserializeSeed.html\" title=\"trait serde_core::de::DeserializeSeed\">DeserializeSeed</a>&lt;'d&gt; for &amp;<a class=\"enum\" href=\"move_core_types/value/enum.MoveTypeLayout.html\" title=\"enum move_core_types::value::MoveTypeLayout\">MoveTypeLayout</a>"],["impl&lt;'d&gt; <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/de/trait.DeserializeSeed.html\" title=\"trait serde_core::de::DeserializeSeed\">DeserializeSeed</a>&lt;'d&gt; for &amp;<a class=\"struct\" href=\"move_core_types/value/struct.MoveStructLayout.html\" title=\"struct move_core_types::value::MoveStructLayout\">MoveStructLayout</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[747]}
