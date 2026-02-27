9123c9ae0762b421bd8efefb7aaa07ae6d9a3342
Peilun Li <peilun.li@confluxnetwork.org>
2026-02-27 20:16:05 +0800

    fix warning.

--- crates/dbs/kvdb-rocksdb/src/lib.rs
+++ crates/dbs/kvdb-rocksdb/src/lib.rs
@@ -42,7 +42,6 @@ use std::path::PathBuf;
 use std::process::Command;
 
 type DBError = String;
-type KeyValuePair = (Box<[u8]>, Box<[u8]>);
 fn other_io_err<E>(e: E) -> io::Error
 where E: Into<Box<dyn error::Error + Send + Sync>> {
     io::Error::new(io::ErrorKind::Other, e)
