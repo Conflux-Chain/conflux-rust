83a93868961d250c2884291bd5b082c7f2c515c4
Pana <pana.wang@outlook.com>
2026-04-27 17:33:50 +0800

    op

--- crates/rpc/rpc-utils/src/error/mod.rs
+++ crates/rpc/rpc-utils/src/error/mod.rs
@@ -1,14 +1,9 @@
 pub mod api;
 pub mod error_codes;
 pub mod errors;
-// pub mod jsonrpc_error_helpers;
 pub mod jsonrpsee_error_helpers;
 
 pub use errors::{
     EthApiError, EthResult, RevertError, RpcInvalidTransactionError,
     RpcPoolError,
 };
-
-// pub use jsonrpc_error_helpers::{
-//     error_object_owned_to_jsonrpc_error, jsonrpc_error_to_error_object_owned,
-// };
