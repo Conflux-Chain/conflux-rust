56cb910efd10d5c5ad594d84c3a1cba5316beb28
Pana <Pana@users.noreply.github.com>
2026-06-11 08:42:35 +0000

    deploy: 5f6c34cd39183f17392952b21e8cf2bade8ad080

--- cfx_rpc_middlewares/sidebar-items.js
+++ cfx_rpc_middlewares/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["Logger","Metrics","Throttle"]};
+window.SIDEBAR_ITEMS = {"enum":["CorsDomainError"],"fn":["create_cors_layer","maybe_cors_layer"],"struct":["Logger","Metrics","Throttle"]};
